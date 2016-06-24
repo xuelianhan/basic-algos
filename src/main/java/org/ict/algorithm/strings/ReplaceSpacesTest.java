2f96a2533db328683e83b2036508e005e180fd3a
xuelian.han <xuelian.han@qunar.com>
2016-06-24 11:15:20 +0800

    add ReplaceSpacesTest.java

---
+++ src/main/java/org/ict/algorithm/strings/ReplaceSpacesTest.java
@@ -0,0 +1,17 @@
+package orgi.cit.algorithm.strings;
+
+/**
+ *
+ * 1.4.Write a method to replace all spaces in a stirng with '%20'.You may assume that the
+ * string has sufficient space at the end of the string to hold the additional characters,
+ * and that you are given the "true" length of the string.(Note:if implementing in Java, 
+ * please use a character array so that you can perform this operation in place.)
+ *
+ *
+ *
+ */
+public class ReplaceSpacesTest {
+    
+    
+
+}
