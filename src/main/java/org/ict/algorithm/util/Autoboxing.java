aa3430dbd69fffe315e28bd8fe737cf3b5d825c2
xuelian.han <xuelian.han@qunar.com>
2016-07-30 10:41:19 +0800

    add autoboxing test

--- src/main/java/org/ict/algorithm/util/Autoboxing.java
+++ src/main/java/org/ict/algorithm/util/Autoboxing.java
@@ -9,14 +9,26 @@ public class Autoboxing {
         } else if (first > second) {
             StdOut.printf("%d > %d\n", first, second);
         } else {
-            StdOut.printf("%d and %d are imcomparable!", first, second);
+            StdOut.printf("%d and %d are imcomparable!\n", first, second);
         }
     }
 
     public static void main(String[] args) {
         cmp(new Integer(42), 42);
         cmp(new Integer(42), 43);
-        cmp(new Integer(42), 41);
+        cmp(new Integer(42), new Integer(42));
+        cmp(43, 43);
+        cmp(143,143);
+
+        double x1 = 0.0, y1 = -0.0;
+        Double a1 = x1, b1 = y1;
+        StdOut.println(x1 == y1);
+        StdOut.println(a1.equals(b1));
+
+        double x2 = 0.0/0.0, y2 = 0.0/0.0;
+        Double a2 = x2, b2 = y2;
+        StdOut.println(x2 != y2);
+        StdOut.println(!a2.equals(b2));
     }
 
 }
