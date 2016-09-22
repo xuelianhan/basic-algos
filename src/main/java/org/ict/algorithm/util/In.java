c2b99f156ef4915f24ef6a56955a409643e88e7c
xuelian.han <xuelian.han@qunar.com>
2016-09-22 19:50:11 +0800

    add code

--- src/main/java/org/ict/algorithm/util/In.java
+++ src/main/java/org/ict/algorithm/util/In.java
@@ -9,12 +9,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.regex.Pattern;
+import java.util.NoSuchElementException;
 
 public final class In {
 
     private static final String CHARSET_NAME = "UTF-8";
 
     private static final Locale LOCALE = Locale.US;
+
+    private static final Pattern WHITESPACE_PATTERN = Pattern.compile("\\p{javaWhitespace}+");
+
+    private static final Pattern EMPTY_PATTERN = Pattern.compile("");
+
+    private static final Pattern EVERYTHING_PATTERN = Pattern.compile("\\A");
 
     private Scanner scanner;
 
@@ -23,6 +32,11 @@ public final class In {
         scanner.useLocale(LOCALE);
     }
 
+    /**
+     * Initializes an input stream from a socket.
+     *
+     *
+     */
     public In(Socket socket) {
         if (socket == null) {
             throw new NullPointerException("socket is null!");
@@ -36,6 +50,10 @@ public final class In {
         }
     }
 
+    /**
+     * Initializes an input stream from a URL.
+     *
+     */
     public In(URL url) {
         if (url == null) {
             throw new NullPointerException("url is null!");
@@ -50,16 +68,126 @@ public final class In {
         }
     }
 
+    /**
+     * Initializes an input stream from a file.
+     *
+     */
     public In(File file) {
         if (file == null) {
             throw new NullPointerException("file is null!");
         }
         try {
-            scanner = new Scanner(file, CHARSET_NAME);
+            //scanner = new Scanner(file, CHARSET_NAME);
+            // for consistency with StdIn, wrap with BufferedInputStream instead of use
+            // file as argument to Scanner
+            FileInputStream fis = new FileInputStream(file);
+            scanner = new Scanner(new BufferedInputStream(fis), CHARSET_NAME);
             scanner.useLocale(LOCALE);
         } catch (IOException e) {
             throw new IllegalArgumentException("Could not open " + file);
         }
     }
 
+    /**
+     * Initializes an input stream from a filename or web page name.
+     *
+     * @param name the filename of webpage name
+     * @throws IllegalArgumentException if cannot open{@code name} as a file or URL.
+     * @throws NullPointerException if {@code name} is {@code null}
+     */
+    public In(String name) {
+        if (name == null) throw new NullPointerException("argument is null");
+        try {
+            //first try to read file from local file system.
+            File file = new File(name);
+            if (file.exists()) {
+                // for consistency with StdIn, wrap with BufferedInputStream 
+                // instead of use file as argument to Scanner.
+                FileInputStream fis = new FileInputStream(file);
+                scanner = new Scanner(new BufferedInputStream(fis), CHARSET_NAME);
+                scanner.useLocale(LOCALE);
+                return;
+            }
+
+            //next try for files included in jar.
+            URL url = getClass().getResource(name);
+
+            //or URL from web.
+            if (url == null) {
+                url = new URL(name);
+            }
+
+            URLConnection site = url.openConnection();
+
+            //in order to set User-Agent, replace above line with these two
+            //HttpURLConnection site = (HttpURLConnection)url.openConnection();
+            //site.addRequestProperty("User-Agent", "MOzilla/4.76");
+
+            InputStream is = site.getInputStream();
+            scanner = new Scanner(new BufferedInputStream(is), CHARSET_NAME);
+            scanner.useLocale(LOCALE);
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException("Could not open " + name);
+        }
+    }
+
+    /**
+     * Initializes an input stream from a given {@link Scanner} source;
+     * use  with {@code new Scanner(String)} to read from a string.
+     * <p>
+     * Note that this does not create a defensive copy, so the scanner
+     * will be mutated as you read on.
+     *
+     * @param Scanner the scanner
+     * @throws NullPointerException if {@code scanner} is {@code null}
+     */
+    public In(Scanner scanner) {
+        if (scanner == null) {
+            throw new NullPointerException("argument is null");
+        }
+        this.scanner = scanner;
+    }
+
+    /**
+     * Returns true if this input stream exists.
+     *
+     * @return {@code true} if this input stream exists;{@code false} otherwise
+     */
+    public boolean exists() {
+        return scanner != null;
+    }
+
+    public boolean isEmpty() {
+        return !scanner.hasNext();
+    }
+
+    public boolean hasNextLine() {
+        return scanner.hasNextLine();
+    }
+
+    public boolean hasNextChar() {
+        scanner.useDelimiter(EMPTY_PATTERN);
+        boolean result = scanner.hasNext();
+        scanner.useDelimiter(WHITESPACE_PATTERN);
+        return result;
+    }
+
+    public String readLine() {
+        String line;
+        try {
+            line = scanner.nextLine();
+        } catch (NoSuchElementException e) {
+            line = null;
+        }
+        return line;
+    }
+
+    /**
+     * Reads and returns the next character in this input stream.
+     *
+     * @return the next character in this input stream
+     */
+    public char readChar() {
+        
+    }
 }
