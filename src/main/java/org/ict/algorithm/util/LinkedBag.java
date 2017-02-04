6c165cce1e375f1f59e86d287bd776e92cab8882
xuelian.han <xuelian.han@qunar.com>
2017-02-04 15:01:02 +0800

    add code for LinkedBag.java

--- src/main/java/org/ict/algorithm/util/LinkedBag.java
+++ src/main/java/org/ict/algorithm/util/LinkedBag.java
@@ -1,7 +1,21 @@
 package org.ict.algorithm.util;
 
-
-
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+/**
+ * The {@code LinkedBag} class represents a bag (or multiset) of 
+ * generic items. it supports insertion and iterating over the
+ * items in arbitrary order.
+ * <p>
+ * This implementation uses a singly-linked list with a non-static nested class Node.
+ * See {@link Bag} for a version that uses a static nested class.
+ * the <em>add</em>, <em>isEmpty</em>, and <em>size</em> operations
+ * take constant time. Iteration takes time proportional to the number of items.
+ * <p>
+ * @author RobertSedgewick
+ * @author Kevin Wayne
+ *
+ */
 public class LinkedBag<Item> implements Iterable<Item> {
     private Node first;
 
@@ -12,5 +26,62 @@ public class LinkedBag<Item> implements Iterable<Item> {
         private Node next;
     }
 
+    public LinkedBag() {
+        first = null;
+        n = 0;
+    }
 
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void add(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        n++;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+
+    public static void main() {
+        LinkedBag<String> bag = new LinkedBag<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            bag.add(item);
+        }
+        StdOut.println("size of bag = " + bag.size());
+        for (String s : bag) {
+            StdOut.println(s);
+        }
+    }
 }
