65339742e76e0ce40b735588f34a313cb1231bf5
xuelian.han <xuelian.han@qunar.com>
2017-02-04 16:32:32 +0800

    mv Stats to fundamentals package

---
+++ src/main/java/org/ict/algorithm/fundamentals/Stats.java
@@ -0,0 +1,37 @@
+package org.ict.algorithm.fundamentals;
+
+import org.ict.algorithm.util.StdIn;
+import org.ict.algorithm.util.StdOut;
+
+public class Stats {
+	
+    public static void main(String[] args) {
+    	//1.input
+        Bag<Double> bag = new Bag<Double>();
+        while(!StdIn.isEmpty()) {
+        	bag.add(StdIn.readDouble());
+        }
+        
+        int N = bag.size();
+        
+        //2.compute sample mean
+        Double sum = 0d;
+        for (Double d : bag) {
+        	sum += d;
+        }
+        Double mean = sum / N;
+        
+        //3.compute sample standard deviation
+        sum = 0.0;
+        for (Double d : bag) {
+        	sum += (d - mean) * (d - mean);
+        }
+        double std = Math.sqrt(sum);
+        
+        //4.output result
+        StdOut.printf("mean: %.2f\n", mean);
+        StdOut.printf("std:  %.2f\n", std);
+        StdOut.println();
+    }
+
+}
