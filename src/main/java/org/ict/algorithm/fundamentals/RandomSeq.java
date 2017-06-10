e575fd882a2080520cd2acdcadcda30fb73e12ed
xuelian.han <xuelian.han@qunar.com>
2017-06-10 15:47:08 +0800

    add comments for RandomSeq

--- src/main/java/org/ict/algorithm/fundamentals/RandomSeq.java
+++ src/main/java/org/ict/algorithm/fundamentals/RandomSeq.java
@@ -8,7 +8,29 @@ import org.ict.algorithm.util.StdRandom;
  * sequence of real numbers in a given range.
  * For additional documentation, see <a href="http://algs4.cs.princeton.edu/11model/">Section 1.1</a> of
  * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- * 
+ *
+ * compilation:
+ * $ javac org/ict/algorithm/fundamentals/RandomSeq.java 
+ *
+ * execution:
+ * $ java org/ict/algorithm/fundamentals/RandomSeq 5
+ * 0.7680989943862664
+ * 0.7532770266960576
+ * 0.11705006491094172
+ * 0.6355493801551122
+ * 0.39025512514970595
+ *
+ * $ java org/ict/algorithm/fundamentals/RandomSeq 10 100 200
+ * 154.96
+ * 114.43
+ * 176.28
+ * 139.28
+ * 131.75
+ * 130.29
+ * 171.42
+ * 129.43
+ * 158.89
+ * 179.65
  *
  */
 public class RandomSeq {
