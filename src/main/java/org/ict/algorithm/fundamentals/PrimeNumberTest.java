96f83fb6f59f13d08683338b58281d74ffdd2e86
xuelian.han <xueliansniper@gmail.com>
2017-06-13 09:09:16 +0800

    add wiki link for prime number class

--- src/main/java/org/ict/algorithm/fundamentals/PrimeNumberTest.java
+++ src/main/java/org/ict/algorithm/fundamentals/PrimeNumberTest.java
@@ -5,8 +5,9 @@ package org.ict.algorithm.fundamentals;
  * A prime number is a natural number greater than 1 that
  * has no positive divisors other than 1 and itself.
  * see <a href="https://en.wikipedia.org/wiki/Prime_number">Prime_number</a>
+ * see <a href="https://www.mkyong.com/java/how-to-determine-a-prime-number-in-java/">Prime_number</a>
+ * see <a href="http://world.mathigon.org/Prime_Numbers">Prime_Numbers</a>
  * 
- * @author hanxuelian
  *
  */
 public class PrimeNumberTest {
