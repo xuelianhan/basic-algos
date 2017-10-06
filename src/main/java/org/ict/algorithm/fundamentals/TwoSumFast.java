694543869f3a2982f3c1e84d2636fa06127eeb44
xuelian.han <xuelian.han@qunar.com>
2017-10-06 19:55:28 +0800

    add TwoSumFast One-Pass HashTable version

--- src/main/java/org/ict/algorithm/fundamentals/TwoSumFast.java
+++ src/main/java/org/ict/algorithm/fundamentals/TwoSumFast.java
@@ -1,8 +1,11 @@
 package org.ict.algorithm.fundamentals;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
 import org.ict.algorithm.util.In;
 import org.ict.algorithm.util.StdOut;
 import org.ict.algorithm.search.BinarySearch;
+
 
 
 /**
@@ -24,12 +27,31 @@ public class TwoSumFast {
         }
         return cnt;
     }
-    
+
+    /**
+     * To improve our running time complexity, we need a more efficient way to check if 
+     * the complement exists in the array. If the complement exists, we need to look up
+     * its index. Using hash table to reduce the look up time from O(n) to O(1) by trading
+     * space for speed.
+     */
+    public static int[] twoSumPair(int[] a, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < a.length; i++) {
+            int complement = target - a[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            map.put(a[i], i);
+        }
+        throw new IllegalArgumentException("No two sum pair solution");
+    }
 
     public static void main(String[] args) {
         In in = new In(args[0]);
         int[] a = in.readAllInts(); 
         StdOut.println(count(a));
+        int[] result = twoSumPair(a, 0);
+        StdOut.println(Arrays.toString(result));
     }
 
 }
