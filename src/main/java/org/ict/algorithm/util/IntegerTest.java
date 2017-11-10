43661a7aaa0d291385e425df4afaf342987d27e2
xuelian.han <xueliansniper@gmail.com>
2017-11-10 15:13:11 +0800

    add integer test

--- src/main/java/org/ict/algorithm/util/IntegerTest.java
+++ src/main/java/org/ict/algorithm/util/IntegerTest.java
@@ -1,11 +1,31 @@
 package org.ict.algorithm.util;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class IntegerTest {
 
-    public static void main(String[] args) {
+    public static void testBigDecimal() {
+        BigDecimal r = new BigDecimal(2596);
+        //BigDecimal r = new BigDecimal(0);
+        BigDecimal d = new BigDecimal(100);
+        BigDecimal result = r.divide(d, 2, RoundingMode.HALF_UP);
+        System.out.println(result);
+    }
+    
+    public static void testNullPointer() {
         Integer a = null;
+        
+        //java.lang.NullPointerException
         int b = a;
+        //java.lang.NullPointerException
+        
         System.out.println("World is OK...");
+    }
+    
+    public static void main(String[] args) {
+        
+        testBigDecimal();
     }
 
 }
