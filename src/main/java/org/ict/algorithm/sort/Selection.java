526850fbc9fe6632b11a958f8460360762cd2d5c
xuelian.han <xueliansniper@gmail.com>
2017-11-22 14:16:37 +0800

    add test for Selection

--- src/main/java/org/ict/algorithm/sort/Selection.java
+++ src/main/java/org/ict/algorithm/sort/Selection.java
@@ -19,7 +19,9 @@
 package org.ict.algorithm.sort;
 import java.util.Comparator;
 
-public class SelectionSort {
+import org.ict.algorithm.util.StdIn;
+
+public class Selection {
 	
 	public static void sort(Comparable[] a) {
 		int N = a.length;
@@ -51,29 +53,15 @@ public class SelectionSort {
 		assert AbstractSortHelper.isSorted(a, c);
 	}
 
-	private static void checkRange(int low, int high, int  length) {
-		if (high < low) 
-			throw new IllegalArgumentException("high:("+high+") < low:("+low+")");
-		if (low < 0)
-			throw new ArrayIndexOutOfBoundsException(low);
-		if (high < length)
-			throw new ArrayIndexOutOfBoundsException(high);
-	}
-
-	private static void showArrayData(Comparable[] a) {
-		for (int i = 0; i < a.length; i++) {
-			if (i == (a.length - 1))
-			    System.out.print(a[i]);
-			else 
-			    System.out.print(a[i]+ ",");
-		}	
-	}
-
+	
+	/**
+	 * Reads in a sequence of strings from standard input; selection sorts them;
+	 * and prints them to standard output in ascending order.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		/*
-		String[] a = {"abigail", "matt", "sed", "apache", "tmall","dangdang","jd","suning","guomei","amazon","ebey","avon","google","apple","nutch","cloud"};	
-		sort(a);
-		showArrayData(a);
-		*/
+		String[] a = StdIn.readAllStrings();
+		Selection.sort(a);
+		AbstractSortHelper.show(a);
 	}
 }
