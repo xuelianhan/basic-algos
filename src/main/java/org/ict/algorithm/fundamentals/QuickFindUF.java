4edba1f4273a75ad5de47a3c64a57aa6d8ce9a4c
xuelian.han <xuelian.han@qunar.com>
2017-11-24 17:06:06 +0800

    add test data for UF.java

---
+++ src/main/java/org/ict/algorithm/fundamentals/QuickFindUF.java
@@ -0,0 +1,11 @@
+package org.ict.algorithm.fundamentals;
+
+public class QuickFindUF {
+
+    //id[id] = component identifier of i
+    private int[] id;
+    
+    private int count;
+    
+    
+}
