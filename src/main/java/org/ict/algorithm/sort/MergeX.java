1adaf6f1db75347e700357d1c8fb9f2ee5ce66cb
xuelian.han <xueliansniper@gmail.com>
2018-01-12 16:21:39 +0800

    add MergeX.java

---
+++ src/main/java/org/ict/algorithm/sort/MergeX.java
@@ -0,0 +1,28 @@
+package org.ict.algorithm.sort;
+
+import org.ict.algorithm.util.StdIn;
+import org.ict.algorithm.util.StdOut;
+
+/**
+ *
+ * The {@code MergeX} class provides static methods for sorting an 
+ * array using an optimized version of mergesort
+ * <p>
+ * For additional documentation,
+ * see <a href="https://algs4.cs.princeton.edu/22mergesort/">Section 2.2</a>
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ */
+public class MergeX extends AbstractSortHelper {
+    private static final int CUTOFF = 7;
+
+    //This class should not be instantiated
+    private MergeX() {}
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
+
