4c22f16db9b6acd6b660979472e4b8d90d036cfb
xuelian.han <xuelian.han@qunar.com>
2018-02-07 17:47:05 +0800

    add Inversions.java

---
+++ src/main/java/org/ict/algorithm/sort/Inversions.java
@@ -0,0 +1,21 @@
+package org.ict.algorithm.sort;
+
+/**
+ *  The {@code Inversions} class provides static methods to count the
+ *  number of <em>inversions</em> in either an array of integers or 
+ *  comparables
+ *  An inversion in an array {@code a[]} is a pair of indicies {@code i}
+ *  and {@code j} such that {@code i < j} and {@code a[i] > a[j]}
+ *  <p>
+ *  This implementation uses a generalization of mergesort
+ *  The <em>count</em> operation takes time proportional to <em>nlogn</em>,
+ *  where <em>n</em> is the number of keys in the array
+ *  
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ *
+ */
+public class Inversions {
+
+
+}
