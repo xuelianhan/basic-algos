c4900aa0ba375d86d61b229475963fe12a4ecd6c
xuelian.han <xueliansniper@gmail.com>
2018-02-12 16:37:52 +0800

    add Inversions.java

--- src/main/java/org/ict/algorithm/sort/Inversions.java
+++ src/main/java/org/ict/algorithm/sort/Inversions.java
@@ -1,5 +1,8 @@
 package org.ict.algorithm.sort;
 
+import org.ict.algorithm.util.StdIn;
+import org.ict.algorithm.util.StdOut;
+
 /**
  *  The {@code Inversions} class provides static methods to count the
  *  number of <em>inversions</em> in either an array of integers or 
@@ -16,6 +19,44 @@ package org.ict.algorithm.sort;
  *
  */
 public class Inversions {
+    // do not instantiate
+    private Inversions() {}
 
+    // merge and count
+    private static long merge(int[] a, int[] aux, int lo, int mid, int hi) {
+        long inversions = 0;
+
+        //copy to aux[]
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        //merge back to a[]
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+
+            } else if (j > hi) {
+
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+                inversions += (mid - i + 1);
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+        return inversions;
+    }
+
+    //return the number of inversions in the subarray b[lo..hi]
+    //side effect b[lo..hi] is rearranged in ascending order
+    private static long count(int[] a, int[] b, int[] aux, int lo, int hi) {
+        long inversions = 0;
+        if (hi <= lo) {
+            return 0;
+        }
+        int mid = lo +  (hi - lo) / 2;
+
+    }
 
 }
