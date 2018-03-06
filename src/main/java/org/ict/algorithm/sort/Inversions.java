6a12eee4b40efcbc8cc28c1b8608926c8c704d43
xuelian.han <xueliansniper@gmail.com>
2018-03-06 12:42:22 +0800

    add codes in Inversions.java

--- src/main/java/org/ict/algorithm/sort/Inversions.java
+++ src/main/java/org/ict/algorithm/sort/Inversions.java
@@ -19,6 +19,18 @@ import org.ict.algorithm.util.StdOut;
  *
  */
 public class Inversions {
+
+    public static void main(String[] args) {
+        int[] a = StdIn.readAllInts();
+        int n = a.length;
+        Integer[] b = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            b[i] = a[i];
+        }
+        StdOut.println(Inversions.count(a));
+        StdOut.println(Inversions.count(b));
+    }
+
     // do not instantiate
     private Inversions() {}
 
@@ -110,7 +122,7 @@ public class Inversions {
      *  @see https://docs.oracle.com/javase/tutorial/java/generics/upperBounded.html
      *  @see 
      */
-    private static <Key extends Comparable<Key>> long merge(Key[] a, key[] aux, int lo, int mid, int hi) {
+    private static <Key extends Comparable<Key>> long merge(Key[] a, Key[] aux, int lo, int mid, int hi) {
         long inversions = 0; 
 
         //copy to aux[]
@@ -122,13 +134,14 @@ public class Inversions {
         int i = lo, j = mid+1;
         for (int k = lo; k <= hi; k++) {
             if (i > mid) {
-
+                a[k] = aux[j++];
             } else if (j > hi) {
-
-            } else if () {
-
+                a[k] = aux[i++];
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+                inversions += (mid - i + 1);
             } else {
-
+                a[k] = aux[i++];
             }
         }
         return inversions;   
@@ -136,6 +149,50 @@ public class Inversions {
 
     // is v < w ?
     private static <Key extends Comparable<Key>> boolean less(Key v, Key w) {
-        
+        return (v.compareTo(w) < 0); 
+    }
+
+    /**
+     * Returns the number os inversions in the comparable array.
+     * The argument array is not modified.
+     * @param a the array
+     * @param <Key> the inferred type of the elements in the array
+     * @return the number of inversions in the array. An inversion is a 
+     * pair of indices {@code i} and {@code j} such that {@code i < j} 
+     * and {@code a[i].compareTo(a[j]) > 0}.
+     *
+     */
+    public static <Key extends Comparable<Key>> long count(Key[] a) {
+        Key[] b = a.clone();
+        Key[] aux = a.clone();
+        long inversions = count(a, b, aux, 0, a.length - 1);
+        return inversions;
+    }
+
+    //return the number of inversions in the subarray b[lo..hi]
+    //side effect b[lo..hi] is rearranged in ascending order
+    private static <Key extends Comparable<Key>> long count(Key[] a, Key[] b, Key[] aux, int lo, int hi) {
+        long inversions = 0;
+        if(hi <= lo) {
+            return 0;
+        }
+        int mid = lo + (hi - lo) / 2;
+        inversions += count(a, b, aux, lo, mid);
+        inversions += count(a, b, aux, mid+1, hi);
+        inversions += merge(b, aux, lo, mid, hi);
+        assert inversions == brute(a, lo, hi);
+        return inversions;
+    }
+
+    private static <Key extends Comparable<Key>> long brute(Key[] a, int lo, int hi) {
+        long inversions = 0;
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i + 1; j <= hi; j++) {
+                if (less(a[j], a[i])) {
+                    inversions++;
+                }
+            }
+        }
+        return inversions;
     }
 }
