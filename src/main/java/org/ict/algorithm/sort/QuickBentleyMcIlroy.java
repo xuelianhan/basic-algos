b1b72186c25c5f10b754e670848316fe398537b1
xuelian.han <xueliansniper@gmail.com>
2018-03-26 13:46:52 +0800

    add codes in QuickBentleyMcIlroy.java

--- src/main/java/org/ict/algorithm/sort/QuickBentleyMcIlroy.java
+++ src/main/java/org/ict/algorithm/sort/QuickBentleyMcIlroy.java
@@ -5,6 +5,13 @@ import org.ict.algorithm.util.StdIn;
 import org.ict.algorithm.util.StdOut;
 
 /**
+ * $ javac org/ict/algorithm/sort/QuickBentleyMcIlroy.java 
+ * Note: org/ict/algorithm/sort/QuickBentleyMcIlroy.java uses unchecked or unsafe operations.
+ * Note: Recompile with -Xlint:unchecked for details.
+ * $ java org/ict/algorithm/sort/QuickBentleyMcIlroy < ../resources/tiny.txt 
+ * true
+ * [A, E, E, L, M, O, P, R, S, T, X]
+ *
  * Uses the Bentley-McIlroy 3-way partitioning scheme,
  * chooses the partitioning element using Tukey's ninther,
  * and cuts off to insertion sort.
@@ -35,13 +42,13 @@ public class QuickBentleyMcIlroy {
         sort(a, 0, a.length - 1);
     }
 
-    private void sort(Comparable[] a, lo, hi) {
+    private static void sort(Comparable[] a, int lo, int hi) {
         int n = hi - lo + 1;
         if (n < INSERTION_SORT_CUTOFF) {
             // cutoff to insertion sort
             insertionSort(a, lo, hi);
             return;
-        } else if ( n <= median_of_3_cutoff) {
+        } else if ( n <= MEDIAN_OF_3_CUTOFF) {
             // use median-of-3 as partitioning element 
             int m = median3(a, lo , lo + n/2, hi);
             exch(a, m, lo);
@@ -70,12 +77,90 @@ public class QuickBentleyMcIlroy {
         int p = lo, q = hi + 1;
         Comparable v = a[lo];
         while (true) {
-            
+            while (less(a[++i], v)) {
+                if (i == hi)break;
+            }
+            while (less(v, a[--j])) {
+                if (j == lo)break;
+            }
+
+            // pointers cross
+            if (i == j && eq(a[i], v)) {
+                exch(a, ++p, i);
+            }
+            if (i >= j) {
+                break;
+            }
+            exch(a, i, j);
+            if (eq(a[i], v)) {
+                exch(a, ++p, i);
+            }
+            if (eq(a[j], v)) {
+                exch(a, --q, j);
+            }
         }
-        
 
-
+        i = j + 1; 
+        for (int k = lo; k <=p; k++) {
+            exch(a, k, j--);
+        }
+        for (int k = hi; k >=q; k--) {
+            exch(a, k, i++);
+        }
+        sort(a, lo, j);
+        sort(a, i, hi);
     }
 
+    // sort from a[lo] to a[hi] using insertion sort 
+    private static void insertionSort(Comparable[] a, int lo, int hi) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j-1]); j--) {
+                exch(a, j, j-1);
+            }
+        }
+    }
 
+    // return the index of the median element amoung a[i], a[j] and a[k]
+    private static int median3(Comparable[] a, int i, int j, int k) {
+        return (less(a[i], a[j]) ?
+               (less(a[j], a[k]) ? j : less(a[i], a[k]) ? k : i):
+               (less(a[k], a[j]) ? j : less(a[k], a[i]) ? k : i));
+    }
+
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        // optimization when reference equal
+        if (v == w) return false; 
+        return v.compareTo(w) < 0;
+    }
+
+    // does v == w ?
+    private static boolean eq(Comparable v, Comparable w) {
+        // optimization when reference equal
+        if (v == w) return true;
+        return v.compareTo(w) == 0;
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i-1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        QuickBentleyMcIlroy.sort(a);
+        StdOut.println(isSorted(a));
+        StdOut.println(Arrays.toString(a));
+    }
 }
