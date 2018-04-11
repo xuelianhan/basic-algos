93c5c75149790a2ee270829f5154cf5c4e2d77b7
xuelian.han <xuelian.han@qunar.com>
2018-04-11 20:37:00 +0800

    add codes in BST.java

--- src/main/java/org/ict/algorithm/search/BST.java
+++ src/main/java/org/ict/algorithm/search/BST.java
@@ -576,6 +576,71 @@ public class BST<Key extends Comparable<Key>, Value> {
         if (!isSizeConsistent()) {
             StdOut.println("Subtree counts not consistent");
         }
+        if (!isRankConsistent()) {
+            StdOut.println("Ranks not consistent");
+        }
+        return isBST() && isSizeConsistent() && isRankConsistent();
+    }
 
+    // Does this binary tree satisfy symmetric order?
+    // Note : this test also ensures that data structure is a binary tree since order is strict
+    private boolean isBST() {
+        return isBST(root, null, null);
+    }
+
+    // is the tree rooted at x a BST with all keys strictly between min and max
+    // (if min or max is null, treat as empty constraint)
+    // Credit: Bob Dondero's elegant solution
+    private boolean isBST(Node x, Key min, Key max) {
+        if (x == null) {
+            return true;
+        }
+        if (min != null && x.key.compareTo(min) <= 0) {
+            return false;
+        }
+        if (max != null && x.key.compareTo(max) >= 0) {
+            return false;
+        }
+        return isBST(x.left, min, x.key) && isBST(x.right, x.key, max);
+    }
+
+    // are the size fields correct?
+    private boolean isSizeConsistent() {
+        return isSizeConsistent(root);
+    }
+
+    private boolean isSizeConsistent(Node x) {
+        if (x == null) {
+            return true;
+        }
+        if (x.size != size(x.left) + size(x.right) + 1) {
+            return false;
+        }
+        return isSizeConsistent(x.left) && isSizeConsistent(x.right);
+    }
+
+    // check that ranks are consistent
+    private boolean isRankConsistent() {
+        for (int i = 0; i < size(); i++) {
+            if (i != rank(select(i))) {
+                return false; 
+            }
+        }
+        for (Key key : keys()) {
+            if (key.compareTo(select(rank(key))) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Unit tests the {@code BST} data type.
+     */
+    public static void main(String[] args) {
+        BST<String, Integer> st = new BST<String, Integer>();
+        for (int i = 0; !StdIn.isEmpty(); i++) {
+            
+        }
     }
 }
