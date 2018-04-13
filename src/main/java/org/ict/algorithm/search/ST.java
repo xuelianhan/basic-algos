1f3a050069a90a69cf71c30863dee9ccf306c77a
xuelian.han <xuelian.han@qunar.com>
2018-04-13 16:50:00 +0800

    add comments in ST.java

--- src/main/java/org/ict/algorithm/search/ST.java
+++ src/main/java/org/ict/algorithm/search/ST.java
@@ -15,9 +15,26 @@ import java.util.TreeMap;
  * It also provides a <em>keys</em> method for iterating over all of the
  * keys.
  * A symbol table implements the <em>associative array</em> abstraction:
- * when associating <em>get</em>    
- * , <em>get</em>, <em>get</em>, <em>get</em>, <em>get</em>, <em>get</em>     
- * , <em>get</em>, <em>get</em>, <em>get</em>   
+ * when associating a value with a key that is already in the symbol table,
+ * the convention is to replace the old value with the new value.
+ * Unlike {@link java.util.Map}, this class uses the convention that 
+ * values cannot be {@code null}-setting the value associated with a key to
+ * {@code null} is equivalent to deleting the key from the symbol table.
+ * <p>
+ * This implementation uses a balanced binary search tree. It requires that
+ * the key type implements the {@code Comparable} interface and calls the 
+ * {@code compareTo()} and method to compare two keys. It does not call 
+ * either {@code equals()} or {@code hashCode()}.
+ * The <em>put</em>, <em>contains</em>, <em>remove</em>, <em>minimum</em>, 
+ * <em>maximum</em>, <em>ceiling</em>, and <em>floor</em> operations each
+ * take logarithmic time in the worst case.
+ *
+ * The<em>size</em>, and <em>is-empty</em> operations take constant time.
+ * Construction takes constant time.
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ *
  */
 public class ST<Key extends Comparable<Key>, Value> implements Iterable<Key> {
 
