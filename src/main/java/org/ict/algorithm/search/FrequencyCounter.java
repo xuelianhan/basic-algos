33151c29a7038aa9312c8f5fd338fe009e50574f
xuelian.han <xuelian.han@qunar.com>
2018-04-13 14:50:02 +0800

    add FrequencyCounter.java

---
+++ src/main/java/org/ict/algorithm/search/FrequencyCounter.java
@@ -0,0 +1,17 @@
+package org.ict.algorithm.search;
+
+/**
+ *
+ * The {@code FrequencyCounter} class provides a client for 
+ * reading in a sequence of words and printing a word (exceeding
+ * a given length that occurs most frequently.It is useful as 
+ * a test client for various symbol table implementations.
+ * 
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ *
+ */
+public class FrequencyCounter {
+
+
+}
