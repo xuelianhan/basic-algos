9b74227733e15085779d91fedd16fae42f325e80
xuelian.han <xueliansniper@gmail.com>
2018-05-04 20:05:44 +0800

    add codes

--- src/main/java/org/ict/algorithm/search/ST.java
+++ src/main/java/org/ict/algorithm/search/ST.java
@@ -32,17 +32,136 @@ import java.util.TreeMap;
  * The<em>size</em>, and <em>is-empty</em> operations take constant time.
  * Construction takes constant time.
  *
+ * @param <Key> the generic type of keys in this symbol table
+ * @param <Value> the generic type of keys in this symbol table
+ *
  * @author Robert Sedgewick
  * @author Kevin Wayne
  *
  */
 public class ST<Key extends Comparable<Key>, Value> implements Iterable<Key> {
 
-    @Override
-    public Iterator<Key> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+    private TreeMap<Key, Value> st;
+
+    /**
+     * Initializes an empty symbol table.
+     */
+    public ST() {
+        st = new TreeMap<Key, Value>();
     }
 
+    /**
+     * Returns the value associated with the given key in this symbol table.
+     * 
+     * @param key the key
+     * @return the value associated with the given key if the key is in this
+     *         symbol table;
+     *         {@code null} if the key is not in this symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls get() with null key");
+        }
+        return st.get(key);
+    }
+
+    /**
+     * Inserts the specified key-value pair into the symbol table, overwriting the 
+     * old value with the new value if the symbol table already contains the specified
+     * key.Deletes the specified key (and its associated value) from this symbol table
+     * if the specified value is {@code null}
+     */
+    public void put(Key key, Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with null key");
+        }
+        if (val == null) {
+            st.remove(key);
+        } else {
+            st.put(key, val);
+        }
+    }
+
+    /**
+     * Removes the specified key and its associated value from this symbol table
+     * (if the key is in this symbol table).
+     *
+     * @param key the key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void delete(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls delete() with null key");
+        }
+        st.remove(key);
+    }
+
+    /**
+     * Returns true if this symbol table contain the given key.
+     * 
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and 
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls contains() with null key");
+        }
+        return st.containsKey(key);
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return st.size();
+    }
+
+    /**
+     * Returns true if this symbol table is empty.
+     * @return {@code true} if this symbol table is empty and {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Returns all keys in this symbol table.
+     * <p>
+     * To iterate over all of the keys in the symbol table named {@code st}.
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     *
+     * @return all keys in this symbol table
+     */
+    public Iterable<Key> keys() {
+        return st.keySet();
+    }
+
+    /**
+     * @return an iterator to all of the keys in this symbol table
+     * @deprecated replaced by {@link #keys()}.
+     */
+    @Override
+    @Deprecated
+    public Iterator<Key> iterator() {
+        return st.keySet().iterator();
+    }
+
+    /**
+     * Returns the smallest key in this symbol table.
+     * 
+     * @return the smallest key in this symbol table
+     * @throws NoSuchElementException if this symbol table is empty
+     */
+    public Key min() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("calls min() with empty() symbol table");
+        }
+        return st.firstKey();
+    }
 
 }
