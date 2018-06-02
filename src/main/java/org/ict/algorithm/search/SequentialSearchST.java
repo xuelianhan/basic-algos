8dc75e0136db8f72e71be501c6832336754a3d8b
xuelian.han <xueliansniper@gmail.com>
2018-06-02 21:07:43 +0800

    add Node in SequentialSearchST.java

--- src/main/java/org/ict/algorithm/search/SequentialSearchST.java
+++ src/main/java/org/ict/algorithm/search/SequentialSearchST.java
@@ -1,5 +1,16 @@
 package org.ict.algorithm.search;
 
 public class SequentialSearchST<Key, Value> {
+    // number of key-value pairs
+    private int n;
+    // the linked list of key-value pairs
+    private Node first;
 
+    // a helper linked list data type
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node next;
+
+    }
 }
