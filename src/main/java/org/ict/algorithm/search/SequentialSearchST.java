63905d4b6f94e028a99b63cf7a4484c24797b078
xuelian.han <xueliansniper@gmail.com>
2018-06-04 10:18:31 +0800

    add get put delete contains methods in SequentialSearchST.java

--- src/main/java/org/ict/algorithm/search/SequentialSearchST.java
+++ src/main/java/org/ict/algorithm/search/SequentialSearchST.java
@@ -1,5 +1,33 @@
 package org.ict.algorithm.search;
 
+/**
+ * The {@code SequentialSearchST} class represent an (unordered)
+ * symbol table of generic key-value pairs.
+ * It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
+ * <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
+ * It also provides a <em>keys</em> method for iterating over all of the
+ * keys.
+ * A symbol table implements the <em>associative array</em> abstraction:
+ * when associating a value with a key that is already in the symbol table,
+ * the convention is to replace the old value with the new value.
+ * The class also uses the convention that values cannot be {@code null}.
+ * Setting the value associated with a key to {@code null} is equivalent to 
+ * deleting the key from the symbol table.
+ * <p>
+ * This implementation uses a singly-linked list and sequential search.
+ * It relies on the {@code equals()} method to test whether two keys are
+ * equal. It does not call either the {@code compareTo()} or {@code hashCode()}
+ * method.
+ * The <em>put</em> and <em>delete</em> operations take linear time;
+ * the <em>get</em> and <em>contains</em> operations takes linear time
+ * in the worst case.
+ * The <em>size</em>, and <em>is-empty</em> operations take constant time.
+ * Construction takes constant time.
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ *
+ */
 public class SequentialSearchST<Key, Value> {
     // number of key-value pairs
     private int n;
@@ -13,4 +41,131 @@ public class SequentialSearchST<Key, Value> {
         private Node next;
 
     }
+
+    /**
+     * Initializes an empty symbol table.
+     */
+    public SequentialSearchST() {}
+
+    /**
+     * Returns the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Returns true if this symbol table is empty.
+     * @return {@code true} if this symbol table is empty;
+     *         {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Returns true if this symbol table contains the specified key.
+     *
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key};
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+
+    /**
+     * Returns the value associated with the given key in this symbol table.
+     *
+     * @param key the key
+     * @return the value associated with the given key if the key is in the
+     * symbol table and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to get() is null");
+        }
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                return x.value;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Inserts the specified key-value pair into the symbol table, overwriting the old
+     * value with the new value if the symbol table already contains the specified key.
+     * Deletes the specified key (and its associated value) from this symbol table if
+     * the specified value is {@code null}
+     *
+     * @param key the key
+     * @param val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(Key key, Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException("first argument to put() is null");
+        }
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        // if found key, replace old value with new value
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        // if not found key, insert new entry at first
+        first = new Node(key, val, first);
+        n++;
+    }
+
+    /**
+     * Removes the specified key and its associated value from this symbol table
+     * (if the key is in this symbol table)
+     *
+     * @param key the key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void delete(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to delete() is null");
+        }
+        first = delete(first, key);
+    }
+
+    // delete key in linked list beginning at Node x
+    // warning: function call stack too large if table is large
+    private Node delete(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        if (key.equals(x.key)) {
+            n--;
+            return x.next;
+        }
+        x.next = delete(x.next, key);
+        return x;
+    }
+
+    /**
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     *
+     * @return all keys in the symbol table
+     */
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+    }
+
 }
