84a01510e7c1d0712b31eff7454eca505d0595ba
xuelian.han <xueliansniper@gmail.com>
2018-06-11 19:44:50 +0800

    add class description in LinearProbingHashST.java

--- src/main/java/org/ict/algorithm/search/LinearProbingHashST.java
+++ src/main/java/org/ict/algorithm/search/LinearProbingHashST.java
@@ -1,5 +1,14 @@
 package org.ict.algorithm.search;
 
+/**
+ * The {@code LinearProbingHashST} class represents a symbol table of generic
+ * key-value pairs
+ *
+ * It supports the usual <em>put</em>,<em>get</em>,<em>contains</em>,<em>delete</em>,
+ * <em>size</em>,<em>is-empty</em>,<em>keys</em>,<em>associative</em>,<em>put</em>,
+ * <em>contains</em>,
+ * <em>remove</em>,<em>size</em>,<em>put</em>
+ */
 public class LinearProbingHashST {
 
 }
