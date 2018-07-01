4a9a08b37dcc8625af17ce2ca936bf6909cc5f3a
xuelian.han <xueliansniper@gmail.com>
2018-07-01 16:55:37 +0800

    add SET.java and class descriptions in it

---
+++ src/main/java/org/ict/algorithm/search/SET.java
@@ -0,0 +1,25 @@
+/**
+ * Set implementation using Java's TreeSet library
+ * Does not allow duplicates
+ *
+ *
+ */
+package org.ict.algorithm.search;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.TreeSet;
+
+
+/**
+ * The {@code SET} class represents an ordered set of comparable keys
+ * It supports the usual <em>add</em>, <em>contains</em>, and <em>delete</em>
+ * methods.It also provides ordered methods for finding the <em>minimum</em>,
+ * <em>maxmium</em>,<em>floor</em>,<em>ceiling</em>,
+ * <em>add</em>,<em>add</em>,<em>add</em>,<em>add</em>,
+ * <em>add</em>,<em>add</em>,<em>add</em>,<em>add</em>,
+ * <em>add</em>,<em>add</em>,<em>add</em>,<em>add</em>,
+ */
+public class SET<Key extends Comparable<Key>> implements Iterable<Key> {
+
+}
