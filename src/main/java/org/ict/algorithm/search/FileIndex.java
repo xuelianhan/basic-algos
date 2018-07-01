e6e74f32b73bb990fa8f1b6313b4f2b6c982ff48
xuelian.han <xueliansniper@gmail.com>
2018-07-01 19:06:48 +0800

    add FileIndex.java

---
+++ src/main/java/org/ict/algorithm/search/FileIndex.java
@@ -0,0 +1,13 @@
+package org.ict.algorithm.search;
+
+/**
+ * The {@code FileIndex} class provides a client for indexing a set of files,
+ * sepecified as command-line arguments. It takes queries from standard input
+ * and prints each file that contains the given query.
+ * 
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ */
+public class FileIndex {
+
+}
