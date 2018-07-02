0c6109845df0672b37a67edac93374e762798e71
xuelian.han <xueliansniper@gmail.com>
2018-07-02 10:25:18 +0800

    add class descriptions in DeDup.java

--- src/main/java/org/ict/algorithm/search/DeDup.java
+++ src/main/java/org/ict/algorithm/search/DeDup.java
@@ -1,8 +1,49 @@
+/**
+ * $ javac org/ict/algorithm/search/DeDup.java
+ * $ more ../resources/tinyTale.txt 
+ * it was the best of times it was the worst of times 
+ * it was the age of wisdom it was the age of foolishness 
+ * it was the epoch of belief it was the epoch of incredulity 
+ * it was the season of light it was the season of darkness 
+ * it was the spring of hope it was the winter of despair
+ * $ java org/ict/algorithm/search/DeDup < ../resources/tinyTale.txt 
+ * it
+ * was
+ * the
+ * best
+ * of
+ * times
+ * worst
+ * age
+ * wisdom
+ * foolishness
+ * epoch
+ * belief
+ * incredulity
+ * season
+ * light
+ * darkness
+ * spring
+ * hope
+ * winter
+ * despair
+ */
 package org.ict.algorithm.search;
 
 import org.ict.algorithm.util.StdIn;
 import org.ict.algorithm.util.StdOut;
 
+
+/**
+ * 
+ * The {@code DeDup} class provides a client fo reading in a sequence of 
+ * words from standard input and printing each word, removing any duplicates.
+ * It is useful as a test client for various symbol table implementations.
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ *
+ */
 public class DeDup {
     // Do not instantiate.
     private DeDup() {}
