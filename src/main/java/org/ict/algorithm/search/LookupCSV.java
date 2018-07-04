1a5eae92f3e384a5502c2037d40bed37fbae03cf
xuelian.han <xueliansniper@gmail.com>
2018-07-04 09:52:40 +0800

    add codes in LookupCSV.java

--- src/main/java/org/ict/algorithm/search/LookupCSV.java
+++ src/main/java/org/ict/algorithm/search/LookupCSV.java
@@ -9,8 +9,38 @@ import org.ict.algorithm.util.StdOut;
  * key-value pairs from a file; then, printing the values corresponding to the
  * keys found on standard input. Both keys and values are strings.
  * The fields to serve as the key and value are taken as command-line arguments.
- *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
-public class LookupCST {
+public class LookupCSV {
+    // Do not instantiate.
+    private LookupCSV(){}
+    
+    public static void main(String[] args) {
+        int keyField = Integer.parseInt(args[1]);
+        int valField = Integer.parseInt(args[2]);
+
+        //symbol table
+        ST<String, String> st = new ST<String, String>();
+
+        // read in the data from csv file
+        In in = new In(args[0]);
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            String[] tokens = line.split(",");
+            String key = tokens[keyField];
+            String val = tokens[valField];
+            st.put(key, val);
+        }
+
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (st.contains(s)) {
+                StdOut.println(st.get(s));
+            } else {
+                StdOut.println("Not found");
+            }
+        }
+    }
 
 }
