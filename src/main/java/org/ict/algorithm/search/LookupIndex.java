219cd34ff3c26acf8d201814a558d43bcd2f4163
xuelian.han <xueliansniper@gmail.com>
2018-07-04 09:56:12 +0800

    add LookupIndex.java

---
+++ src/main/java/org/ict/algorithm/search/LookupIndex.java
@@ -0,0 +1,11 @@
+package org.ict.algorithm.search;
+
+
+import org.ict.algorithm.fundamentals.Queue;
+import org.ict.algorithm.util.In;
+import org.ict.algorithm.util.StdIn;
+import org.ict.algorithm.util.StdOut;
+
+public class LookupIndex {
+
+}
