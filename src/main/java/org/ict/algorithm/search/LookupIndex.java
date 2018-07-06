a28460e9122cbf84d0196ed3df08898ee94498d8
xuelian.han <xueliansniper@gmail.com>
2018-07-06 09:36:49 +0800

    add codes in LookupIndex.java

--- src/main/java/org/ict/algorithm/search/LookupIndex.java
+++ src/main/java/org/ict/algorithm/search/LookupIndex.java
@@ -6,6 +6,16 @@ import org.ict.algorithm.util.In;
 import org.ict.algorithm.util.StdIn;
 import org.ict.algorithm.util.StdOut;
 
+/**
+ * The {@code LookupIndex} class provides a data-driven client for reading in a 
+ * key-value pairs from a file; then, printing the values corresponding to the
+ * keys found on standard input. Keys are strings; values are lists of strings.
+ * The separating delimiter is taken as a command-line argument. This client is
+ * sometimes known as an <em>inverted index</em>.
+ * 
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ */
 public class LookupIndex {
 
     // Do not instantiate
@@ -18,5 +28,39 @@ public class LookupIndex {
         
         ST<String, Queue<String>> st = new ST<String, Queue<String>>();
         ST<String, Queue<String>> ts = new ST<String, Queue<String>>();
+
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            String[] fields = line.split(separator);
+            String key = fields[0];
+            for (int i = 1; i < fields.length; i++) {
+                String val = fields[i];
+                if (!st.contains(key)) {
+                    st.put(key, new Queue<String>());
+                }
+                if (!ts.contains(val)) {
+                    ts.put(val, new Queue<String>());
+                }
+                st.get(key).enqueue(val);
+                ts.get(val).enqueue(key);
+            }
+        }
+
+        StdOut.println("Done indexing");
+
+        // read queries from standard input, one per line
+        while (!StdIn.isEmpty()) {
+            String query = StdIn.readLine();
+            if (st.contains(query)) {
+                for (String vals : st.get(query)) {
+                    StdOut.println(" " + vals);
+                }
+            }
+            if (ts.contains(query)) {
+                for (String keys.ts.get(query)) {
+                    StdOut.println(" " + keys);
+                }
+            }
+        }
     }
 }
