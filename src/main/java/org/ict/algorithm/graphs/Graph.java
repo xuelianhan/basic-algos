b9cd7775638ffa2010c591c376b296a2f3a79db8
xuelian.han <xueliansniper@gmail.com>
2018-07-16 19:13:28 +0800

    add construct in Graph.java

--- src/main/java/org/ict/algorithm/graphs/Graph.java
+++ src/main/java/org/ict/algorithm/graphs/Graph.java
@@ -10,8 +10,49 @@ import org.ict.algorithm.util.StdOut;
 /**
  * A graph, implemented using an array of sets.
  * Parallel edges and self-loops allowed.
- * The {@code Graph} class
+ * The {@code Graph} class represents an undirected graph of vertices
+ * named 0 through <em>V</em> - 1.
+ * It supports the following two primary operations: add an edge to the graph,
+ * iterate over all of the vertices adjacent to a vertex. It also provides
+ * methods for returning the number of vertices <em>V</em> and the number 
+ * of edges <em>E</em>. Parallel edges and self-loops are permmitted.
+ * By convention, a self-loop <em>v</em>-<em>v</em> appears in the 
+ * adjacency list of <em>v</em> twice and contributes two to the degree
+ * of <em>v</em>.
+ * <p>
+ * This implementation uses an adjacency-lists representation, which
+ * is a vertex-indexed array of {@link Bag} objects.
+ * All operations take constant time (in the worst case) except
+ * iterating over the vertices adjacent to a given vertex, which takes
+ * time proportional to the number of such vertices.
+ * <p>
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ *
  */
 public class Graph {
+    private static final String NEWLINE = System.getProperty("line.separator");
 
+    private final int V;
+
+    private int E;
+
+    private Bag<Integer> adj;
+
+    /**
+     * Initializes an empty graph with {@code V} vertices and 0 edges.
+     * param V the number of vertices
+     * 
+     * @param V number of vertices
+     * @throws IllegalArgumentException if {@code V < 0}
+     */
+    public Graph(int v) {
+        if (V < 0) {
+            throw new IllegalArgumentException("Number of vertices must be nonnegative");
+        }
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[])new Bag[V];
+
+    }
 }
