b0ed6d2e50c70624d4864c73dbfd7ba06a289aa1
xuelian.han <xueliansniper@gmail.com>
2018-07-22 20:16:55 +0800

    add skeleton of IndexMinPQ.java

---
+++ src/main/java/org/ict/algorithm/sort/IndexMinPQ.java
@@ -0,0 +1,13 @@
+package org.ict.algorithm.sort;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ *
+ *
+ */
+public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer> {
+
+
+}
