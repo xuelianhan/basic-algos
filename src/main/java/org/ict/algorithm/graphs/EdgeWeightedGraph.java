6842afb9c1b5f9da705eb343b95e94478f14f525
xuelian.han <xueliansniper@gmail.com>
2018-07-25 19:20:27 +0800

    add codes

--- src/main/java/org/ict/algorithm/graphs/EdgeWeightedGraph.java
+++ src/main/java/org/ict/algorithm/graphs/EdgeWeightedGraph.java
@@ -1,5 +1,14 @@
 package org.ict.algorithm.graph;
 
+
+/**
+ * The {@code EdgeWeightedGraph} class represents an edge-weighted
+ * graph of vertices named 0 through <em>V</em> - 1, where each 
+ * undirected edge is of type {@link Edge} and has a real-valued weight.
+ * It supports the following two primary operations: add an edge to
+ * the graph,
+ *
+ */
 public class EdgeWeightedGraph {
     private static final String NEWLINE = System.getProperty("line.separator");
 
