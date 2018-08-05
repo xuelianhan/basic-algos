6b5604d2734730526a3e4eb7af501a5da042dcad
xuelian.han <xueliansniper@gmail.com>
2018-08-05 11:35:09 +0800

    complete BreadthFirstPaths.java

--- src/main/java/org/ict/algorithm/graphs/BreadthFirstPaths.java
+++ src/main/java/org/ict/algorithm/graphs/BreadthFirstPaths.java
@@ -1,11 +1,41 @@
 package org.ict.algorithm.graphs;
 
+import org.ict.algorithm.fundamentals.Queue;
 import org.ict.algorithm.fundamentals.Stack;
 import org.ict.algorithm.util.In;
 import org.ict.algorithm.util.StdOut;
 
 
 /**
+ * $ javac org/ict/algorithm/graphs/BreadthFirstPaths.java 
+ * $ cat ../resources/tinyCG.txt 
+ * 6
+ * 8
+ * 0 5
+ * 2 4
+ * 2 3
+ * 1 2
+ * 0 1
+ * 3 4
+ * 3 5
+ * 0 2
+ * $ java org/ict/algorithm/graphs/Graph ../resources/tinyCG.txt 
+ * 6 vertices, 8 edges 
+ * 0: 2 1 5 
+ * 1: 0 2 
+ * 2: 0 1 3 4 
+ * 3: 5 4 2 
+ * 4: 3 2 
+ * 5: 3 0 
+ *
+ * $ java org/ict/algorithm/graphs/BreadthFirstPaths ../resources/tinyCG.txt 0
+ * 0 to 0: 0
+ * 0 to 1: 0-1
+ * 0 to 2: 0-2
+ * 0 to 3: 0-2-3
+ * 0 to 4: 0-2-4
+ * 0 to 5: 0-5
+ *
  * The {@code BreadthFirstPaths} class represents a data type for finding
  * shortest paths (number of edges) from a source vertex <em>s</em>
  * (or a set of source vertices) 
@@ -37,9 +67,87 @@ public class BreadthFirstPaths {
     private int[] distTo;
 
     /**
+     * Computes the shortest path between the source vertex {@code s}
+     * and every other vertex in the graph {@code G}.
      *
+     * @param G the graph
+     * @param s the source vertex
+     * @throws IllegalArgumentException unless {@code 0 <= s < V}
+     */
+    public BreadthFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        distTo = new int[G.V()];
+        validateVertex(s);
+        bfs(G, s);
+        //assert check(G, s);
+    }
+
+    /**
+     * Computes the shortest path between any one of the source vertices in {@code sources}
+     * and every other vertex in graph {@code G}.
+     *
+     * @param G the graph
+     * @param sources the source vertices
+     * @throws IllegalArgumentException unless {@code 0 <= s < V} for each vertex
+     *         {@code s} in  {@code sources}
      *
      */
+    public BreadthFirstPaths(Graph G, Iterable<Integer> sources) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        distTo = new int[G.V()];
+        for (int v = 0; v < G.V(); v++) {
+            distTo[v] = INFINITY;
+        }
+        validateVertices(sources);
+        bfs(G, sources); 
+    }
+
+    // breadth-first search from a single source
+    private void bfs(Graph G, int s) {
+        Queue<Integer> q = new Queue<Integer>();
+        for (int v = 0; v < G.V(); v++) {
+            distTo[v] = INFINITY;
+        }
+        distTo[s] = 0;
+        marked[s] = true;
+        q.enqueue(s);
+        
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    q.enqueue(w);
+                }
+            }
+        }
+    }
+
+    // breadth-first search from multiple sources
+    private void bfs(Graph G, Iterable<Integer> sources) {
+        Queue<Integer> q = new Queue<Integer>();
+        for (int s : sources) {
+            marked[s] = true;
+            distTo[s] = 0;
+            q.enqueue(s);
+        }
+
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    q.enqueue(w);
+                }
+            }
+        }
+    }
 
     /**
      * Returns the number of edges in a shortest path between the source vertex {@code s} 
@@ -94,6 +202,7 @@ public class BreadthFirstPaths {
 
     // check that v = edgeTo[w] satisfies distTo[w] = distTo[v] + 1 
     // provided v is reachable from s
+    //private boolean check(Graph G, int s);
 
     // throw an IllegalArgumentException unless {@code 0 <= v < V}
     private void validateVertex(int v) {
