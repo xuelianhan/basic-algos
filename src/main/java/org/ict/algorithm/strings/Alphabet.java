42b01991ef986af957679f2fa1288f1ecedfce76
xuelian.han <xueliansniper@gmail.com>
2018-08-08 19:50:56 +0800

    add skeleton of Alphabet.java

---
+++ src/main/java/org/ict/algorithm/strings/Alphabet.java
@@ -0,0 +1,12 @@
+package org.ict.algorithm.strings;
+
+
+public class Alphabet {
+    /**
+     * The binary alphabet {0, 1}.
+     */
+    public static final Alphabet BINARY = new Alphabet("01");
+
+
+
+}
