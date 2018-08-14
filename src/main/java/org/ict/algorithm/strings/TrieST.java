fecfe3ae4aa11e4a8302d222557f18d1b31f7ca7
xuelian.han <xueliansniper@gmail.com>
2018-08-14 10:21:48 +0800

    add TrieST.java

---
+++ src/main/java/org/ict/algorithm/strings/TrieST.java
@@ -0,0 +1,27 @@
+package org.ict.algorithm.strings;
+
+
+/**
+ * The {@code TrieST} class represents an symbol table of key-value 
+ * pairs, with string keys and generic values.
+ * It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
+ * <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
+ * It also provides character-based methods for finding the string
+ * in the symbol table that is the <em>longest prefix</em> of a given
+ * prefix, finding all strings in the symbol table that <em>start with</em>
+ * a given prefix, and finding all strings in the symbol table that
+ * <em>match</em> a given pattern.
+ * A symbol table implements the <em>associative array</em> abstraction:
+ * when associating a value with a key that is already in the symbol table,
+ * the convention is to replace the old value with the new value.
+ * Unlike {@link java.util.Map}, this class uses the convention that values
+ * cannot be {@code null}---setting the value associated with a key to 
+ * {@code null} is equivalent to deleting the key from the symbol table.
+ * <p>
+ *
+ *
+ *
+ */
+public class TrieST {
+
+}
