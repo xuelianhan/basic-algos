0ff272e4945e96033904a7f9c72b3d970af9a080
xuelian.han <xueliansniper@gmail.com>
2018-08-15 13:57:37 +0800

    add get and put method in TrieST.java

--- src/main/java/org/ict/algorithm/strings/TrieST.java
+++ src/main/java/org/ict/algorithm/strings/TrieST.java
@@ -1,6 +1,8 @@
 package org.ict.algorithm.strings;
 
-
+import org.ict.algorithm.fundamentals.Queue;
+import org.ict.algorithm.util.StdIn;
+import org.ict.algorithm.util.StdOut;
 /**
  * The {@code TrieST} class represents an symbol table of key-value 
  * pairs, with string keys and generic values.
@@ -18,10 +20,165 @@ package org.ict.algorithm.strings;
  * cannot be {@code null}---setting the value associated with a key to 
  * {@code null} is equivalent to deleting the key from the symbol table.
  * <p>
- *
- *
- *
+ * This implementation uses a 256-way trie.
+ * The <em>put</em>, <em>contains</em>, <em>delete</em>, and 
+ * <em>longest prefix</em> operations take time proportional to the length
+ * of the key (in the worst case). Construction takes constant time.
+ * The <em>size</em>, and <em>is-empty</em> operations take constant time.
+ * Construction takes constant time.
+ * 
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
-public class TrieST {
+public class TrieST<Value> {
+    // extended ASCII
+    private static final int R = 256; 
 
+    // root of trie
+    private Node root;
+
+    //number of keys in trie
+    private int n;
+
+    // R-way trie node
+    private static class Node {
+        private Object val;
+        private Node[] next = new Node[R];
+    }
+
+    /**
+     * Initializes an empty string symbol table.
+     */
+    public TrieST() {}
+
+    /**
+     * Returns the value associated with the given key.
+     * @param key the key
+     * @return the value associated with the given key if the key is in the 
+     * symbol table and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to get() is null");
+        }
+        Node x = get(root, key, 0);
+        return (x == null ? null :(Value)x.val);
+    }
+
+    /**
+     * Does this symbol table contains the given key?
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(String key) { 
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+
+    private Node get(Node x, String key, int d) {
+        if (x == null) {
+            return null;
+        }
+        if (d == key.length()) {
+            return x;
+        }
+        char c = key.charAt(d);
+        return get(x.next[c], key, d+1);
+    }
+
+    /**
+     * Inserts the key-value pair into the symbol table, overwriting the old value
+     * with the new value if the key is already in the symbol table.
+     * If the value is {@code null}, this effectively deletes the key from the symbol
+     * table.
+     * @param key the key
+     * @param val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(String key, Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to put() is null");
+        }
+        if (val == null) {
+            delete(key);
+        } else {
+            root = put(root, key, val, 0);
+        }
+    }
+
+    private Node put(Node x, String key, Value val, int d) {
+       if (x == null) {
+            x = new Node();
+       }
+       if (d == key.length()) {
+            if (x.val == null) {
+                n++;
+            }
+            x.val = val;
+            return x;
+       }
+       char c = key.charAt(d);
+       x.next[c] = put(x.next[c], key, val, d+1);
+       return x;
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Is this symbol table empty?
+     * @return {@code true} if this symbol table is empty and
+     *         {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     * @return all keys in the symbol table as an {@code Iterable}
+     */
+    public Iterable<String> keys() {
+        return keysWithPrefix("");
+    }
+
+    /**
+     * Returns all of the keys in the set that start with {@code prefix}.
+     *
+     * @param prefix the prefix
+     * @return all of the keys in the set that start with {@code prefix},
+     *         as an iterable
+     */
+    public Iterable<String> keysWithPrefix(String prefix) {
+        Queue<String> results = new Queue<String>();
+        Node x = get(root, prefix, 0);
+        collect(x, new StringBuilder(prefix), results);
+        return results;
+    }
+    
+    private void collect(Node x, StringBuilder prefix, Queue<String> results) {
+        if (x == null) {
+            return;
+        }
+        if (x.val != null) {
+            results.enqueue(prefix.toString());
+        }
+        for (char c = 0; c < R; c++) {
+            prefix.append(c);
+            collect(x.next[c], prefix, results);
+            prefix.deleteCharAt(prefix.length() - 1);
+        }
+    }
 }
