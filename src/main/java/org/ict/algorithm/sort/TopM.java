6d909c55962f12c0b55b4f031b294fdfa89ca291
xuelianhan <xueliansniper@gmail.com>
2019-01-15 23:43:01 +0800

    add constructor with String parameter in Transaction.java

--- src/main/java/org/ict/algorithm/sort/TopM.java
+++ src/main/java/org/ict/algorithm/sort/TopM.java
@@ -1,7 +1,15 @@
 package org.ict.algorithm.sort;
 
+import org.ict.algorithm.fundamentals.Stack;
+import org.ict.algorithm.fundamentals.Transaction;
+import org.ict.algorithm.util.StdIn;
+import org.ict.algorithm.util.StdOut;
 
 /**
+ * Given an integer m from the command line and an input stream where
+ * each line contains a String and a long value, this MinPQ client 
+ * prints the m lines whose numbers are the highest.
+ * 
  * The {@code TopM} class provides a client that reads a sequence of 
  * transactions from standard input and prints the <em>m</em> largest
  * ones to standard output. This implementation uses a {@link MinPQ} 
@@ -26,6 +34,30 @@ public class TopM {
 	 * @param args the command-line arguments
 	 */
 	public static void main(String[] args) {
+		int m = Integer.parseInt(args[0]);
+		MinPQ<Transaction> pq = new MinPQ<Transaction>(m+1);
+		
+		while (StdIn.hasNextLine()) {
+			// Create an entry from the next line and put on the pq
+			String line = StdIn.readLine();
+			Transaction transaction = new Transaction(line);
+			pq.insert(transaction);
+			
+			// remove minimum if m+1 entries on the PQ
+			if (pq.size() > m) {
+				pq.delMin();
+			}
+			//top m entries are on the pq
+		}
+		
+		// print the entries on pq in reverse order
+		Stack<Transaction> stack = new Stack<Transaction>();
+		for (Transaction transaction : pq) {
+			stack.push(transaction);
+		}
+		for (Transaction transaction : stack) {
+			StdOut.println(transaction);
+		}
 		
 	}
 			
