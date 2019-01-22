51717c6e2ef9eb6f55b5f57e5b99bfa7c4dd1049
xuelianhan <xueliansniper@gmail.com>
2019-01-22 12:35:38 +0800

    fix bugs of inplementation of SpiralMatrix.java

--- src/main/java/org/ict/algorithm/leetcode/array/SpiralMatrix.java
+++ src/main/java/org/ict/algorithm/leetcode/array/SpiralMatrix.java
@@ -1,6 +1,7 @@
 package org.ict.algorithm.leetcode.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -30,10 +31,50 @@ import java.util.List;
  */
 
 public class SpiralMatrix {
+	
+	public static void main(String[] args) {
+		int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9,10,11,12}};
+		List<Integer> result = spiralOrderV1(matrix);
+		System.out.println(result);
+	}
+	
 
-	public List<Integer> spiralOrder(int[][] matrix) {
+	public static List<Integer> spiralOrderV1(int[][] matrix) {
 		List<Integer> result = new ArrayList<>();
-		
+		if (matrix.length == 0) {
+			return result;
+		}
+		int rowBegin = 0, rowEnd = matrix.length - 1;
+		int colBegin = 0, colEnd = matrix[0].length - 1;
+		while (rowBegin <= rowEnd && colBegin <= colEnd) {
+			// Traverse Right
+			for (int i = colBegin; i <= colEnd; i++) {
+				result.add(matrix[rowBegin][i]);
+			}
+			rowBegin++;
+			
+			// Traverse Down
+			for (int i = rowBegin; i <= rowEnd; i++) {
+				result.add(matrix[i][colEnd]);
+			}
+			colEnd--;
+			
+			// Traverse left
+			if (rowBegin <= rowEnd) {
+				for (int i = colEnd; i>= colBegin; i--) {
+					result.add(matrix[rowEnd][i]);
+				}
+			}
+			rowEnd--;
+			
+			// Traverse Up
+			if (colBegin <= colEnd) {
+				for (int i = rowEnd; i >= rowBegin; i--) {
+					result.add(matrix[i][colBegin]);
+				}
+			}
+			colBegin++;
+		}
 		return result;
 	}
 			
