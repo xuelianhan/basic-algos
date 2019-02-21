af7505ebdabf4d9407f9bc9cfaae56eabe7696c4
xuelianhan <xueliansniper@gmail.com>
2019-02-21 23:45:50 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/leetcode/breadthfirstsearch/SymmetricTree.java
@@ -0,0 +1,40 @@
+package org.ict.algorithm.leetcode.breadthfirstsearch;
+
+/**
+ * 
+ *Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+ *
+ */
+public class SymmetricTree {
+	
+	public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+
+	/**
+	 * Definition for a binary tree node.
+	 * }
+	 */
+	 public class TreeNode {
+	    int val;
+	    TreeNode left;
+	    TreeNode right;
+	    TreeNode(int x) { val = x; }
+	}
+}
