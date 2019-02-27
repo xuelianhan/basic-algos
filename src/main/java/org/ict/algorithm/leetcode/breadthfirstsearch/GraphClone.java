bbc5ead0395a8c13773f7c217fa938ea934d5534
xuelianhan <xueliansniper@gmail.com>
2019-02-27 23:31:23 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/breadthfirstsearch/GraphClone.java
+++ src/main/java/org/ict/algorithm/leetcode/breadthfirstsearch/GraphClone.java
@@ -15,6 +15,10 @@ import java.util.Queue;
  * {"$id":"1","neighbors":[{"$id":"2","neighbors":[{"$ref":"1"},
  * {"$id":"3","neighbors":[{"$ref":"2"},
  * {"$id":"4","neighbors":[{"$ref":"3"},{"$ref":"1"}],"val":4}],"val":3}],"val":2},{"$ref":"4"}],"val":1}
+ * 1-------2
+ * |       |
+ * |       |
+ * 4-------3
  * Explanation:
  * Node 1's value is 1, and it has two neighbors: Node 2 and 4.
  * Node 2's value is 2, and it has two neighbors: Node 1 and 3.
