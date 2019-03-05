d87b203a788bae2434670b9b295fcd6dee3d4f7a
xuelianhan <xueliansniper@gmail.com>
2019-03-05 11:32:09 +0800

    change comments

--- src/main/java/org/ict/algorithm/leetcode/breadthfirstsearch/GraphClone.java
+++ src/main/java/org/ict/algorithm/leetcode/breadthfirstsearch/GraphClone.java
@@ -13,8 +13,10 @@ import java.util.Queue;
  * Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
  * Input:
  * {"$id":"1","neighbors":[{"$id":"2","neighbors":[{"$ref":"1"},
- * {"$id":"3","neighbors":[{"$ref":"2"},
- * {"$id":"4","neighbors":[{"$ref":"3"},{"$ref":"1"}],"val":4}],"val":3}],"val":2},{"$ref":"4"}],"val":1}
+ *                                                 {"$id":"3","neighbors":[{"$ref":"2"},
+ *                         {"$id":"4","neighbors":[{"$ref":"3"},
+ *                                                 {"$ref":"1"}],"val":4}],"val":3}],"val":2},{"$ref":"4"}],
+ *  "val":1}
  * 1-------2
  * |       |
  * |       |
