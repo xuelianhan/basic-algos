90884f71f1bf2c9d439ad1da6c8a951cf7c34825
xuelian.han <xueliansniper@gmail.com>
2019-06-15 09:58:47 +0800

    add codes

--- src/main/java/cn/edu/pku/pattern/builder/GuavaFunctionTransform.java
+++ src/main/java/cn/edu/pku/pattern/builder/GuavaFunctionTransform.java
@@ -2,15 +2,23 @@ package cn.edu.pku.pattern.builder;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Ordering;
+import com.google.common.collect.Sets;
 
 /**
  * @see https://www.baeldung.com/guava-functions-predicates
@@ -88,5 +96,90 @@ public class GuavaFunctionTransform {
 		List<Integer> numbers = Lists.newArrayList(1, 2, 3);
 		List<String> asStrings = Lists.transform(numbers, Functions.toStringFunction());
 		System.out.println(asStrings);
+	}
+	
+	/**
+	 * sort collection by first applying an intermediary function
+	 */
+	public static void test6() {
+		List<Integer> numbers = Arrays.asList(2, 1, 11, 100, 8, 14);
+		Ordering<Object> ordering = Ordering.natural().onResultOf(Functions.toStringFunction());
+		List<Integer> inAlphabeticalOrder = ordering.sortedCopy(numbers);
+		List<Integer> correctAlphabeticalOrder = Lists.newArrayList(1, 100, 11, 14, 2, 8);
+		System.out.println(inAlphabeticalOrder);
+		System.out.println(correctAlphabeticalOrder);
+	}
+	
+	/**
+	 * complex example – chaining predicates and functions
+	 */
+	public static void test7() {
+		List<Integer> numbers = Arrays.asList(2, 1, 11, 100, 8, 14);
+		Predicate<Integer> acceptEvenNumber = new Predicate<Integer>() {
+		    @Override
+		    public boolean apply(Integer number) {
+		        return (number % 2) == 0;
+		    }
+		};
+		Function<Integer, Integer> powerOfTwo = new Function<Integer, Integer>() {
+		    @Override
+		    public Integer apply(Integer input) {
+		        return (int) Math.pow(input, 2);
+		    }
+		};
+		 
+		FluentIterable<Integer> powerOfTwoOnlyForEvenNumbers = 
+		FluentIterable.from(numbers).filter(acceptEvenNumber).transform(powerOfTwo);
+		System.out.println(powerOfTwoOnlyForEvenNumbers);
+	}
+	
+	/**
+	 * compose two functions
+	 */
+	public static void test8() {
+		List<Integer> numbers = Arrays.asList(2, 3);
+		Function<Integer, Integer> powerOfTwo = new Function<Integer, Integer>() {
+		    @Override
+		    public Integer apply(Integer input) {
+		        return (int) Math.pow(input, 2);
+		    }
+		};
+		List<Integer> result = Lists.transform(numbers, Functions.compose(powerOfTwo, powerOfTwo));
+		System.out.println(result);
+	}
+	
+	/**
+	 * create a Map backed by a Set and a Function
+	 */
+	public static void test9() {
+		Function<Integer, Integer> powerOfTwo = new Function<Integer, Integer>() {
+		    @Override
+		    public Integer apply(Integer input) {
+		        return (int) Math.pow(input, 2);
+		    }
+		};
+		Set<Integer> lowNumbers = Sets.newHashSet(2, 3, 4);
+		 
+		Map<Integer, Integer> numberToPowerOfTwoMuttable = Maps.asMap(lowNumbers, powerOfTwo);
+		Map<Integer, Integer> numberToPowerOfTwoImuttable = Maps.toMap(lowNumbers, powerOfTwo);
+		System.out.println(numberToPowerOfTwoMuttable);
+		System.out.println(numberToPowerOfTwoImuttable);
+	}
+	
+	
+	/**
+	 * create a Function out of a Predicate
+	 */
+	public static void test10() {
+		List<Integer> numbers = Lists.newArrayList(1, 2, 3, 6);
+		Predicate<Integer> acceptEvenNumber = new Predicate<Integer>() {
+		    @Override
+		    public boolean apply(Integer number) {
+		        return (number % 2) == 0;
+		    }
+		};
+		Function<Integer, Boolean> isEventNumberFunction = Functions.forPredicate(acceptEvenNumber);
+		List<Boolean> areNumbersEven = Lists.transform(numbers, isEventNumberFunction);
+		System.out.println(areNumbersEven);
 	}
 }
