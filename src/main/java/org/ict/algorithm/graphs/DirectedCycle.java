aba0e2b4a43dbcfbb9f62df2d779b079df073460
xuelian.han <xueliansniper@gmail.com>
2019-10-17 19:09:18 +0800

    add WordLadder comment

--- src/main/java/org/ict/algorithm/graphs/DirectedCycle.java
+++ src/main/java/org/ict/algorithm/graphs/DirectedCycle.java
@@ -1,10 +1,31 @@
 package org.ict.algorithm.graphs;
 
+import org.ict.algorithm.fundamentals.Stack;
+
 public class DirectedCycle {
 
 	// marked[v] = has vertex v been marked?
 	private boolean[] marked;
-	// 
+	// edgeTo[v] = previous vertex on path to v
 	private int[] edgeTo;
+	// onStack[v] = is vertex on the stack?
+	private boolean[] onStack;
+	// directed cycle(or null if no such cycle)
+	private Stack<Integer> cycle;
 	
+	public DirectedCycle(Digraph G) {
+		marked  = new boolean[G.V()];
+		onStack = new boolean[G.V()];
+		edgeTo  = new int[G.V()];
+		for (int v = 0; v < G.V(); v++) {
+			if(!marked[v] && cycle == null) {
+				dfs(G, v);
+			}
+		}
+	}
+	
+	// check that algorithm computes either the topological order or finds a directed cycle
+	private void dfs(Digraph G, int v) {
+		
+	}
 }
--- src/test/java/org/ict/util/HashSetTest.java
+++ src/test/java/org/ict/util/HashSetTest.java
@@ -7,6 +7,33 @@ import org.junit.Test;
 import com.google.common.collect.Sets;
 
 public class HashSetTest {
+	
+	@Test
+	public void testDiff() {
+		Set<Long> oldSearchResult = Sets.newHashSet();
+		Set<Long> newSearchResult = Sets.newHashSet();
+		
+		for(long i = 0; i < 3; i++) {
+			oldSearchResult.add(i);
+			newSearchResult.add(i);
+		}
+		for(long i = 3; i < 6; i++) {
+			oldSearchResult.add(i);
+			
+		}
+		
+		for(long i = 6; i < 9; i++) {
+			newSearchResult.add(i);
+		}
+		System.out.println("oldSearchResult:" + oldSearchResult);
+		System.out.println("newSearchResult:" + newSearchResult);
+		
+		Set<Long> set1 = Sets.difference(oldSearchResult, newSearchResult);
+		Set<Long> set2 = Sets.difference(newSearchResult, oldSearchResult);
+		
+		System.out.println("set1:" + set1);
+		System.out.println("set2:" + set2);
+	}
 
 	@Test
 	public void testSet() {
