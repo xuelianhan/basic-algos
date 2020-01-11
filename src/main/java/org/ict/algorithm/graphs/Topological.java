2b9e27caf525d29e9eb06984fab4c728bfea2c37
xuelianhan <xueliansniper@gmail.com>
2020-01-11 21:01:43 +0800

    add codes in Topological.java

--- src/main/java/org/ict/algorithm/graphs/Topological.java
+++ src/main/java/org/ict/algorithm/graphs/Topological.java
@@ -65,5 +65,68 @@ public class Topological {
 	 */
 	public Topological(EdgeWeightedDigraph G) {
 		EdgeWeightedDirectedCycle finder = new EdgeWeightedDirectedCycle(G);
+        if (!finder.hasCycle()) {
+            DepthFirstOrder dfs = new DepthFirstOrder(G);
+            order = dfs.reversePost();
+        }
 	}
+
+    /**
+     * Returns a topological order if the digraph has a topological order,
+     * and {@code null} otherwise.
+     * @return a topological order of the vertices (as an interable) if the
+     *         digraph has a topological order (or equivalently, if the digraph is a DAG),
+     *         and {@code null) otherwise
+     */
+    public Iterable<Integer> order() {
+        return order;
+    }
+
+    /**
+     * Does the digraph have a topological order?
+     * @return {@code true} if the digraph has a topological order(or equivalently,
+     *                      if the digraph is a DAG), and {@code false} otherwise
+     */
+    public boolean hasOrder() {
+        return order != null;
+    }
+    
+    /**
+     * The rank of vertex{@code v} in the topological order;
+     * -1 if the digraph is not a DAG
+     *
+     * @param v the vertex
+     * @return the position of vertex {@code v} in a topological order
+     *         of the digraph;
+     *         -1 if the digraph is not a DAG
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int rank(int v) {
+        validateVertex(v);
+        if (hasOrder()) {
+            return rank[v];
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Throw an IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    private void validateVertex(int v) {
+        int V = rank.length;
+        if (v < 0 || v >= V) {
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+        }
+    }
+
+    public static void main(String[] args) {
+        String filename = args[0];
+        String delimiter = args[1];
+        SymboDigraph sg = new SymbolDigraph(filename, delimiter);
+        Topological topological = new Topological(sg.digraph());
+        for (int v : topological.order()) {
+            StdOut.println(sg.nameOf(v));
+        }
+    }
 }
