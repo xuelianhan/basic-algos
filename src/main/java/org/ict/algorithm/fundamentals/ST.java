90fa8a084f2e8a3d9c26a8e6f26a7610a8d22a57
xuelianhan <xueliansniper@gmail.com>
2020-01-12 17:28:29 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/fundamentals/ST.java
@@ -0,0 +1,25 @@
+package org.ict.algorithm.fundamentals;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.TreeMap;
+
+/**
+ * The {@code ST} class represents an ordered symbol table of generic key-value pairs.
+ * It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
+ * <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
+ * It also provides ordered methods for finding the <em>minimum</em>,
+ * <em>maximum</em>, <em>floor</em>, and <em>ceiling</em>.
+ * It also provides a <em>keys</em> method for iterating over all of the keys.
+ * A symbol table implements the </em>associative array</em> abstraction:
+ * when associating a value with a key that is already in the symbol table,
+ * the convention is to replace the old value with the new value.
+ * Unlike {@link java.util.Map}, this class uses the convention that
+ * values cannot be {@code null}---setting the value associated with a key
+ * to {@code null} is equivalent to deleting the key from the symbol table.
+ * <p>
+ * It requires that the key type implements the {@code Comparable} interface and calls the
+ * {@code compareTo()} and method to compare two keys. It does not call either
+ * {@code equals()} or {@code hashCode()}.
+ *
+ */
