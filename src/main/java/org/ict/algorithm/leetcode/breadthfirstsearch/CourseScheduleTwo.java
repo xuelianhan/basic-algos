0124c7acc37c2b5bf4b5c118bdb596eadfd05c1a
xuelianhan <xueliansniper@gmail.com>
2020-02-23 14:32:46 +0800

    add PerfectSquares.java

--- src/main/java/org/ict/algorithm/leetcode/breadthfirstsearch/CourseScheduleTwo.java
+++ src/main/java/org/ict/algorithm/leetcode/breadthfirstsearch/CourseScheduleTwo.java
@@ -7,6 +7,8 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 /**
+ * LC210 
+ *
  * There are a total of n courses you have to take, labeled from 0 to n-1.
  * Some courses may have prerequisites, for example to take course 0 you have to first take
  * course 1, which is expressed as a pair:[0, 1]
---
+++ src/main/java/org/ict/algorithm/leetcode/breadthfirstsearch/PerfectSquares.java
@@ -0,0 +1,6 @@
+package org.ict.algorithm.leetcode.breadthfirstsearch;
+
+public class PerfectSquares {
+
+
+}
