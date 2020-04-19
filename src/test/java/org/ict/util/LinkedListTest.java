f6a3cdce997b063791682d22009a9d9b9c5a73f5
xuelian.han <xueliansniper@gmail.com>
2020-04-19 16:57:57 +0100

    add codes

--- src/test/java/org/ict/util/LinkedListTest.java
+++ src/test/java/org/ict/util/LinkedListTest.java
@@ -3,11 +3,27 @@ package org.ict.util;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class LinkedListTest {
 
 	public static void main(String[] args) {
+		testForeach();
+		//testRemove();
+	} 
+	
+	public static void testForeach() {
+		List<Integer> numList = Arrays.asList(10,21,31,40,59,60);
+		numList.forEach( x-> {
+		    if( x%2 == 0) {
+		        return; // only skips this iteration.
+		    }
+		    System.out.println(x);
+		});
+	}
+	
+	public static void testRemove() {
 		Queue<Integer> list = new LinkedList<Integer>();
 		for (int i = 0; i < 5; i++) {
 			list.add(i);
@@ -24,5 +40,5 @@ public class LinkedListTest {
 		Integer[] a = new Integer[list.size()];
 		list.toArray(a);
 		System.out.println(Arrays.toString(a));
-	} 
+	}
 }
