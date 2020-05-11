ca25864c7cc48d1cc064e0fc639727b9b9b5baf8
xuelian.han <xueliansniper@gmail.com>
2020-05-11 03:38:03 +0100

    add TrappingRainWaterTwo.java with empty content

---
+++ src/main/java/org/ict/algorithm/leetcode/breadthfirstsearch/TrappingRainWaterTwo.java
@@ -0,0 +1,28 @@
+package org.ict.algorithm.leetcode.breadthfirstsearch;
+
+/**
+ * 
+ * Given an m x n matrix of positive integers representing the height of each unit cell in a 2D elevation map, 
+ * compute the volume of water it is able to trap after raining.
+ * Example:
+ * 
+ * Given the following 3x6 height map:
+ * [
+ *   [1,4,3,1,3,2],
+ *   [3,2,1,3,2,4],
+ *   [2,3,3,2,3,1]
+ * ]
+ * 
+ * Return 4.
+ * 
+ * Constraints:
+ * 1 <= m, n <= 110
+ * 0 <= heightMap[i][j] <= 20000
+ *
+ */
+public class TrappingRainWaterTwo {
+
+	public int trapRainWater(int[][] heightMap) {
+        return 0;
+    }
+}
