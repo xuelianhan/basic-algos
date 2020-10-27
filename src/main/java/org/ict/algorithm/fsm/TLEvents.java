ea368f5a8f4794ddc5b0c105c7972f5125cc803b
xuelian.han <xueliansniper@gmail.com>
2020-10-27 16:28:57 +0100

    add codes

--- src/main/java/org/ict/algorithm/fsm/TLEvents.java
+++ src/main/java/org/ict/algorithm/fsm/TLEvents.java
@@ -1,11 +1,11 @@
 package org.ict.algorithm.fsm;
 
 public enum TLEvents {
-	T("task正常外呼事件"),
-	L("Listener消息监听事件"),
-	TR("task终结外呼事件"),
-	TSR("task语音识别事件"),
-	TF("task结果通知事件");
+	T("task callout event"),
+	L("listener for message event"),
+	TR("task end callout event"),
+	TSR("task speech recognize event"),
+	TF("task result notify event");
 	
 	private TLEvents(String name) {
 	    this.name = name;
