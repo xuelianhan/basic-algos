1c3716b7562446cf7b1a0111010aae6124cd3510
xueliansniper <xueliansniper@gmail.com>
2020-11-18 00:13:54 +0800

    add sha256 rsa signature algorithms

--- src/main/java/org/ict/algorithm/util/RSAUtil.java
+++ src/main/java/org/ict/algorithm/util/RSAUtil.java
@@ -61,6 +61,30 @@ public class RSAUtil {
         System.out.println("decrypt content:" + decryptStr);
     }
 
+    public static void verifySign() throws Exception {
+        //original data
+        String data = "hello world";
+        //Signature with baes64 encoded
+        String b64sig = "Oebbp754ozaX+/539cRYomsZSOVhZ8L7NcNbLIw+hUWn0HMLvYjmK/B78ixMMQD+vk1zB6RRo2kyYeqoWka+FQ==";
+        //public key with base64 encoded
+        String b64pubkey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAI5SXpw1SSsM3FN43JVKn4gb+oGXfjL7rCDluqydAyHZ8vV7ySqi8oM1CoHRC9U2ST7IldydsQ+4cjC9xfzexxcCAwEAAQ==";
+        //transform the base64 string to byte[]
+        byte[] pk = Base64.getDecoder().decode(b64pubkey);
+        byte[] sig = Base64.getDecoder().decode(b64sig);
+        System.out.println(new String(sig));
+        //get PublicKey Object from byte[]
+        X509EncodedKeySpec spec = new X509EncodedKeySpec( pk );
+        KeyFactory kf = KeyFactory.getInstance( "RSA" );
+        PublicKey pubKey = kf.generatePublic( spec );
+        //verify the Signature
+        Signature signature = Signature.getInstance( "SHA256withRSA" );
+        signature.initVerify( pubKey );
+        signature.update( data.getBytes() );
+        boolean ret = signature.verify( sig );
+        System.out.println(ret);
+        return;
+    }
+
     public static Map<String, String> generateSignKeyPair() throws Exception {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         keyGen.initialize(512);
