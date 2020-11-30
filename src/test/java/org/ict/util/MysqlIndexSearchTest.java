bb9ec76a56429bf7cb7038af2255597fb51fe9d2
xueliansniper <xueliansniper@gmail.com>
2020-11-30 15:28:35 +0800

    mysql number type faster than character index

---
+++ src/test/java/org/ict/util/MysqlIndexSearchTest.java
@@ -0,0 +1,8 @@
+package org.ict.util;
+
+/**
+ * https://dba.stackexchange.com/questions/137945/indexes-integer-vs-string-performance-if-the-number-of-nodes-is-the-same
+ */
+public class MysqlIndexSearchTest {
+    
+}
