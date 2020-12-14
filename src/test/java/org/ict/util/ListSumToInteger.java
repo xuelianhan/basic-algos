a79cf9d25a2cbdc83e760d197ef32cf70f66caf3
xueliansniper <xueliansniper@gmail.com>
2020-12-14 13:46:35 +0800

    add List sum to Int example

---
+++ src/test/java/org/ict/util/ListSumToInteger.java
@@ -0,0 +1,9 @@
+package org.ict.util;
+
+/**
+ * https://stackoverflow.com/questions/23110853/java8-sum-values-from-specific-field-of-the-objects-in-a-list
+ * https://www.websparrow.org/java/java-8-stream-sum-of-list-of-integers
+ */
+public class ListSumToInteger {
+
+}
