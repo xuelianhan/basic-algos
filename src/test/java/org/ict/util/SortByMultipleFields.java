31ab5c872a1659b580acf48e73f877d9bc9df988
xueliansniper <xueliansniper@gmail.com>
2020-12-15 02:08:59 +0800

    add sort by multiple fields example

---
+++ src/test/java/org/ict/util/SortByMultipleFields.java
@@ -0,0 +1,8 @@
+package org.ict.util;
+
+/**
+ * https://www.javaprogramto.com/2020/08/java-8-sorting-stream-on-multiple-fields.html
+ * https://stackoverflow.com/questions/4805606/how-to-sort-by-two-fields-in-java
+ */
+public class SortByMultipleFields {
+}
