730227be113a829c886d5c691343f6443a7844a3
xueliansniper <xueliansniper@gmail.com>
2020-12-17 18:21:38 +0800

    add file upload test

---
+++ src/test/java/org/ict/util/MultipleFileUpload.java
@@ -0,0 +1,7 @@
+package org.ict.util;
+
+/**
+ * @see https://github.com/callicoder/spring-boot-file-upload-download-rest-api-example/blob/master/src/main/java/com/example/filedemo/controller/FileController.java
+ */
+public class MultipleFileUpload {
+}
