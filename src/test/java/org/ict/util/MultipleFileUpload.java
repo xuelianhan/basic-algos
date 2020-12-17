8b67b93b518b2e3abaabf6d15967cecbd461eff0
xueliansniper <xueliansniper@gmail.com>
2020-12-17 18:32:31 +0800

    add file upload test

--- src/test/java/org/ict/util/MultipleFileUpload.java
+++ src/test/java/org/ict/util/MultipleFileUpload.java
@@ -1,7 +1,11 @@
 package org.ict.util;
 
 /**
- * @see https://github.com/callicoder/spring-boot-file-upload-download-rest-api-example/blob/master/src/main/java/com/example/filedemo/controller/FileController.java
+ *
+ * @see <a href=“https://www.baeldung.com/spring-controller-return-image-file"></a>
+ * @see <a href="https://github.com/callicoder/spring-boot-file-upload-download-rest-api-example/blob/master/src/main/java/com/example/filedemo/controller/FileController.java"></a>
+ *
+ *
  */
 public class MultipleFileUpload {
 }
