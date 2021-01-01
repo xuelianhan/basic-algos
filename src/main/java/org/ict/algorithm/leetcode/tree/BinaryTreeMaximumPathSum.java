7d3c083377feedbf550a54fee2d11b49686ea737
xuelianhan <xueliansniper@gmail.com>
2021-01-01 18:53:47 +0800

    add MaxPathSum method in Binary Tree

--- src/main/java/org/ict/algorithm/leetcode/tree/BinaryTreeMaximumPathSum.java
+++ src/main/java/org/ict/algorithm/leetcode/tree/BinaryTreeMaximumPathSum.java
@@ -11,17 +11,23 @@ public class BinaryTreeMaximumPathSum {
     }
 
     public int recursion(TreeNode root) {
-         if (null == root) {
+         if (null == root) {//1
              return 0;
          }
-         int left = Math.max(0, recursion(root.left));
-         int right = Math.max(0, recursion(root.right));
-         ans = Math.max(ans, left + right + root.val);
-         return Math.max(left, right) + root.val;
+         int left = Math.max(0, recursion(root.left));//2
+         int right = Math.max(0, recursion(root.right));//3
+         ans = Math.max(ans, left + right + root.val);//4
+         return Math.max(left, right) + root.val;//used in 2 and 3
     }
 
     public static void main(String[] args) {
-
+        TreeNode root = new TreeNode(1);
+        TreeNode left = new TreeNode(2);
+        TreeNode right = new TreeNode(3);
+        root.left = left;
+        root.right = right;
+        BinaryTreeMaximumPathSum instance = new BinaryTreeMaximumPathSum();
+        instance.maxPathSum(root);
     }
 
 }
