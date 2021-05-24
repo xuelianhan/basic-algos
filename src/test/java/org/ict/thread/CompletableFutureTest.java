2a6f1a428075d27187dc90eca3c2cc0b71dc28f9
xueliansniper <xueliansniper@gmail.com>
2021-05-24 14:59:55 +0800

    add CompletableFutureTest.java

---
+++ src/test/java/org/ict/thread/CompletableFutureTest.java
@@ -0,0 +1,9 @@
+package org.ict.thread;
+
+/**
+ * @see <a href="https://www.callicoder.com/java-8-completablefuture-tutorial/"></a>
+ * @date 2021/5/24 2:59 PM
+ */
+public class CompletableFutureTest {
+
+}
