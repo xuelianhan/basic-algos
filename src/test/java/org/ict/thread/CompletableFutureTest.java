d0857740d8ebc184e23daa85edaa807e7a0d05f6
xueliansniper <xueliansniper@gmail.com>
2021-06-08 11:33:17 +0800

    add codes

--- src/test/java/org/ict/thread/CompletableFutureTest.java
+++ src/test/java/org/ict/thread/CompletableFutureTest.java
@@ -3,6 +3,9 @@ package org.ict.thread;
 import java.util.concurrent.CompletableFuture;
 
 /**
+ * https://crondev.blog/2017/01/23/timeouts-with-java-8-completablefuture-youre-probably-doing-it-wrong/
+ * https://stackoverflow.com/questions/30705981/what-is-the-recommended-way-to-wait-till-the-completable-future-threads-finish
+ * https://github.com/spring-projects/spring-framework/blob/main/spring-context/src/main/java/org/springframework/scheduling/concurrent/ExecutorConfigurationSupport.java
  * @see <a href="https://www.callicoder.com/java-8-completablefuture-tutorial/"></a>
  * @date 2021/5/24 2:59 PM
  */
---
+++ src/test/java/org/ict/util/StreamApiTest.java
@@ -0,0 +1,34 @@
+package org.ict.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author hxl
+ * @date 2021/6/8 11:29 AM
+ */
+public class StreamApiTest {
+
+    public static void main(String[] args) {
+        testMergeMultipleLists();
+    }
+
+    public static void testMergeMultipleLists() {
+        List<Long> list1 = new ArrayList<>();
+        list1.add(1L);
+
+        List<Long> list2 = new ArrayList<>();
+        list2.add(2L);
+
+        List<Long> list3 = null;//can not add stream of, due to npe
+
+        List<Long> list4 = new ArrayList<>();
+        List<Long> mergedList = Stream.of(list1, list2, list4)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+        System.out.println(mergedList);
+    }
+}
