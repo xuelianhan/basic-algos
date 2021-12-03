81b841c574b0d536a0030c487273cd1a57905a25
xueliansniper <xueliansniper@gmail.com>
2021-12-03 18:52:08 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/tree/KthLargestElementInStream.java
+++ src/main/java/org/ict/algorithm/leetcode/tree/KthLargestElementInStream.java
@@ -1,9 +1,64 @@
 package org.ict.algorithm.leetcode.tree;
 
 /**
+ * Design a class to find the k^th largest element in a stream.
+ * Note that it is the k^th largest element in the sorted order, not the k^th distinct element.
+ *
+ * Implement K^thLargest class:
+ *
+ * K^thLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+ * int add(int val) Appends the integer val to the stream and
+ * returns the element representing the k^th largest element in the stream.
+ *
+ *
+ * Example 1:
+ *
+ * Input
+ * ["KthLargest", "add", "add", "add", "add", "add"]
+ * [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+ * Output
+ * [null, 4, 5, 5, 8, 8]
+ *
+ * Explanation
+ * KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+ * kthLargest.add(3);   // return 4
+ * kthLargest.add(5);   // return 5
+ * kthLargest.add(10);  // return 5
+ * kthLargest.add(9);   // return 8
+ * kthLargest.add(4);   // return 8
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= k <= 10^4
+ * 0 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * -10^4 <= val <= 10^4
+ * At most 10^4 calls will be made to add.
+ * It is guaranteed that there will be at least k elements in the array when you search for the k^th element.
  * @author sniper
  * @date 2021/12/1
  * LC703
  */
 public class KthLargestElementInStream {
+    /**
+     * Your KthLargest object will be instantiated and called as such:
+     * KthLargest obj = new KthLargest(k, nums);
+     * int param_1 = obj.add(val);
+     */
+
+    public int add(int val) {
+        return 0;
+    }
+
+    public static class KthLargest {
+        private int k;
+        private int[] nums;
+
+        public KthLargest(int k, int[] nums) {
+           this.k = k;
+           this.nums = nums;
+        }
+
+    }
 }
