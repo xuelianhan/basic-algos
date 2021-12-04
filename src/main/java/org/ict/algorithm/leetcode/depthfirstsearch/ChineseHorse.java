cf87833ee1a987d43e441d987ed325f2b8655184
xuelianhan <xueliansniper@gmail.com>
2021-12-05 03:03:45 +0800

    add ChineseHorse.java

---
+++ src/main/java/org/ict/algorithm/leetcode/depthfirstsearch/ChineseHorse.java
@@ -0,0 +1,14 @@
+package org.ict.algorithm.leetcode.depthfirstsearch;
+
+/**
+ * input:
+ *  from Point (x0, y0) to Point(x1, y1)
+ *  find the total number of paths under the rule of Chinese Horse
+ *  if no such path exists, return 0;
+ */
+public class ChineseHorse {
+
+    public int totalPaths(int x0, int y0, int x1, int y1) {
+        return 0;
+    }
+}
--- src/main/java/org/ict/algorithm/leetcode/tree/SumOfRootToLeafBinaryNumbers.java
+++ src/main/java/org/ict/algorithm/leetcode/tree/SumOfRootToLeafBinaryNumbers.java
@@ -1,9 +1,33 @@
 package org.ict.algorithm.leetcode.tree;
 
 /**
+ * You are given the root of a binary tree where each node has a value 0 or 1.
+ * Each root-to-leaf path represents a binary number starting with the most significant bit.
+ * For example, if the path is 0 -> 1 -> 1 -> 0 -> 1,
+ * then this could represent 01101 in binary, which is 13.
+ *
+ * For all leaves in the tree,
+ * consider the numbers represented by the path from the root to that leaf.
+ *
+ * Return the sum of these numbers. The answer is guaranteed to fit in a 32-bits integer.
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [1, 1000].
+ * Node.val is 0 or 1.
+ *
  * @author sniper
  * @date 2021/12/1
  * LC1022
  */
 public class SumOfRootToLeafBinaryNumbers {
+
+    public int sumRootToLeaf(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        return 0;
+    }
+
+
 }
