bd4ead8487af35aadd186a84c3412271fc019ab8
xuelianhan <xueliansniper@gmail.com>
2021-12-07 21:38:48 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/tree/SumOfRootToLeafBinaryNumbers.java
+++ src/main/java/org/ict/algorithm/leetcode/tree/SumOfRootToLeafBinaryNumbers.java
@@ -25,10 +25,7 @@ import java.util.*;
  * LC1022
  */
 public class SumOfRootToLeafBinaryNumbers {
-
-    public static void main(String[] args) {
-        
-    }
+    
 
     public int sumRootToLeaf(TreeNode root) {
         if (null == root) {
@@ -37,31 +34,6 @@ public class SumOfRootToLeafBinaryNumbers {
         return 0;
     }
 
-    public static List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            /* queue size indicates number of nodes at each level */
-            int size = queue.size();
-            List<Integer> temp = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode cur = queue.poll();
-                temp.add(cur.val);
-                if (cur.left != null) {
-                    queue.add(cur.left);
-                }
-                if (cur.right != null) {
-                    queue.add(cur.right);
-                }
-            }
-            result.add(temp);
-        }
-        return result;
-    }
 
 
 }
