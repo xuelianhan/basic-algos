5e8fba941a9a01fb1bbb5be8f172441dccf2440e
xuelianhan <xueliansniper@gmail.com>
2021-12-21 00:05:10 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/string/IsomorphicString.java
+++ src/main/java/org/ict/algorithm/leetcode/string/IsomorphicString.java
@@ -5,7 +5,9 @@ package org.ict.algorithm.leetcode.string;
  *
  * Two strings s and t are isomorphic if the characters in s can be replaced to get t.
  *
- * All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+ * All occurrences of a character must be replaced with another character while preserving the order of characters.
+ * No two characters may map to the same character,
+ * but a character may map to itself.
  *
  *
  *
@@ -25,17 +27,42 @@ package org.ict.algorithm.leetcode.string;
  *
  * Constraints:
  *
- * 1 <= s.length <= 5 * 104
+ * 1 <= s.length <= 5 * 10^4
  * t.length == s.length
  * s and t consist of any valid ascii character.
  * LC205
  */
 public class IsomorphicString {
 
-    public boolean isIsomorphic(String s, String t) {
+    
+
+    /**
+     * Instead of directly mapping 'e' to 'a',
+     * another way is to mark them with same value,
+     * for example,
+     * 'e' -> 1,
+     * 'a'-> 1,
+     * 'g' -> 2,
+     * and 'd' -> 2,
+     * this works same.
+     * @param s
+     * @param t
+     * @return
+     */
+    public static boolean isIsomorphic(String s, String t) {
         if (s.equalsIgnoreCase(t)) {
             return true;
         }
-        return false;
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) {
+                return false;
+            }
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
+        }
+        return true;
     }
 }
