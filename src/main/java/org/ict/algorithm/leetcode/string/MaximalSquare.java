e0b9c82279bf49646a3203d7f89cf71962819f1e
xueliansniper <xueliansniper@gmail.com>
2021-12-22 12:50:20 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/leetcode/string/MaximalSquare.java
@@ -0,0 +1,34 @@
+package org.ict.algorithm.leetcode.string;
+
+/**
+ * Given an m x n binary matrix filled with 0's and 1's,
+ * find the largest square containing only 1's and return its area.
+ *
+ * Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * Output: 4
+ *
+ * Input: matrix = [["0","1"],["1","0"]]
+ * Output: 1
+ *
+ * Example 3:
+ *
+ * Input: matrix = [["0"]]
+ * Output: 0
+ *
+ *
+ * Constraints:
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 300
+ * matrix[i][j] is '0' or '1'.
+ *
+ * @author sniper
+ * @date 2021/12/17
+ */
+public class MaximalSquare {
+
+    public int maximalSquare(char[][] matrix) {
+        return 0;
+    }
+}
---
+++ src/main/java/org/ict/algorithm/leetcode/string/ReverseVowelsOfString.java
@@ -0,0 +1,9 @@
+package org.ict.algorithm.leetcode.string;
+
+/**
+ * @author sniper
+ * @date 2021/12/22 12:46 PM
+ * LC345
+ */
+public class ReverseVowelsOfString {
+}
