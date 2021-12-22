295ae9bda522b5e8f08a53cf46f36b74277eaff3
xueliansniper <xueliansniper@gmail.com>
2021-12-22 22:20:30 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/string/MaximalSquare.java
+++ src/main/java/org/ict/algorithm/leetcode/string/MaximalSquare.java
@@ -25,10 +25,56 @@ package org.ict.algorithm.leetcode.string;
  *
  * @author sniper
  * @date 2021/12/17
+ * LC221
  */
 public class MaximalSquare {
 
+    /**
+     * 1)Construct a sum matrix S[R][C] for the given M[R][C].
+     *      a)    Copy first row and first columns as it is from M[][] to S[][]
+     *      b)    For other entries, use following expressions to construct S[][]
+     *          If M[i][j] is 1 then
+     *             S[i][j] = min(S[i][j-1], S[i-1][j], S[i-1][j-1]) + 1
+     *          Else If M[i][j] is 0
+     *          S[i][j]=0
+     * 2) Find the maximum entry in S[R][C]
+     * 3) Using the value and coordinates of maximum entry in S[i],print sub-matrix ofM[][]
+     * @param matrix
+     * @return
+     */
     public int maximalSquare(char[][] matrix) {
-        return 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] aux = new int[m][n];
+        /**
+         * Copy First row to aux
+         */
+        for (int j = 0; j < n; j++) {
+            aux[0][j] = matrix[0][j];
+        }
+        /**
+         * Copy first column to aux
+         */
+        for (int i = 0; i < m; i++) {
+            aux[i][0] = matrix[i][0];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    aux[i][j] = Math.min(aux[i-1][j], Math.min(aux[i][j-1], aux[i-1][j-1])) + 1;
+                } else {
+                    aux[i][j] = 0;
+                }
+            }
+        }
+        int maxSize = 0;
+        for (int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (aux[i][j] > maxSize) {
+                    maxSize = aux[i][j];
+                }
+            }
+        }
+        return maxSize * maxSize;
     }
 }
