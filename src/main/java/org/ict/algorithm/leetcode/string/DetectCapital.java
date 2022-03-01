aab21dc3e6fea3948eb0dea2b735967306613b63
xuelianhan <xueliansniper@gmail.com>
2022-03-01 23:32:58 +0800

    add solution for DetectCapital.java

--- src/main/java/org/ict/algorithm/leetcode/string/DetectCapital.java
+++ src/main/java/org/ict/algorithm/leetcode/string/DetectCapital.java
@@ -30,7 +30,25 @@ package org.ict.algorithm.leetcode.string;
  */
 public class DetectCapital {
 
+    /**
+     *
+     * @param word
+     * @return
+     */
     public boolean detectCapitalUse(String word) {
+        char[] arr = word.toCharArray();
+        int upperCaseCnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (Character.isUpperCase(arr[i])) {
+                upperCaseCnt++;
+            }
+        }
+        /**
+         * ffffffffffffffffffffF
+         */
+        if (upperCaseCnt == arr.length || upperCaseCnt == 0 || (upperCaseCnt == 1 && Character.isUpperCase(arr[0]))) {
+            return true;
+        }
         return false;
     }
 }
