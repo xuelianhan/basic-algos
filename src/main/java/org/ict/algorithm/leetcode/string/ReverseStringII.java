3c6963863d0906d1619ec09e395d8e0f927cfb9f
xuelianhan <xueliansniper@gmail.com>
2022-03-13 15:33:59 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/string/ReverseStringII.java
+++ src/main/java/org/ict/algorithm/leetcode/string/ReverseStringII.java
@@ -8,17 +8,18 @@ import java.util.Stack;
  * reverse the first k characters for every 2k characters counting from the start of the string.
  *
  * If there are fewer than k characters left,
- * reverse all of them. If there are less than 2k but greater than or equal to k characters,
+ * reverse all of them.
+ *
+ * If there are less than 2k but greater than or equal to k characters,
  * then reverse the first k characters and leave the other as original.
  *
  *
  *
  * Example 1:
- *
  * Input: s = "abcdefg", k = 2
  * Output: "bacdfeg"
- * Example 2:
  *
+ * Example 2:
  * Input: s = "abcd", k = 2
  * Output: "bacd"
  *
@@ -34,13 +35,20 @@ import java.util.Stack;
 public class ReverseStringII {
 
     public String reverseStr(String s, int k) {
-        if (s.length() == 1) {
+        if (s.length() == 1 || k == 1) {
             return s;
+        }
+        if (s.length() <= k) {
+            return reverseString(s);
         }
         int i = 0;
         int j = i + 2 * k;
         for(i = 0; i < s.length(); ) {
+            if ((i + 1) % k == 0) {
 
+            } else {
+
+            }
             i  = j;
             j = i + 2 * k;
         }
