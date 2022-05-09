7ec112dbfe616ff3682ecb39ec7aa996471b1bd3
xuelianhan <xueliansniper@gmail.com>
2022-05-09 21:03:53 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/string/CountBinarySubstrings.java
+++ src/main/java/org/ict/algorithm/leetcode/string/CountBinarySubstrings.java
@@ -1,5 +1,8 @@
 package org.ict.algorithm.leetcode.string;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Give a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's,
  * and all the 0's and all the 1's in these substrings are grouped consecutively.
@@ -34,6 +37,13 @@ package org.ict.algorithm.leetcode.string;
  */
 public class CountBinarySubstrings {
 
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        String s = "00110011";
+        subString(s.toCharArray(), s.length(), list);
+        System.out.println(list);
+    }
+
     public int countBinarySubstrings(String s) {
         if (s.length() == 1) {
             return 0;
@@ -46,7 +56,33 @@ public class CountBinarySubstrings {
             }
         }
         //length of string greater than 2
+        int size = s.length() / 2;
 
         return 0;
+    }
+
+    /**
+     * Raw implementation of all substring for str array with length of n
+     * @param str
+     * @param n
+     * @param list
+     */
+    public static void subString(char str[], int n, List<String> list) {
+        // Pick starting point
+        for (int len = 1; len <= n; len++) {
+            // Pick ending point
+            for (int i = 0; i <= n - len; i++) {
+                //  Print characters from current
+                // starting point to current ending
+                // point.
+                int j = i + len - 1;
+                StringBuffer sb = new StringBuffer();
+                for (int k = i; k <= j; k++) {
+                   sb.append(str[k]);
+                }
+                list.add(sb.toString());
+                sb.delete(0, sb.length());
+            }
+        }
     }
 }
