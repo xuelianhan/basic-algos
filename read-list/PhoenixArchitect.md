0f3bde61550f6edddee0644c3f55826d01fe40c3
xueliansniper <xueliansniper@gmail.com>
2022-07-21 10:01:44 +0800

    add read-list markdown directory

---
+++ read-list/PhoenixArchitect.md
@@ -0,0 +1,6 @@
+###Lock
+**1.Write Lock**
+
+**2.Read Lock**
+
+**3.Range Lock**
--- src/test/java/org/ict/thread/ThreadOutPutTest.java
+++ src/test/java/org/ict/thread/ThreadOutPutTest.java
@@ -15,13 +15,13 @@ public class ThreadOutPutTest {
         ThreadOutPut obj = new ThreadOutPut();
         Thread thread = new Thread(obj);
         thread.start();
-        System.out.printf("Geeks "); 
+        System.out.printf("Geeks ");
         try { 
             thread.join(); 
         }  catch (InterruptedException e) { 
             e.printStackTrace(); 
         } 
-        System.out.println("for "); 
+        System.out.println("for ");
     }
     
 }
