796194ea792911e83a9c7b6297b02f73e70b9919
xueliansniper <xueliansniper@gmail.com>
2022-08-04 14:12:07 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/leetcode/string/LongPressedName.java
@@ -0,0 +1,44 @@
+package org.ict.algorithm.leetcode.string;
+
+/**
+ * Your friend is typing his name into a keyboard.
+ * Sometimes, when typing a character c,
+ * the key might get long pressed,
+ * and the character will be typed 1 or more times.
+ *
+ * You examine the typed characters of the keyboard.
+ * Return True if it is possible that it was your friends name,
+ * with some characters (possibly none) being long pressed.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: name = "alex", typed = "aaleex"
+ * Output: true
+ * Explanation: 'a' and 'e' in 'alex' were long pressed.
+ * Example 2:
+ *
+ * Input: name = "saeed", typed = "ssaaedd"
+ * Output: false
+ * Explanation: 'e' must have been pressed twice,
+ * but it was not in the typed output.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= name.length, typed.length <= 1000
+ * name and typed consist of only lowercase English letters.
+ * @author sniper
+ * @date 2022/8/4
+ * lc925
+ */
+public class LongPressedName {
+
+    public boolean isLongPressedName(String name, String typed) {
+        if (name.equals(typed)) {
+            return true;
+        }
+        return false;
+    }
+}
