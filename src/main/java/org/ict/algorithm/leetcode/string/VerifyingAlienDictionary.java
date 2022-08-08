c07aa904d5accc938d2d361b8e9b3b7018bff843
xueliansniper <xueliansniper@gmail.com>
2022-08-08 15:04:28 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/string/VerifyingAlienDictionary.java
+++ src/main/java/org/ict/algorithm/leetcode/string/VerifyingAlienDictionary.java
@@ -1,5 +1,8 @@
 package org.ict.algorithm.leetcode.string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * In an alien language, surprisingly,
  * they also use English lowercase letters, but possibly in a different order.
@@ -42,7 +45,64 @@ package org.ict.algorithm.leetcode.string;
  */
 public class VerifyingAlienDictionary {
 
-    public boolean isAlienSorted(String[] words, String order) {
-        return false;
+    public static void main(String[] args) {
+        //String[] words = {"hello","leetcode"};
+        //String order = "hlabcdefgijkmnopqrstuvwxyz";
+
+        //String[] words = {"word","world","row"};
+        //String order = "worldabcefghijkmnpqstuvxyz";
+
+        //String[] words = {"apple","app"};
+        //String order = "abcdefghijklmnopqrstuvwxyz";
+
+        //String[] words = {"apap","app"};
+        //String order = "abcdefghijklmnopqrstuvwxyz";
+
+        String[] words = {"fxasxpc","dfbdrifhp","nwzgs","cmwqriv","ebulyfyve","miracx","sxckdwzv","dtijzluhts","wwbmnge","qmjwymmyox"};
+        String order = "zkgwaverfimqxbnctdplsjyohu";
+        boolean result = isAlienSortedV1(words, order);
+        System.out.println(result);
+    }
+
+
+
+    /**
+     * Using hashmap to store the order of 26 characters.
+     * @param words
+     * @param order
+     * @return
+     */
+    public static boolean isAlienSortedV1(String[] words, String order) {
+        Map<Character, Integer> map = new HashMap<>(26);
+        for(int i = 0; i < order.length(); i++) {
+            map.put(order.charAt(i), i);
+        }
+        for(int j = 0; j < words.length - 1; j++) {
+            if (!compare(words[j], words[j + 1], map)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean compare(String s1, String s2, Map<Character, Integer> map) {
+        int l1 = s1.length();
+        int l2 = s2.length();
+        for (int i = 0, j = 0; i < l1 && j < l2; i++, j++) {
+            /**
+             * critical condition: Only if not equal can to be compared.
+             */
+            if (s1.charAt(i) != s2.charAt(j)) {
+                if (map.get(s1.charAt(i)) > map.get(s2.charAt(j))) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+        if (l1 > l2) {
+            return false;
+        }
+        return true;
     }
 }
