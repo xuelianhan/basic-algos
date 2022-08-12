c00259d774fd87ebdbacf804b0781e4f484479ca
xueliansniper <xueliansniper@gmail.com>
2022-08-12 11:00:25 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/string/NumberOfDaysBetweenTwoDates.java
+++ src/main/java/org/ict/algorithm/leetcode/string/NumberOfDaysBetweenTwoDates.java
@@ -20,6 +20,7 @@ package org.ict.algorithm.leetcode.string;
  * Constraints:
  *
  * The given dates are valid dates between the years 1971 and 2100.
+ * @see <a href="https://www.mathsisfun.com/leap-years.html"></a>
  * @author sniper
  * @date 2022/8/11
  * LC1360
@@ -32,8 +33,9 @@ public class NumberOfDaysBetweenTwoDates {
 
     public int daysBetweenDates(String date1, String date2) {
         int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-        return 0;
+        int d1 = calculateDays(date1, monthDays);
+        int d2 = calculateDays(date2, monthDays);
+        return Math.abs(d2 - d1);
     }
 
     public int calculateDays(String date, int[] monthDays) {
@@ -41,8 +43,36 @@ public class NumberOfDaysBetweenTwoDates {
         int year = Integer.valueOf(s[0]);
         int month = Integer.valueOf(s[1]);
         int day = Integer.valueOf(s[2]);
-        
-        return 0;
+
+        int days = year * 365 + day;
+        /**
+         * Add days for months in given date
+         */
+        for (int i = 0; i < month - 1; i++) {
+            days += monthDays[i];
+        }
+        /**
+         * Since every leap year is of 366 days,
+         * Add a day for every leap year
+         */
+        days += countLeapYear(year, month);
+        return days;
+    }
+
+    public int countLeapYear(int year, int month) {
+        int y = year;
+        /**
+         * Check if the current year needs to be considered
+         * for the count of leap years or not
+         */
+        if (month <= 2) {
+            y--;
+        }
+        /**
+         * An year is a leap year if it is a multiple of 4,
+         * multiple of 400 and not a multiple of 100.
+         */
+        return y / 4 - y / 100 + y / 400;
     }
 
     public boolean isLeapYear(int year) {
