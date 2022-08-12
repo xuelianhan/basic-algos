4ace0753059cb5f78dd17e6d6ac16cbb77286f66
xueliansniper <xueliansniper@gmail.com>
2022-08-12 18:39:30 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/leetcode/string/ConsecutiveCharacters.java
@@ -0,0 +1,54 @@
+package org.ict.algorithm.leetcode.string;
+
+/**
+ * The power of the string is the maximum length of a non-empty substring that contains only one unique character.
+ *
+ * Given a string s, return the power of s.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "leetcode"
+ * Output: 2
+ * Explanation: The substring "ee" is of length 2 with the character 'e' only.
+ * Example 2:
+ *
+ * Input: s = "abbcccddddeeeeedcba"
+ * Output: 5
+ * Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 500
+ * s consists of only lowercase English letters.
+ * @author sniper
+ * @date 2022/8/12
+ * LC1446
+ */
+public class ConsecutiveCharacters {
+
+    public static void main(String[] args) {
+        String s = "ccbccbb";
+        int result = maxPower(s);
+        System.out.println(result);
+    }
+
+    public static int maxPower(String s) {
+        if (s.length() == 1) {
+            return 1;
+        }
+        int max = 1;
+        for (int i = 0, j = 1; i < s.length() && j < s.length();) {
+            if (s.charAt(i) != s.charAt(j)) {
+                i++;
+            }
+            j++;
+            if ((j - i + 1) > max) {
+                max = j - i;
+            }
+        }
+        return max;
+    }
+}
