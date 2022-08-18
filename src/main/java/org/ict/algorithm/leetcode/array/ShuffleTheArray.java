aac19ddd71b1ba1c915e7d8bc6f6dda7722edc3f
xuelianhan <xueliansniper@gmail.com>
2022-08-19 04:56:36 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/leetcode/array/ShuffleTheArray.java
@@ -0,0 +1,59 @@
+package org.ict.algorithm.leetcode.array;
+
+/**
+ * Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+ *
+ * Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,5,1,3,4,7], n = 3
+ * Output: [2,3,5,4,1,7]
+ * Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+ * Example 2:
+ *
+ * Input: nums = [1,2,3,4,4,3,2,1], n = 4
+ * Output: [1,4,2,3,3,2,4,1]
+ * Example 3:
+ *
+ * Input: nums = [1,1,2,2], n = 2
+ * Output: [1,2,1,2]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 500
+ * nums.length == 2n
+ * 1 <= nums[i] <= 10^3
+ * @author sniper
+ * @date 19 Aug, 2022
+ * LC1470
+ */
+public class ShuffleTheArray {
+
+
+    /**
+     * 0, 1, 2, 3, 4, 5
+     * 0, 3, 1, 4, 2, 5
+     * @param nums
+     * @param n
+     * @return
+     */
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[nums.length];
+        boolean flag = true;
+        res[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (flag) {
+                res[i] = nums[i - 1 + n];
+                flag = false;
+            } else {
+                res[i] = nums[i - 1];
+                flag = true;
+            }
+        }
+        return res;
+    }
+}
