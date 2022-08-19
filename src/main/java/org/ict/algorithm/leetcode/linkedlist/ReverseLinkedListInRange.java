8b5cf278118914dd6ecd30ee439166c8ece1d6e0
xueliansniper <xueliansniper@gmail.com>
2022-08-19 18:12:46 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/leetcode/linkedlist/ReverseLinkedListInRange.java
@@ -0,0 +1,28 @@
+package org.ict.algorithm.leetcode.linkedlist;
+
+/**
+ *
+ * @author sniper
+ * @date 2022/8/19
+ * LC92
+ */
+public class ReverseLinkedListInRange {
+
+
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        return null;
+    }
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+}
