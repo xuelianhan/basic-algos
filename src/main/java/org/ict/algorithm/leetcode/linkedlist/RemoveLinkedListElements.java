f21d729720f90701f47fb5500a827bd3787ec010
xueliansniper <xueliansniper@gmail.com>
2022-08-26 14:38:05 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/linkedlist/RemoveLinkedListElements.java
+++ src/main/java/org/ict/algorithm/leetcode/linkedlist/RemoveLinkedListElements.java
@@ -31,6 +31,25 @@ package org.ict.algorithm.leetcode.linkedlist;
  */
 public class RemoveLinkedListElements {
 
+
+    public ListNode removeElementsV2(ListNode head, int val) {
+        /**
+         * The key to solve this problem
+         * is using a helper node to track the head of the list.
+         */
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+
     public ListNode removeElements(ListNode head, int val) {
         /**
          * The key to solve this problem
