3b7b19118468b036d2a1ac8605851dfe0429189b
xuelianhan <xueliansniper@gmail.com>
2022-09-18 11:32:14 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/linkedlist/RemoveNthNodeFromEndOfList.java
+++ src/main/java/org/ict/algorithm/leetcode/linkedlist/RemoveNthNodeFromEndOfList.java
@@ -49,10 +49,27 @@ public class RemoveNthNodeFromEndOfList {
      * fast:5, step==4, so slow:3, step:5
      * fast.next==null, while-loop ended.
      *
+     * Input: head = [1], n = 1
+     * Output: []
+     * Dummy->1->null
+     * fast:dummy, slow:dummy, step:0
+     * fast:1, step==0, so slow:dummy, step:1
+     * fast.next==null, while-loop ended.
+     *
+     *
+     * Input: head = [1,2], n = 1
+     * Output: [1]
+     * Dummy->1->2->null
+     * fast:dummy, slow:dummy, step:0
+     * fast:1, step==0, so slow:dummy, step:1
+     * fast:2, step==1, so slow:1, step:2
+     * fast.next==null, while-loop ended.
+     *
+     *
      * slow point at the previous node before the node waiting to delete.
      * slow.next is 4 that the one we need to delete.
-     * 1.Firstly, we mark the next node of slow.next
-     * 2.Then we link slow next to next mark
+     * 1.Firstly, we mark the next node of slow.next.
+     * 2.Then we link slow next to next node marked on above step.
      * 3.Finally, we return the head.
      *
      * @param head
@@ -70,6 +87,8 @@ public class RemoveNthNodeFromEndOfList {
          *
          * Why step start with zero?
          * Because both fast and slow are initialized with a dummy node.
+         * we need to assure fast pointer forwards (n+1) steps than slow pointer.
+         * You can change step value to 1, but need to change the comparable condition to step >= (n+1).
          */
         int step = 0;
         while (fast.next != null) {
@@ -79,9 +98,58 @@ public class RemoveNthNodeFromEndOfList {
             fast = fast.next;
             step++;
         }
+        /**
+         * If head is null, Both fast and slow point at dummy.
+         * dummy.next==null.
+         */
         if (slow.next == null) {
             return dummy.next;
         }
+        ListNode next = slow.next.next;
+        slow.next = next;
+        return dummy.next;
+    }
+
+    /**
+     * A one pass solution can be done with two-pointers.
+     * Move fast pointer (n+1) steps forward firstly.
+     * Then move both pointers fast and slow with the same speed forward.
+     * Finally, when fast pointer reaches at the end, the slow pointer stays at
+     * the location before the node to be deleted.
+     *
+     * the n is in normal range here, so we don't need to check its legality.
+     * The number of nodes in the list is sz.
+     * 1 <= sz <= 30
+     * 1 <= n <= sz.
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEndV2(ListNode head, int n) {
+        if (null == head || n <= 0) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        /**
+         * Move fast n + 1 steps firstly.
+         * because fast move one step over n, slow will move one step less.
+         * At the end, fast point at the null,
+         * slow point at the previous node before the node waiting to delete.
+         */
+        for (int i = 1; i <= (n + 1); i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
         ListNode next = slow.next.next;
         slow.next = next;
         return dummy.next;
