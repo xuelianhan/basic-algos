3876c15d1bac9bdaade69a2882ae1fc44fa58eba
xuelianhan <xueliansniper@gmail.com>
2022-09-19 22:45:22 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/array/PascalTriangleII.java
+++ src/main/java/org/ict/algorithm/leetcode/array/PascalTriangleII.java
@@ -46,8 +46,23 @@ public class PascalTriangleII {
     }
 
     public List<Integer> getRowV3(int rowIndex) {
-        //todo
-        return null;
+        /**
+         * Here res initialized all elements with 0.
+         */
+        int[] res = new int[rowIndex+1];
+        res[0] = 1;
+        /**
+         * Due to level 0 has only one element res[0], it has been set to 1.
+         * So level starts at 1 here, not start at 0, so condition in outer-loop is level=1
+         * col ends at 1 because res[0] has set to 1, so condition int inner-loop is col>=1
+         * res[col-1] condition also requires col >= 1 because array index minimum is zero.
+         */
+        for (int level = 1; level <= rowIndex; level++) {
+            for (int col = level; col >= 1; col--) {
+                res[col] += res[col-1];
+            }
+        }
+        return IntStream.of(res).boxed().collect(Collectors.toList());
     }
 
     public List<Integer> getRowV2(int rowIndex) {
@@ -79,33 +94,56 @@ public class PascalTriangleII {
          * Every rowIndex has (rowIndex+1) elements.
          * So outer-for-loop condition is 0 <= i <= rowIndex.
          * Notice the fact that res[i] initialized as 0, where i>0, res[0]=1.
+         * we calculate the value from right to left line by line from top to down.
+         * 1.level start from 0, and level++ control the process from top to down.
+         * 2.level <= rowIndex, col = level, col > 0, this three conditions assure the process from right to left.
          *
-         * Firstly, we calculate the value on the main diagonal line from left-top-corner to right-bottom-corner
+         * For each line, process likes the following:
+         * 1.Firstly, we calculate the value on the main diagonal line on right.
+         * 3.Secondly,we calculate res from right to left.
+         *
          * level0: res[0] = 1
-         * level1: res[1] = level0.res[1] + level0.res[0] = 0 + 1 = 1
-         * level2: res[2] = level1.res[2] + level1.res[1] = 0 + 1 = 1
-         * level3: res[3] = level2.res[3] + level2.res[2] = 0 + 1 = 1
-         * level4: res[4] = level3.res[4] + level3.res[3] = 0 + 1 = 1
          *
-         * Secondly,we calculate res from right to left, top to down cumulatively.
+         * level1: res[1] = level1.res[1] + level1.res[0] = 0 + 1 = 1
          *
-         * level2: res[1] = level1.res[1] + level1.res[0] = 1 + 1 = 2
+         * level2: res[2] = level2.res[2] + level1.res[1] = 0 + 1 = 1
+         *         res[1] = level1.res[1] + level2.res[0] = 1 + 1 = 2
          *
-         * level3: res[2] = level2.res[2] + level2.res[1] = 1 + 2 = 3
-         *         res[1] = level2.res[1] + level2.res[0] = 2 + 1 = 3
          *
-         * level4: res[3] = level3.res[3] + level3.res[2] = 1 + 3 = 4
+         *
+         * level3: res[3] = level3.res[3] + level2.res[2] = 0 + 1 = 1
+         *         res[2] = level2.res[2] + level2.res[1] = 1 + 2 = 3
+         *         res[1] = level2.res[1] + level3.res[0] = 2 + 1 = 3
+         *
+         *
+         * level4: res[4] = level4.res[4] + level3.res[3] = 0 + 1 = 1
+         *         res[3] = level3.res[3] + level3.res[2] = 1 + 3 = 4
          *         res[2] = level3.res[2] + level3.res[1] = 3 + 3 = 6
-         *         res[1] = level3.res[1] + level3.res[0] = 3 + 1 = 4
+         *         res[1] = level3.res[1] + level4.res[0] = 3 + 1 = 4
          *
          */
         int[] res = new int[rowIndex+1];
+        /**
+         * ---0--1--2--3--4--5
+         * 0| 0  0  0  0  0
+         * 1| 0  0  0  0  0
+         * 2| 0  0  0  0  0
+         * 3| 0  0  0  0  0
+         * 4| 0  0  0  0  0
+         */
         res[0] = 1;
+        /**
+         * ---0--1--2--3--4--5
+         * 0| 1  0  0  0  0
+         * 1| 1  0  0  0  0
+         * 2| 1  0  0  0  0
+         * 3| 1  0  0  0  0
+         * 4| 1  0  0  0  0
+         */
         for (int level = 0; level <= rowIndex; level++) {
             for (int col = level; col > 0; col--) {
                res[col] += res[col-1];
             }
-            System.out.println(Arrays.toString(res));
         }
         return IntStream.of(res).boxed().collect(Collectors.toList());
     }
---
+++ src/main/java/org/ict/algorithm/leetcode/monotonicstack/MonotonicStack.md
@@ -0,0 +1,10 @@
+A monotonic stack is a stack whose elements are monotonically increasing or decreasing. 
+It contains all qualities that a typical stack has and its elements are all monotonic decreasing or increasing.
+
+Below are the features of a monotonic stack:
+
+It is a range of queries in an array situation
+The minima/maxima elements
+When an element is popped from the monotonic stack, it will never be utilised again.
+The monotonic stack problem is mainly the previous/next smaller/larger problem. 
+It maintains monotonicity while popping elements when a new item is pushed into the stack.
