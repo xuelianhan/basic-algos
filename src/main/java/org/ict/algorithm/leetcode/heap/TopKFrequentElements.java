bf7926673d453147f0c72a332011553365cd512b
xuelianhan <xueliansniper@gmail.com>
2022-09-21 13:29:48 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/heap/TopKFrequentElements.java
+++ src/main/java/org/ict/algorithm/leetcode/heap/TopKFrequentElements.java
@@ -37,24 +37,30 @@ import java.util.Queue;
 public class TopKFrequentElements {
 
     public int[] topKFrequentV2(int[] nums, int k) {
-        // O(1) time
         if (k == nums.length) {
             return nums;
         }
 
-        // 1. build hash map : character and how often it appears
-        // O(N) time
+        /**
+         * 1. build hash map : character and how often it appears.
+         *  O(N) time.
+         */
         Map<Integer, Integer> count = new HashMap();
         for (int n: nums) {
             count.put(n, count.getOrDefault(n, 0) + 1);
         }
 
-        // init heap 'the less frequent element first'
+        /**
+         * init heap 'the less frequent element first'
+         */
         Queue<Integer> heap = new PriorityQueue<>(
                 (n1, n2) -> count.get(n1) - count.get(n2));
 
-        // 2. keep k top frequent elements in the heap
-        // O(N log k) < O(N log N) time
+
+        /**
+         *  2. keep k top frequent elements in the heap.
+         *  O(N log k) < O(N log N) time.
+         */
         for (int n: count.keySet()) {
             heap.add(n);
             if (heap.size() > k) {
@@ -62,8 +68,10 @@ public class TopKFrequentElements {
             }
         }
 
-        // 3. build an output array
-        // O(k log k) time
+        /**
+         *  3. build an output array
+         *   O(k log k) time.
+         */
         int[] top = new int[k];
         for(int i = k - 1; i >= 0; --i) {
             top[i] = heap.poll();
