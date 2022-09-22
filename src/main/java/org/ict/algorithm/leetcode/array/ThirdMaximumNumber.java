ad402169aeb06c0a51db51c2ed4b0947a9fd9a7c
xuelianhan <xueliansniper@gmail.com>
2022-09-22 14:38:51 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/array/ThirdMaximumNumber.java
+++ src/main/java/org/ict/algorithm/leetcode/array/ThirdMaximumNumber.java
@@ -1,5 +1,7 @@
 package org.ict.algorithm.leetcode.array;
 
+import java.util.*;
+
 /**
  * Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
  *
@@ -44,7 +46,133 @@ package org.ict.algorithm.leetcode.array;
  */
 public class ThirdMaximumNumber {
 
-    public int thirdMax(int[] nums) {
-       return 0;
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 3, 1};
+        int res = thirdMaxV1(nums);
+        System.out.println(res);
+    }
+
+
+    public static int thirdMaxV3(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int num : nums) {
+            set.add(num);
+            if (set.size() > 3) {
+                set.pollFirst();
+
+            }
+        }
+        return (set.size() < 3 ? set.pollLast() : set.pollFirst());
+    }
+
+
+    public static int thirdMaxV2(int[] nums) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        Set<Integer> set = map.descendingKeySet();
+        int res = 0;
+        int i = 1;
+        for (Integer key : set) {
+            if (set.size() < 3) {
+                res = key;
+                break;
+            } else {
+                res = key;
+                if (i == 3) {
+                    break;
+                }
+                i++;
+            }
+        }
+        return res;
+    }
+
+    public static int thirdMaxV1(int[] nums) {
+        int max = 0;
+        int k = 3;
+        Set<Integer> set = new HashSet<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                continue;
+            }
+            if (num > max) {
+                max = num;
+            }
+            set.add(num);
+            queue.offer(num);
+
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+        if (queue.size() == k) {
+            return queue.peek();
+        } else {
+            return max;
+        }
+    }
+
+    /**
+     * Time Complexity is O(N);
+     * Space Complexity is O(1)
+     * @param nums
+     * @return
+     */
+    public static int thirdMaxTuningVersion(int[] nums) {
+        Integer firstMax = null;
+        Integer secondMax = null;
+        Integer thirdMax = null;
+        for (Integer num : nums) {
+            if (num.equals(firstMax) || num.equals(secondMax) || num.equals(thirdMax)) {
+                continue;
+            }
+            if (firstMax == null || num > firstMax) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = num;
+            } else if (secondMax == null || num > secondMax) {
+                thirdMax = secondMax;
+                secondMax = num;
+            } else if (thirdMax == null || num > thirdMax) {
+                thirdMax = num;
+            }
+        }
+        return thirdMax == null ? firstMax : thirdMax;
+    }
+
+    /**
+     * Time Complexity O(N)
+     * Input [1, 2] expected 2
+     * Notice the constraint  -2^31 <= nums[i] <= 2^31 - 1
+     * The range is Integer, So The minimum should initialize with Long.MIN_VALUE.
+     * Another method is to Initialize with null which be showed in thirdMaxTuningVersion.
+     *
+     * Time Complexity is O(N);
+     * Space Complexity is O(1)
+     *
+     * @param nums
+     * @return
+     */
+    public static int thirdMax(int[] nums) {
+        long min = Long.MIN_VALUE;
+        long firstMax = min;
+        long secondMax = min;
+        long thirdMax = min;
+        for (int num : nums) {
+            if (num > firstMax) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = num;
+            } else if (num > secondMax && num < firstMax) {
+                thirdMax = secondMax;
+                secondMax = num;
+            } else if (num > thirdMax && num < secondMax) {
+                thirdMax = num;
+            }
+        }
+        return (int)(thirdMax == min ? firstMax : thirdMax);
     }
 }
