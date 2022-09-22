8dc7988124f24a0edc04b9a24164a2668771e9ef
xuelianhan <xueliansniper@gmail.com>
2022-09-23 00:11:08 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/array/SqrtX.java
+++ src/main/java/org/ict/algorithm/leetcode/array/SqrtX.java
@@ -87,6 +87,9 @@ public class SqrtX {
         int low = 1, high = x;
         while (low < high) {
             int mid = low + (high - low) / 2;
+            /**
+             * prevent overflow.
+             */
             if (mid <= x / mid && (mid + 1) > x/(mid + 1)) {
                 return mid;
             } else if (mid > x / mid) {
