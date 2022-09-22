281e4264aa92d6e11e15cd1ed8aa9c3070c54372
xuelianhan <xueliansniper@gmail.com>
2022-09-22 22:41:05 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/array/SqrtX.java
+++ src/main/java/org/ict/algorithm/leetcode/array/SqrtX.java
@@ -36,14 +36,32 @@ public class SqrtX {
     }
 
     /**
-     * Newton's method.
+     * Newton-Raphson Iteration Method
+     *
+     * f(x) = f(x0) + f'(x0)(x-x0)
+     *
+     * 0 = f(x0) + f'(x0)(x1-x0) --> x1 = x0 - f(x0)/f'(x0)
+     *                           --> x2 = x1 - f(x1)/f'(x1)
+     *                           ....
+     *                           --> Xn+1 = Xn - f(Xn)/f'(Xn)
+     *
+     * At here f(x) = x^2-a, we need find the solution makes x^2 - a = 0
+     * f'(x) = 2x
+     * Xn+1 = Xn - (Xn^2-a)/2*Xn
+     *      = (2*Xn^2 - Xn^2 + a)/2*Xn
+     *      = (Xn^2 + a)/2*Xn
+     *      = (1/2)*(Xn + a/Xn)
+     *      = (Xn + a/Xn)/2
+     *
      * A classic algorithm that illustrates many of these concerns is 'Newton-Raphson' method to compute square
      * roots x = √a for a > 0, i.e. to solve x^2 = a.
      * The algorithm starts with some guess x1 > 0 and computes the sequence of improved guesses
      * Xn+1 = (Xn + a/Xn)/2.
      *
-     * The intuition is very simple: if Xn is too big (>√a), then a/Xn will be too small (<
-     * √a), and so their arithmetic mean xn+1 will be closer to √a.
+     * The intuition is very simple: if Xn is too big (>√a), then a/Xn will be too small (<√a),
+     * and so their arithmetic mean xn+1 will be closer to √a.
+     *
+     * @see <a href="https://math.mit.edu/~stevenj/18.335/newton-sqrt.pdf"></a>
      *
      *
      * @param x
@@ -64,6 +82,7 @@ public class SqrtX {
         int low = 1, high = x;
         while (low < high) {
             int mid = low + (high - low) / 2;
+
 
         }
         return low;
