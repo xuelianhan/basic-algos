08db022db774b84f05eaf2a19f56bb7c17935786
xuelianhan <xueliansniper@gmail.com>
2022-09-28 21:21:55 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/array/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/ict/algorithm/leetcode/array/BestTimeToBuyAndSellStock.java
@@ -33,31 +33,66 @@ package org.ict.algorithm.leetcode.array;
  */
 public class BestTimeToBuyAndSellStock {
 
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        //int[] prices = {7,6,4,3,1};
+        maxProfit(prices);
+    }
+
+    public static int maxProfitV1(int[] prices) {
+        int n = prices.length;
+        int maxSP[] = new int[n];
+        int max = Integer.MIN_VALUE;
+        // Construct the maxSP array
+        for (int i = n - 1; i >= 0; i--) {
+            if (prices[i] > max) {
+                max = prices[i];
+                maxSP[i] = Integer.MIN_VALUE;
+            } else {
+                maxSP[i] = max;
+            }
+        }
+        int profit = 0;
+        for (int i = 0; i < n; i++) {
+            if (maxSP[i] != Integer.MIN_VALUE) {
+                profit = Math.max(profit, maxSP[i] - prices[i]);
+            }
+        }
+        // Return profit
+        return profit;
+    }
+
     /**
+     * Time Limit Exceeded.
+     *
      * [3,2,6,5,0,3]
-     * 6,6,0,0,3,0
      * expected:4
      *
      * [7,1,5,3,6,4]
-     * 0,5,6,6,0,0
+     * expected:5
+     *
+     *  0 1 2 3 4
+     * [7,6,4,3,1]
+     * expected:0
      *
      * @param prices
      * @return
      */
-    public int maxProfit(int[] prices) {
-        int left = 0;
-        int right = prices.length-1;
-        int minLeft = 10001;
-        int maxRight = -1;
+    public static int maxProfit(int[] prices) {
+        int maxRes = 0;
         for (int i = 0; i < prices.length; i++) {
-            for (int j = prices.length - 1; j >= i + 1; j--) {
+            int maxRight = 0;
+            for (int j = i + 1; j < prices.length; j++) {
                 if (prices[j] > maxRight) {
                     maxRight = prices[j];
                 }
-                right--;
             }
-
+            System.out.println("maxRight:" + maxRight + ", price[" + i +"]:" + prices[i] + ", maxRes:" + maxRes);
+            if ((maxRight - prices[i]) > maxRes) {
+                maxRes = (maxRight - prices[i]);
+            }
+            System.out.println(prices[i] + ", maxRes:" + maxRes);
         }
-        return (minLeft < maxRight ? (maxRight-minLeft) : 0);
+        return maxRes;
     }
 }
