03e24802bcbc43db2eef5451a8ff07b1bf79ed17
xuelianhan <xueliansniper@gmail.com>
2022-09-29 17:45:24 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/array/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/ict/algorithm/leetcode/array/BestTimeToBuyAndSellStock.java
@@ -36,78 +36,108 @@ public class BestTimeToBuyAndSellStock {
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
         //int[] prices = {7,6,4,3,1};
-        maxProfit(prices);
+        maxProfitV2(prices);
     }
 
-    public int maxProfixV2(int[] prices) {
+    /**
+     * Solution provided by Earl<a href="https://leetcode.com/earlme/">Earl</a>
+     * Kadane's Algorithm
+     *
+     * The logic to solve this problem is same as "max subarray problem" using Kadane's Algorithm.
+     * Since no body has mentioned this so far, I thought it's a good thing for everybody to know.
+     *
+     * All the straight forward solution should work,
+     * but if the interviewer twists the question slightly by giving the difference array of prices, Ex: for {1, 7, 4, 11},
+     * if he gives {0, 6, -3, 7},
+     * you might end up being confused.
+     *
+     * Here, the logic is to calculate the difference (maxCur += prices[i] - prices[i-1]) of the original array,
+     * and find a contiguous subarray giving maximum profit. If the difference falls below 0, reset it to zero.
+     *
+     * @param prices
+     * @return
+     */
+    public static int maxProfitV3(int[] prices) {
+        int currentMax = 0;
+        int soFarFoundMax = 0;
+        for (int i = 1; i < prices.length; i++) {
+            currentMax = Math.max(0, currentMax += (prices[i] - prices[i-1]));
+            soFarFoundMax = Math.max(soFarFoundMax, currentMax);
+        }
+        return soFarFoundMax;
+    }
+
+
+    public static int maxProfitV2(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int price : prices) {
+            minPrice = Math.min(price, minPrice);
+            int profit = price - minPrice;
+            maxProfit = Math.max(profit, maxProfit);
+        }
+        return maxProfit;
+    }
+
+    /**
+     * This problem can be solved using the greedy approach.
+     * To maximize the profit we have to minimize the buy cost and to sell it at the maximum price.
+     * Follow the steps bellow to implement the above idea.
+     * 1.Declare a buy variable to store the buy cost and a maxProfix variable to store the maximum.
+     * 2.Initialize the buy variable to the first element of the prices array.
+     * Iterate over the prices array and check if the current price is minimum or not.
+     * a.If the current price is minimum then buy on this ith day.
+     * b.If the current price is greater than the previous buy cost then make profit from it and maximize the maxProfit.
+     * 3.Finally, return the maxProfit.
+     *
+     * Time Complexity O(N)
+     * Auxiliary Space O(1)
+     *
+     * @param prices
+     * @return
+     */
+    public static int maxProfixV1(int[] prices) {
         int n = prices.length;
         int buy = prices[0], maxProfit = 0;
         for (int i = 1; i < n; i++) {
             if (buy > prices[i]) {
-                // Checking for lower buy value
+                /**
+                 * Checking for lower buy value
+                 */
                 buy = prices[i];
             } else if (prices[i] - buy > maxProfit) {
-                // Checking for higher profit
+                /**
+                 * Checking for higher profit
+                 */
                 maxProfit = prices[i] - buy;
             }
         }
         return maxProfit;
     }
 
-    public int maxProfitV1(int[] prices) {
+    public static int maxProfixV0(int[] prices) {
         int n = prices.length;
-        int maxSP[] = new int[n];
-        int max = Integer.MIN_VALUE;
-        // Construct the maxSP array
-        for (int i = n - 1; i >= 0; i--) {
-            if (prices[i] > max) {
-                max = prices[i];
-                maxSP[i] = Integer.MIN_VALUE;
+        int minBuySoFar = prices[0], maxProfit = 0;
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > minBuySoFar) {
+                maxProfit = Math.max(maxProfit, prices[i] - minBuySoFar);
             } else {
-                maxSP[i] = max;
+                minBuySoFar = prices[i];
             }
         }
-        int profit = 0;
-        for (int i = 0; i < n; i++) {
-            if (maxSP[i] != Integer.MIN_VALUE) {
-                profit = Math.max(profit, maxSP[i] - prices[i]);
-            }
-        }
-        // Return profit
-        return profit;
+        return maxProfit;
     }
 
-    /**
-     * Brute-Force But Time Limit Exceeded.
-     *
-     * [3,2,6,5,0,3]
-     * expected:4
-     *
-     * [7,1,5,3,6,4]
-     * expected:5
-     *
-     *  0 1 2 3 4
-     * [7,6,4,3,1]
-     * expected:0
-     *
-     * @param prices
-     * @return
-     */
-    public static int maxProfit(int[] prices) {
-        int maxRes = 0;
-        for (int i = 0; i < prices.length; i++) {
-            int maxRight = 0;
-            for (int j = i + 1; j < prices.length; j++) {
-                if (prices[j] > maxRight) {
-                    maxRight = prices[j];
-                }
+    public static int maxProfix(int[] prices) {
+        int n = prices.length;
+        int minBuySoFar = prices[0], maxProfit = 0;
+        for (int i = 1; i < n; i++) {
+            if (prices[i] < minBuySoFar) {
+                minBuySoFar = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - minBuySoFar);
             }
-            System.out.println("maxRight:" + maxRight + ", price[" + i +"]:" + prices[i] + ", maxRes:" + maxRes);
-            if ((maxRight - prices[i]) > maxRes) {
-                maxRes = (maxRight - prices[i]);
-            }
-            System.out.println(prices[i] + ", maxRes:" + maxRes);
         }
-        return maxRes;
+        return maxProfit;
     }
 }
