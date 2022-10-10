7ef8bcd539514c9915f6fc54519e2ea3b1ad54b1
xuelianhan <xueliansniper@gmail.com>
2022-10-10 20:25:22 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/array/BestTimeToBuyAndSellStockIV.java
+++ src/main/java/org/ict/algorithm/leetcode/array/BestTimeToBuyAndSellStockIV.java
@@ -36,6 +36,12 @@ public class BestTimeToBuyAndSellStockIV {
     /**
      * Solution provided by jinwu
      *
+     * dp[i, j] represents the max profit up until prices[j] using at most i transactions.
+     * dp[i, j] = max(dp[i, j-1], prices[j] - prices[jj] + dp[i-1, jj]) { jj in range of [0, j-1] }
+     *          = max(dp[i, j-1], prices[j] + max(dp[i-1, jj] - prices[jj]))
+     * dp[0, j] = 0; 0 transactions makes 0 profit.
+     * dp[i, 0] = 0; if there is only one price data point you can't make any transaction.
+     *
      * @param k
      * @param prices
      * @return
