344b91f934008ded559463442fd3a19fb1d606fd
xuelianhan <xueliansniper@gmail.com>
2022-10-13 20:40:37 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/monotonicstack/LargestRectangleInHistogram.java
+++ src/main/java/org/ict/algorithm/leetcode/monotonicstack/LargestRectangleInHistogram.java
@@ -47,10 +47,11 @@ public class LargestRectangleInHistogram {
      * 0 1 2 3 4 5
      * 2 1 5 6 2 3
      * i:0, h:2, stack:0
-     * i:1, h:1, heights[0] > h, pop 0 from the stack, height=heights[0]=2, start:-1, area=2*(1 - (-1) - 1)=2*1=2, res=max(0,2)=2
+     * i:1, h:1, stack:0, heights[0] > h, pop 0 from the stack, height=heights[0]=2, start:-1, area=2*(1 - (-1) - 1)=2*1=2, res=max(0,2)=2
      * i:2, h:5, stack is empty, push 2 into the stack, stack:2
      * i:3, h:6, stack:2, heights[2] < h, push 3 into the stack, stack:2,3
-     * i:4, h:2, stack:2,3, heights[3] > h
+     * i:4, h:2, stack:2,3, heights[3] > h, pop 3 from the stack, height=heights[3]=6, start:2, area=6*(4 - 2 - 1)=6, res=max(2, 6)=6
+     *           stack:2, heights[2] > h, pop 2 from the stack, height=heights[2]=5, start:-1, area=5*(4 - (-1) - 1)=20, res=max(6, 20)=20
      *
      *
      * @param heights
