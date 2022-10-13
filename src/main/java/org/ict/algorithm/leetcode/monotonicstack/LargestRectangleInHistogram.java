e9ad2bb036fa25460f227ba3039b1217f6289338
xuelianhan <xueliansniper@gmail.com>
2022-10-13 20:29:39 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/monotonicstack/LargestRectangleInHistogram.java
+++ src/main/java/org/ict/algorithm/leetcode/monotonicstack/LargestRectangleInHistogram.java
@@ -43,6 +43,14 @@ public class LargestRectangleInHistogram {
 
     /**
      * Time Cost 60ms
+     * e.g.
+     * 0 1 2 3 4 5
+     * 2 1 5 6 2 3
+     * i:0, h:2, stack:0
+     * i:1, h:1, heights[0] > h, pop 0 from the stack, height=heights[0]=2, start:-1, area=2*(1 - (-1) - 1)=2*1=2, res=max(0,2)=2
+     * i:2, h:5, stack is empty, push 2 into the stack, stack:2
+     * i:3, h:6, stack:2, heights[2] < h, push 3 into the stack, stack:2,3
+     * i:4, h:2, stack:2,3, heights[3] > h
      *
      *
      * @param heights
@@ -61,7 +69,7 @@ public class LargestRectangleInHistogram {
         for (int i = 0; i <= heights.length; i++) {
             int h = (i == heights.length ? 0 : heights[i]);
             /**
-             *
+             * Bottom-Top Increasing Monotonic Stack.
              */
             while (!stack.isEmpty() && heights[stack.peek()] > h) {
                 int height = heights[stack.pop()];
