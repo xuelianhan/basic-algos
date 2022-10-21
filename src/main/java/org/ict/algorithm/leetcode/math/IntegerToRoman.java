37c403399ac3a222d86bcf3241df6bff6ac119b5
xuelianhan <xueliansniper@gmail.com>
2022-10-21 14:42:15 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/math/IntegerToRoman.java
+++ src/main/java/org/ict/algorithm/leetcode/math/IntegerToRoman.java
@@ -63,13 +63,30 @@ public class IntegerToRoman {
     }
 
     public static String intToRomanV2(int num) {
+        /**
+         * "", 1000, 2000, 3000
+         */
         String M[] = {"", "M", "MM", "MMM"};
+        /**
+         * "", 100, 200, 300, 400, 500, 600, 700, 800, 900
+         */
         String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        /**
+         * "", 10, 20, 30, 40, 50, 60, 70, 80, 90
+         */
         String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        /**
+         * "", 1, 2, 3, 4, 5, 6, 7, 8, 9
+         */
         String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
         return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
     }
 
+    /**
+     * Understand the following solution.
+     * @param num
+     * @return
+     */
     public String intToRomanV1(int num) {
         StringBuilder res = new StringBuilder();
         String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
