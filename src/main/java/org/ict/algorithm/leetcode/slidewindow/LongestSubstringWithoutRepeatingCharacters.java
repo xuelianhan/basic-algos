8260d980f0b45f7fe1e54d4408a0bfa590b4fd28
xuelianhan <xueliansniper@gmail.com>
2022-10-22 15:48:12 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/slidewindow/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/org/ict/algorithm/leetcode/slidewindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,8 @@
 package org.ict.algorithm.leetcode.slidewindow;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Given a string s, find the length of the longest substring without repeating characters.
  *
@@ -33,8 +36,81 @@ package org.ict.algorithm.leetcode.slidewindow;
  */
 public class LongestSubstringWithoutRepeatingCharacters {
 
+
+    public int lengthOfLongestSubstringV5(String s) {
+        int res = 0;
+        if (s == null || s.length() == 0) {
+            return res;
+        }
+        //todo
+        return res;
+    }
+
+    public int lengthOfLongestSubstringV4(String s) {
+        int res = 0;
+        if (s == null || s.length() == 0) {
+            return res;
+        }
+        //todo
+        return res;
+    }
+
+
+    public int lengthOfLongestSubstringV3(String s) {
+        int res = 0;
+        if (s == null || s.length() == 0) {
+            return res;
+        }
+        //todo
+        return res;
+    }
+
+
+    public int lengthOfLongestSubstringV2(String s) {
+        int res = 0;
+        if (s == null || s.length() == 0) {
+            return res;
+        }
+        //todo
+        return res;
+    }
+
+
+    public int lengthOfLongestSubstringV1(String s) {
+        int res = 0;
+        if (s == null || s.length() == 0) {
+            return res;
+        }
+        //todo
+        return res;
+    }
+
+    /**
+     * Understand the following solution
+     *
+     * Two-Pointers + HashSet solution.
+     * The idea is use a hashset to track the longest substring without repeating characters so far,
+     * use a fast pointer j to see if character j is in the hashset or not, if not, add it to the hashset, move j forward and update the max length,
+     * otherwise, delete from the head by using a slow pointer i until we can put character j to the hash set.
+     * 
+     * @param s
+     * @return
+     */
     public int lengthOfLongestSubstring(String s) {
         int res = 0;
+        if (s == null || s.length() == 0) {
+            return res;
+        }
+        int i = 0, j = 0;
+        Set<Character> set = new HashSet<>();
+        while (j < s.length()) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                res = Math.max(res, set.size());
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
         return res;
     }
 }
