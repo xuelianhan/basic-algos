833828a6f5b6515bf0e4f486d58fab6b7797f57e
xuelianhan <xueliansniper@gmail.com>
2022-10-23 13:43:05 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/slidewindow/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/org/ict/algorithm/leetcode/slidewindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -169,6 +169,12 @@ public class LongestSubstringWithoutRepeatingCharacters {
     /**
      * HashMap Solution
      *
+     * The basic idea is, keep a hashmap which stores the characters in string as keys, and their positions as values,
+     * and keep two pointers which define the max substring. move the right pointer to scan through the string ,
+     * and meanwhile update the hashmap.
+     * If the character is already in the hashmap, then move the left pointer to the right of the same character last found.
+     * Note that the two pointers can only move forward.
+     *
      * e.g.
      * s = "abcab"
      *
