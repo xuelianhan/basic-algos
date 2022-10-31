385c456e5dca8d626f9417b437790abce6916673
xuelianhan <xueliansniper@gmail.com>
2022-10-31 16:46:21 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/array/ToeplitzMatrix.java
+++ src/main/java/org/ict/algorithm/leetcode/array/ToeplitzMatrix.java
@@ -35,13 +35,57 @@ package org.ict.algorithm.leetcode.array;
  *
  * Follow up:
  *
- * What if the matrix is stored on disk, and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
+ * What if the matrix is stored on disk,
+ * and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
  * What if the matrix is so large that you can only load up a partial row into the memory at once?
+ *
  * @author sniper
  * @date 31 Oct, 2022
  * LC766
  */
 public class ToeplitzMatrix {
+
+
+    /**
+     * follow up.
+     * todo
+     * @param matrix
+     * @return
+     */
+    public boolean isToeplitzMatrixV2(int[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+            return true;
+        }
+        //todo
+        return true;
+    }
+
+    /**
+     * matrix = [
+     *           [1,2,3,4],
+     *           [5,1,2,3],
+     *           [9,5,1,2]
+     *          ]
+     * i:0, j:0, mat[0][0] == mat[1][1] == 1
+     *      j:1, mat[0][1] == mat[1][2] == 2
+     *      j:2, mat[0][2] == mat[1][3] == 3
+     * i:1, j:0, mat[1][0] == mat[2][1] == 5
+     *      j:1, mat[1][1] == mat[2][2] == 1
+     *      j:2, mat[1][2] == mat[2][3] == 2
+     *
+     * @param matrix
+     * @return
+     */
+    public boolean isToeplitzMatrixV1(int[][] matrix) {
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[i].length - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     public boolean isToeplitzMatrix(int[][] matrix) {
         int m = matrix.length;
