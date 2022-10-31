ea6cbd1262894ded13d8d7b933d7d1206e4dc8d9
xuelianhan <xueliansniper@gmail.com>
2022-10-31 15:10:11 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/slidewindow/SlidingWindowMaximum.java
+++ src/main/java/org/ict/algorithm/leetcode/slidewindow/SlidingWindowMaximum.java
@@ -53,12 +53,62 @@ public class SlidingWindowMaximum {
     /**
      * Understanding the following solution
      * Monotonic Queue Solution
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] maxSlidingWindowV4(int[] nums, int k) {
+        if (nums == null || k <= 0) {
+            return new int[0];
+        }
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        int j = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            /**
+             * Remove index out of range k from the head of the queue.
+             * Kick off the left index out of range, and it no longer participate the subsequent comparision.
+             */
+            if (!queue.isEmpty() && queue.peek() < i - k + 1) {
+                // default pull the head element of the queue, equals the operation of queue.pollFirst()
+                queue.poll();
+            }
+
+            /**
+             * Remove smaller elements in range k from the tail of the queue
+             * This operation
+             */
+            while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
+                queue.pollLast();
+            }
+
+            queue.offer(i);
+            /**
+             * The head of the queue contains the maximum of the current window.
+             * Notice the res array length is [n - k + 1]
+             * both i and j start from 0, so the j index can be replaced with i - k + 1
+             * if k == 3, the first element should be stored at the index of i == 2
+             * 2 >= 3 - 1.
+             * So we start from k - 1, and no need to consider the upper limit, because i < n assure the iteration in proper range.
+             */
+            if (i >= k - 1) {
+                res[j++] = nums[queue.peek()];
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Understanding the following solution
+     * Monotonic Queue Solution
      * Cost 86 ms
      * @param nums
      * @param k
      * @return
      */
-    public static int[] maxSlidingWindowV1(int[] nums, int k) {
+    public static int[] maxSlidingWindowV3(int[] nums, int k) {
         if (nums == null || k <= 0) {
             return new int[0];
         }
@@ -98,6 +148,20 @@ public class SlidingWindowMaximum {
      * provided by <a href="https://leetcode.com/flyingpenguin/">flyingpenguin</a>, an awesome guy!
      * He has finished all the problems on leetcode. So crazy!
      *
+     * We scan the array from 0 to n-1, keep "promising" elements in the deque.
+     * The algorithm is amortized O(n), as each element put and polled once.
+     * At each i, we keep "promising" elements, which are potentially max number in the window [i-(k-1),i] or any subsequent window.
+     * 1.if an element in the deque, and it is out of i-(k-1), we discard them.
+     * We just need to poll from the head, as we are using a deque and elements are ordered as the sequence in the array.
+     *
+     * 2.Now only those elements within [i-(k-1),i] are in the deque.
+     * We then discard elements smaller than a[i] from the tail.
+     * This is because if a[x] <a[i] and x<i, then a[x] has no chance to be the "max" in [i-(k-1),i],
+     * or any other subsequent window: a[i] would always be a better candidate.
+     *
+     * 3.As a result elements in the deque are ordered in both sequence in array and their value.
+     * At each step the head of the deque is the max element in [i-(k-1),i]
+     *
      * Time Cost 33ms
      * Time Complexity: amortised O(N)
      * e.g. k = 3
@@ -112,7 +176,7 @@ public class SlidingWindowMaximum {
      *      queue:2,3, nums[3] < nums[4], pull 3 from the queue, queue:2
      *      queue:2, nums[2] < nums[4], pull 2 from the queue, queue:empty, push 4 into the queue, queue:4, 4 >= 3-1, res[2]=nums[4]=5
      * i:5, queue:4, peek:4, i-k+1=5-3+1=3, peek > 3, nums[4] > nums[5], push 5 into the queue, queue:4,5, 5 >= 3-1, res[3]=nums[4]=5
-     * i:6, queue:4,5, peek:4, i-k+1=6-3+1=4, peek = 4, nums[5] < nums[6], pull 5 from the queue,
+     * i:6, queue:4,5, peek:4, i-k+1=6-3+1=4, peek = 4, nums[5] < nums[6], pull 5 from the queue, queue:4
      *      queue:4, nums[4] < nums[6], pull 4 from the queue, queue: empty, push 6 into the queue:, queue:6, 6 >= 3-1, res[4]=nums[6]=6
      * i:7, queue:6, peek:6, i-k+1=7-3+1=5, peek > 5,
      *      queue:6, nums[6] < nums[7], pull 6 from the queue, queue:empty, push 7 into the queue, queue:7, 7 >= 3-1, res[5]=nums[7]=7
@@ -121,7 +185,7 @@ public class SlidingWindowMaximum {
      * @param k
      * @return
      */
-    public static int[] maxSlidingWindow(int[] nums, int k) {
+    public static int[] maxSlidingWindowV2(int[] nums, int k) {
         if (nums == null || k <= 0) {
             return new int[0];
         }
@@ -160,6 +224,74 @@ public class SlidingWindowMaximum {
                 res[j] = nums[queue.peek()];
                 j++;
             }
+        }
+        return res;
+    }
+
+    /**
+     * Brute-Force Slide Window Solution, only for understanding the question better.
+     * Time Complexity O(N*k)
+     *
+     * Time Cost: Time Limit Exceeded
+     *
+     * Check every sliding window and compute the maximum value
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] maxSlidingWindowV1(int[] nums, int k) {
+        if (nums == null || k <= 0) {
+            return new int[0];
+        }
+
+        int n = nums.length;
+        int size = n - k + 1;
+        int[] res = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            int max = nums[i];
+            for (int left = i + 1, right = i + k - 1;left <= right; left++) {
+                if (nums[left] > max) {
+                    max = nums[left];
+                }
+            }
+            res[i] = max;
+        }
+        return res;
+    }
+
+    /**
+     * Brute-Force Slide Window Solution, only for understanding the question better.
+     * Time Complexity O(N*k)
+     *
+     * Time Cost: Time Limit Exceeded
+     *
+     * Check every sliding window and compute the maximum value
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || k <= 0) {
+            return new int[0];
+        }
+
+        int n = nums.length;
+        int size = n - k + 1;
+        int[] res = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            int max = nums[i];
+            int left = i + 1, right = i + k - 1;
+            while(left <= right) {
+                if (nums[left] > max) {
+                    max = nums[left];
+                }
+                left++;
+            }
+            res[i] = max;
         }
         return res;
     }
