c1a9d8b9d0dffb28ea730cd2c55c8514faef0826
xuelianhan <xueliansniper@gmail.com>
2022-11-02 00:02:39 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/acwing/DreamOfMondrian.java
@@ -0,0 +1,10 @@
+package org.ict.algorithm.acwing;
+
+/**
+ * @see <a href="https://www.acwing.com/problem/content/description/293/"></a>
+ * @author sniper
+ * @date 01 Nov, 2022
+ * AW293
+ */
+public class DreamOfMondrian {
+}
