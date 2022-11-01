b9548db47e417815447fbf0edc2c3b6d8a6a08ac
xuelianhan <xueliansniper@gmail.com>
2022-11-01 14:54:35 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/backtrack/PermutationsII.java
+++ src/main/java/org/ict/algorithm/leetcode/backtrack/PermutationsII.java
@@ -43,6 +43,56 @@ public class PermutationsII {
         return result;
     }
 
+    /**
+     * Cost 2ms
+     * 1.i > 0 && nums[i] == nums[i-1] && !visited[i-1]
+     * 2.i > 0 && nums[i] == nums[i-1] && visited[i-1]
+     * Why using 1 is faster than 2?
+     * What's the difference of 1 and 2?
+     * 
+     *
+     * @param nums
+     * @param path
+     * @param visited
+     * @param result
+     */
+    public void dfsV1(int[] nums, LinkedList<Integer> path, boolean[] visited,  List<List<Integer>> result) {
+        if (path.size() == nums.length) {
+            //Make a deep copy of path here, otherwise we'd be append the same path over and over
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            /**
+             *  skip visited num
+             *  skip duplicated items, this step requires the input array has sorted already.
+             *  i > 0 && nums[i] == nums[i-1] && !visited[i-1]
+             *
+             */
+            if (visited[i] || i > 0 && nums[i] == nums[i-1] && !visited[i-1]) {
+                continue;
+            }
+            /**
+             * add num on the path and mark num as visited.
+             */
+            path.add(nums[i]);
+            visited[i] = true;
+            dfsV1(nums, path, visited, result);
+            /**
+             * remove num off the path and mark num as unvisited.
+             */
+            path.removeLast();
+            visited[i] = false;
+        }
+    }
+
+    /**
+     * Cost 7ms
+     * @param nums
+     * @param path
+     * @param visited
+     * @param result
+     */
     public void dfs(int[] nums, LinkedList<Integer> path, boolean[] visited,  List<List<Integer>> result) {
         if (path.size() == nums.length) {
             //Make a deep copy of path here, otherwise we'd be append the same path over and over
@@ -53,6 +103,7 @@ public class PermutationsII {
             /**
              *  skip visited num
              *  skip duplicated items, this step requires the input array has sorted already.
+             *  i > 0 && nums[i] == nums[i-1] && visited[i-1]
              */
             if (visited[i] || i > 0 && nums[i] == nums[i-1] && visited[i-1]) {
                 continue;
