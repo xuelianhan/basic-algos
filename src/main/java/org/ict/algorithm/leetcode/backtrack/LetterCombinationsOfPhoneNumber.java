7cf82d45f0b6f04375975fbba97c1833f130b999
xuelianhan <xueliansniper@gmail.com>
2022-11-02 08:26:46 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/backtrack/LetterCombinationsOfPhoneNumber.java
+++ src/main/java/org/ict/algorithm/leetcode/backtrack/LetterCombinationsOfPhoneNumber.java
@@ -47,36 +47,98 @@ public class LetterCombinationsOfPhoneNumber {
 
     /**
      * Understanding the following Breadth-First-Search Solution
-     * 
+     * Same as letterCombinationsV1 but without outer-for-loop
+     *
+     * Time Cost 8ms
+     *
      * @param digits
      * @return
      */
     public List<String> letterCombinationsV2(String digits) {
-        return null;
-    }
-
-
-    /**
-     * Understanding the following Breadth-First-Search Solution
-     *
-     *
-     * @see <a href="https://afteracademy.com/blog/letter-combinations-of-a-phone-number"></a>
-     *
-     * @param digits
-     * @return
-     */
-    public List<String> letterCombinationsV1(String digits) {
-        String[] map = new String[] {"0",   "1",   "abc",  "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         LinkedList<String> queue = new LinkedList<>();
         if (digits.isEmpty()) {
             return queue;
+        }
+
+        /**
+         * Add "0", "1" here for sake of the array index from 2 to 9.
+         */
+        String[] map = new String[] {"0",   "1",   "abc",  "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        queue.add("");
+        while (queue.peek().length() != digits.length()) {
+            String head = queue.poll();
+            int idx = digits.charAt(head.length()) - '0';
+            for (char ch : map[idx].toCharArray()) {
+                queue.offer(head + ch);
+            }
         }
         return queue;
     }
 
 
     /**
+     * Understanding the following Breadth-First-Search Solution
+     *
+     * Time Cost 11 ms
+     *
+     *
+     * @see <a href="https://afteracademy.com/blog/letter-combinations-of-a-phone-number"></a>
+     *
+     * e.g. digits = "23"
+     * i:0, digit = 2, queue:"", queue.peek.length == 0, head:"", for-loop:"abc", queue:"a", "b", "c"
+     * i:1, digit = 3, queue:"a", "b", "c",
+     *                 queue.peek.length == 1, head:"a", queue:"b", "c",
+     *                                        for-loop:"def", queue:"b", "c", "ad", "ae", "af"
+     *                 queue.peek.length == 1, head:"b", queue:"c", "ad", "ae", "af",
+     *                                        for-loop:"def", queue: "c", "ad", "ae", "af", "bd", "be", "bf"
+     *                 queue.peek.length == 1, head:"c", queue: "ad", "ae", "af", "bd", "be", "bf",
+     *                                        for-loop:"def", queue: "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"
+     *
+     * @param digits
+     * @return
+     */
+    public List<String> letterCombinationsV1(String digits) {
+        LinkedList<String> queue = new LinkedList<>();
+        if (digits.isEmpty()) {
+            return queue;
+        }
+        /**
+         * Add "0", "1" here for sake of the array index from 2 to 9.
+         */
+        String[] map = new String[] {"0",   "1",   "abc",  "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        queue.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            int digit = digits.charAt(i) - '0';
+            while (queue.peek().length() == i) {
+                /**
+                 * Both remove and poll operations retrieve and remove the head (first element) of this list.
+                 * So remove or poll are both satisfied here.
+                 */
+                //queue.remove();
+                String head = queue.poll();
+                for (char ch : map[digit].toCharArray()) {
+                    /**
+                     * Don't use push, push operation inserts the element at the front of this list.
+                     * push equals addFirst.
+                     * queue.push(head + ch);
+                     *
+                     * add and offer operation appends the specified element to the end of this list.
+                     * add equals addLast, also equal offer and offerLast.
+                     */
+                    //queue.add(head + ch);
+                    queue.offer(head + ch);
+                }
+            }
+        }
+
+        return queue;
+    }
+
+
+    /**
      * Understanding the following Backtracking Solution
+     *
+     * Time Cost 1ms
      *
      * @param digits
      * @return
