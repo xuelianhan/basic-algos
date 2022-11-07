ee526ca5318fa30f39c731d42a727c9795d06de0
xuelianhan <xueliansniper@gmail.com>
2022-11-07 21:46:53 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/backtrack/WordSearchII.java
+++ src/main/java/org/ict/algorithm/leetcode/backtrack/WordSearchII.java
@@ -76,6 +76,31 @@ public class WordSearchII {
         return res;
     }
 
+    /**
+     * Intuitively, start from every cell and try to build a word in the dictionary.
+     * Backtracking (dfs) is the powerful way to exhaust every possible ways.
+     * Apparently, we need to do pruning when current character is not in any word.
+     *
+     * How do we instantly know the current character is invalid? HashMap?
+     * How do we instantly know what's the next valid character? LinkedList?
+     * But the next character can be chosen from a list of characters. "Mutil-LinkedList"?
+     * Combing them, Trie is the natural choice. Notice that:
+     *
+     * TrieNode is all we need. search and startsWith are useless.
+     * No need to store character at TrieNode. c.next[i] != null is enough.
+     * Never use c1 + c2 + c3. Use StringBuilder.
+     * No need to use O(n^2) extra space visited[m][n].
+     * No need to use StringBuilder. Storing word itself at leaf node is enough.
+     * No need to use HashSet to de-duplicate. Use "one time search" trie.
+     * For more explanations, check out dietpepsi's blog.
+     *
+     * @author yavinci
+     * @param board
+     * @param i
+     * @param j
+     * @param p
+     * @param res
+     */
     public void searchV2(char[][] board, int i, int j, TrieNode p, List<String> res) {
         char c = board[i][j];
         if (c == '#' || p.children[c - 'a'] == null) {
