331b4bce241754e8af65ba0525ad51da426801c7
xuelianhan <xueliansniper@gmail.com>
2022-11-10 13:07:37 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/trie/WordBreak.java
+++ src/main/java/org/ict/algorithm/leetcode/trie/WordBreak.java
@@ -41,12 +41,18 @@ import java.util.*;
  */
 public class WordBreak {
 
+    public static void main(String[] args) {
+        String s = "catsandog";
+        String[] dict = {"cats","dog","sand","and","cat"};
+        wordBreak(s, new ArrayList<>(Arrays.asList(dict)));
+    }
+
     /**
      * Recursive solution with Memorization.
      * You can subscribe Huahua's channel on YT.
      * @author Huahua
      */
-    public boolean wordBreak(String s, List<String> wordDict) {
+    public static boolean wordBreak(String s, List<String> wordDict) {
         Set<String> dict = new HashSet<>(wordDict);
         Map<String, Boolean> memo = new HashMap<>();
         return wordBreak(s, memo, dict);
@@ -60,23 +66,31 @@ public class WordBreak {
      * How can we split? from left to right? If we split at the first place, then how to deal with the rest part?
      *
      * e.g. s = "catsandog", dict = ["cats","dog","sand","and","cat"]
+     *
      * inDict("atsandog") && wordBreak("c"); --> false, because "atsandog" is not in the dict
      * inDict("tsandog") && wordBreak("ca"); --> false, because "tsandog" is not in the dict
      * inDict("sandog") && wordBreak("cat"); --> false, because "sandog" is not in the dict
      * inDict("andog") && wordBreak("cats"); --> false, because "andog" is not in the dict
      * inDict("ndog") && wordBreak("catsa"); --> false, because "ndog" is not in the dict
      * inDict("dog") && wordBreak("catsan"); --> "dog" in the dict, recursive to break "catsan"
+     *                  inDict("atsan") && wordBreak("c"); --> false, because "astan" is not in the dict
+     *                  inDict("tsan") && wordBreak("ca"); --> false, because "tsan" is not in the dict
+     *                  inDict("san") && wordBreak("cat"); --> false, because "san" is not in the dict
+     *                  inDict("an") && wordBreak("cats"); --> false, because "an" is not in the dict
+     *                  inDict("n") && wordBreak("catsa"); --> false, because "n" is not in the dict
+     *                  return wordBreak("catsan"); --> false
      * inDict("og") && wordBreak("catsand"); --> false, because "og" is not in the dict
      * inDict("g") && wordBreak("catsando"); --> false, because "g" is not in the dict
-     * inDict("") && wordBreak("catsandog"); --> repeat the above steps.
+     * return wordBreak("catsandog");
+     *
      * @param s
      * @param memo
      * @param dict
      * @return
      */
-    public boolean wordBreak(String s, Map<String, Boolean> memo, Set<String> dict) {
+    public static boolean wordBreak(String s, Map<String, Boolean> memo, Set<String> dict) {
         /**
-         * Return directly if s has existed in memo.
+         * Return directly if s has existed in the memo.
          */
         if (memo.containsKey(s)) {
             return memo.get(s);
@@ -94,6 +108,7 @@ public class WordBreak {
         for (int i = 1; i < s.length(); i++) {
             String left = s.substring(0, i);
             String right = s.substring(i);
+            System.out.println("inDict(" + right + ") && wordBreak(" + left + ")");
             if (dict.contains(right) && wordBreak(left, memo, dict)) {
                 memo.put(s, true);
                 return true;
