412e14913b5b17a9b7da249b1810891288758b8c
xuelianhan <xueliansniper@gmail.com>
2022-11-24 20:24:07 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/leetcode/array/GroupAnagrams.java
@@ -0,0 +1,44 @@
+package org.ict.algorithm.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an array of strings strs, group the anagrams together.
+ * You can return the answer in any order.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * Example 2:
+ *
+ * Input: strs = [""]
+ * Output: [[""]]
+ * Example 3:
+ *
+ * Input: strs = ["a"]
+ * Output: [["a"]]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= strs.length <= 10^4
+ * 0 <= strs[i].length <= 100
+ * strs[i] consists of lowercase English letters.
+ * @author sniper
+ * @date 24 Nov, 2022
+ * LC49
+ */
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        return result;
+    }
+}
