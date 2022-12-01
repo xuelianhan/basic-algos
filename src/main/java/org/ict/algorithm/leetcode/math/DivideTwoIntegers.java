69b98ec93bf4dbf58cb4f8849538d0d31cd1d371
xuelianhan <xueliansniper@gmail.com>
2022-12-01 19:06:15 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/math/DivideTwoIntegers.java
+++ src/main/java/org/ict/algorithm/leetcode/math/DivideTwoIntegers.java
@@ -58,6 +58,13 @@ public class DivideTwoIntegers {
         return 0;
     }
 
+    /**
+     * e.g.
+     * dividend = -2147483648, divisor = 1
+     * @param dividend
+     * @param divisor
+     * @return
+     */
     public int divide(int dividend, int divisor) {
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
