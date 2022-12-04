e406f47cd8ba4184a0ef0d5b535762dc042a5cae
xuelianhan <xueliansniper@gmail.com>
2022-12-04 13:35:08 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/bitvector/SortIntegersByNumberOf1Bits.java
+++ src/main/java/org/ict/algorithm/leetcode/bitvector/SortIntegersByNumberOf1Bits.java
@@ -7,7 +7,8 @@ import java.util.PriorityQueue;
 
 /**
  * You are given an integer array arr.
- * Sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
+ * Sort the integers in the array in ascending order by the number of 1's
+ * in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
  *
  * Return the array after sorting it.
  *
