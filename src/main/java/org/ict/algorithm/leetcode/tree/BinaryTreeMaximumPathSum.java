c7850d9e7be0a048978e779dae28879979e778ff
xuelianhan <xueliansniper@gmail.com>
2022-12-11 19:31:55 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/tree/BinaryTreeMaximumPathSum.java
+++ src/main/java/org/ict/algorithm/leetcode/tree/BinaryTreeMaximumPathSum.java
@@ -43,20 +43,51 @@ public class BinaryTreeMaximumPathSum {
         instance.maxPathSum(root);
     }
 
+    /**
+     * Time Cost 1ms
+     * @param root
+     * @return
+     */
+    public int maxPathSumV1(TreeNode root) {
+        int[] res = new int[1];
+        res[0] = Integer.MIN_VALUE;
+        helperV1(root, res);
+        return res[0];
+    }
+
+    public int helperV1(TreeNode root, int[] res) {
+        if (null == root) {//1
+            return 0;
+        }
+        int left = Math.max(0, helperV1(root.left, res));//2
+        int right = Math.max(0, helperV1(root.right, res));//3
+        res[0] = Math.max(res[0], left + right + root.val);//4
+        return Math.max(left, right) + root.val;//used in 2 and 3
+    }
+
+
     private int ans;
 
+    /**
+     * Time Cost 0ms
+     * Time Complexity O(N)
+     * Space Complexity O(N)
+     * 
+     * @param root
+     * @return
+     */
     public int maxPathSum(TreeNode root) {
         ans = Integer.MIN_VALUE;
-        recursion(root);
+        helper(root);
         return ans;
     }
 
-    public int recursion(TreeNode root) {
+    public int helper(TreeNode root) {
          if (null == root) {//1
              return 0;
          }
-         int left = Math.max(0, recursion(root.left));//2
-         int right = Math.max(0, recursion(root.right));//3
+         int left = Math.max(0, helper(root.left));//2
+         int right = Math.max(0, helper(root.right));//3
          ans = Math.max(ans, left + right + root.val);//4
          return Math.max(left, right) + root.val;//used in 2 and 3
     }
