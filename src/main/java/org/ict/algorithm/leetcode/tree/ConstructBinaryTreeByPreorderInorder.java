bb4baa92f012c1957f49a3dcb7877b1bb1384502
xuelianhan <xueliansniper@gmail.com>
2022-12-14 20:58:31 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/tree/ConstructBinaryTreeByPreorderInorder.java
+++ src/main/java/org/ict/algorithm/leetcode/tree/ConstructBinaryTreeByPreorderInorder.java
@@ -34,13 +34,74 @@ package org.ict.algorithm.leetcode.tree;
  */
 public class ConstructBinaryTreeByPreorderInorder {
 
+    public TreeNode buildTreeV5(int[] preorder, int[] inorder) {
+        //todo
+        return null;
+    }
+
+    public TreeNode buildTreeV4(int[] preorder, int[] inorder) {
+        //todo
+        return null;
+    }
+
+
+    public TreeNode buildTreeV3(int[] preorder, int[] inorder) {
+        //todo
+        return null;
+    }
+
+    public TreeNode buildTreeV2(int[] preorder, int[] inorder) {
+        //todo
+        return null;
+    }
+
+
     public TreeNode buildTreeV1(int[] preorder, int[] inorder) {
         //todo
         return null;
     }
 
+    /**
+     * Understand the following Solution
+     * 
+     * Time Cost 3ms
+     * preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+     * root:3, left:9, right:15, 20, 7
+     * root:9, skip
+     * root:20, left:15, right:7
+     * ---------------------------
+     * 0  1   2   3  4
+     * 3  9  20  15  7
+     * 9 [3] 15  20  7
+     *
+     *
+     * @param preorder
+     * @param inorder
+     * @return
+     */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        //todo
-        return null;
+        int n = preorder.length;
+        return helper(preorder, 0, n - 1, inorder, 0, n - 1);
+    }
+
+    public TreeNode helper(int[] preorder, int pStart, int pEnd, int[] inorder, int iStart, int iEnd) {
+        if (pStart > pEnd || iStart > iEnd) {
+            return null;
+        }
+        int i = 0;
+        for (i = iStart; i <= iEnd; i++) {
+            if (preorder[pStart] == inorder[i]) {
+                break;
+            }
+        }
+        /**
+         * preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+         * pStart:0, i:1
+         *
+         */
+        TreeNode cur = new TreeNode(preorder[pStart]);
+        cur.left = helper(preorder, pStart + 1, pStart + i - iStart, inorder, iStart, i - 1);
+        cur.right = helper(preorder, pStart + i - iStart + 1, pEnd, inorder, i + 1, iEnd);
+        return cur;
     }
 }
