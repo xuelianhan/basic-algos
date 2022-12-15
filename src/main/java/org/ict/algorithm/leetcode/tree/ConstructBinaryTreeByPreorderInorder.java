074d2188f4a513fd227b21a82d8eb7ac4adb7940
xuelianhan <xueliansniper@gmail.com>
2022-12-15 18:19:01 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/tree/ConstructBinaryTreeByPreorderInorder.java
+++ src/main/java/org/ict/algorithm/leetcode/tree/ConstructBinaryTreeByPreorderInorder.java
@@ -1,5 +1,8 @@
 package org.ict.algorithm.leetcode.tree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given two integer arrays preorder and inorder where preorder is the preorder traversal
  * of a binary tree and inorder is the inorder traversal of the same tree,
@@ -34,31 +37,91 @@ package org.ict.algorithm.leetcode.tree;
  */
 public class ConstructBinaryTreeByPreorderInorder {
 
-    public TreeNode buildTreeV5(int[] preorder, int[] inorder) {
-        //todo
-        return null;
-    }
-
+    /**
+     * Stack Iterative Solution
+     * @param preorder
+     * @param inorder
+     * @return
+     */
     public TreeNode buildTreeV4(int[] preorder, int[] inorder) {
-        //todo
         return null;
     }
 
-
+    /**
+     * Stack Iterative Solution
+     * @param preorder
+     * @param inorder
+     * @return
+     */
     public TreeNode buildTreeV3(int[] preorder, int[] inorder) {
-        //todo
         return null;
     }
 
+
+    /**
+     * Time Cost 2ms
+     * After converting inorder[] to the HashMap,
+     * we don't need inorder[] anymore in the helper function.
+     * @param preorder
+     * @param inorder
+     * @return
+     */
     public TreeNode buildTreeV2(int[] preorder, int[] inorder) {
-        //todo
-        return null;
+        int n = preorder.length;
+        int[] preorderIndex = new int[1];
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return helpV2(preorder, 0, n - 1, indexMap, preorderIndex);
+    }
+
+    public TreeNode helpV2(int[] preorder, int pStart, int pEnd, Map<Integer, Integer> indexMap, int[] preorderIndex) {
+        if (pStart > pEnd) {
+            return null;
+        }
+        int val = preorder[preorderIndex[0]++];
+        int i = indexMap.get(val);
+        TreeNode cur = new TreeNode(val);
+        cur.left = helpV2(preorder, pStart, i - 1, indexMap, preorderIndex);
+        cur.right = helpV2(preorder, i + 1, pEnd, indexMap, preorderIndex);
+        return cur;
     }
 
 
+    /**
+     *
+     * Understand the following Solution
+     * Time Cost 2ms
+     * Using indexMap to speed up.
+     *
+     * @param preorder
+     * @param inorder
+     * @return
+     */
     public TreeNode buildTreeV1(int[] preorder, int[] inorder) {
-        //todo
-        return null;
+        int n = preorder.length;
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return helperV1(preorder, 0, n - 1, inorder, 0, n - 1, indexMap);
+    }
+
+    public TreeNode helperV1(int[] preorder, int pStart, int pEnd, int[] inorder, int iStart, int iEnd, Map<Integer, Integer> indexMap) {
+        if (pStart > pEnd || iStart > iEnd) {
+            return null;
+        }
+        int i = indexMap.get(preorder[pStart]);
+        /**
+         * preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+         * pStart:0, i:1
+         *
+         */
+        TreeNode cur = new TreeNode(preorder[pStart]);
+        cur.left = helperV1(preorder, pStart + 1, pStart + i - iStart, inorder, iStart, i - 1, indexMap);
+        cur.right = helperV1(preorder, pStart + i - iStart + 1, pEnd, inorder, i + 1, iEnd, indexMap);
+        return cur;
     }
 
     /**
@@ -96,7 +159,7 @@ public class ConstructBinaryTreeByPreorderInorder {
         }
         /**
          * preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
-         * pStart:0, i:1
+         * pStart:0, i:1, 
          *
          */
         TreeNode cur = new TreeNode(preorder[pStart]);
