25458b9855a5f1f43c481e17eb13fbb7c8f6593e
xuelianhan <xueliansniper@gmail.com>
2022-12-17 07:40:35 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/design/ImplementQueueUsingStacks.java
+++ src/main/java/org/ict/algorithm/leetcode/design/ImplementQueueUsingStacks.java
@@ -3,7 +3,8 @@ package org.ict.algorithm.leetcode.design;
 import java.util.Stack;
 
 /**
- * Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+ * Implement a first in first out (FIFO) queue using only two stacks.
+ * The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
  *
  * Implement the MyQueue class:
  *
@@ -14,7 +15,8 @@ import java.util.Stack;
  * Notes:
  *
  * You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
- * Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+ * Depending on your language, the stack may not be supported natively.
+ * You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
  *
  *
  * Example 1:
@@ -65,6 +67,10 @@ public class ImplementQueueUsingStacks {
 
         public MyQueue() {}
 
+        /**
+         * Newly elements all being pushed into newStack.
+         * @param x
+         */
         public void push(int x) {
            newStack.push(x);
         }
@@ -83,6 +89,9 @@ public class ImplementQueueUsingStacks {
             return oldStack.isEmpty() && newStack.isEmpty();
         }
 
+        /**
+         * Transfer elements from newStack to oldStack.
+         */
         private void shiftStack() {
             if (!oldStack.isEmpty()) {
                 return;
