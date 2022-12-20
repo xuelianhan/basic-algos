40762897f87bde9efd2dbf80a1a1aeba23b88c3a
xuelianhan <xueliansniper@gmail.com>
2022-12-20 12:48:31 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/math/PrimeChecker.java
+++ src/main/java/org/ict/algorithm/leetcode/math/PrimeChecker.java
@@ -1,11 +1,25 @@
 package org.ict.algorithm.leetcode.math;
 
+import java.math.BigInteger;
+
 /**
  * Check a number is prime or not.
  * @author sniper
  * @date 04 Dec, 2022
  */
 public class PrimeChecker {
+
+    public static void main(String[] args) {
+        PrimeChecker instance = new PrimeChecker();
+        int[] arr = {2, 3, 31, 71, 101, 103, 107, 131, 151, 181, 191, 199};
+        for (int num : arr) {
+            if (instance.isPrimeV1(num)) {
+                System.out.println(num + " is a prime");
+            } else {
+                System.out.println(num + " is not prime");
+            }
+        }
+    }
 
     /**
      * If a positive integer is a composite number,
@@ -30,5 +44,10 @@ public class PrimeChecker {
             }
         }
         return true;
+    }
+
+    public boolean isPrimeV1(int num) {
+        BigInteger x = BigInteger.valueOf(num);
+        return x.isProbablePrime(1);
     }
 }
