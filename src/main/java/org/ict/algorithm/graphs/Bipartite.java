54c457db9b1b848d8551c53927bedbdbf27a7158
xuelianhan <xueliansniper@gmail.com>
2022-12-21 11:03:42 +0800

    add codes

--- src/main/java/org/ict/algorithm/graphs/Bipartite.java
+++ src/main/java/org/ict/algorithm/graphs/Bipartite.java
@@ -1,5 +1,7 @@
 package org.ict.algorithm.graphs;
 
+import org.ict.algorithm.fundamentals.Stack;
+
 /**
  * The {@code Bipartite} class represents a data type for 
  * determining whether an undirected graph is bipartite or whether
@@ -24,6 +26,69 @@ package org.ict.algorithm.graphs;
  * @author Kevin Wayne
  */
 public class Bipartite {
+    /**
+     * Is the graph bipartite?
+     */
+    private boolean isBipartite;
+    /**
+     * color[v] gives vertices on one side of bi-partition.
+     */
+    private boolean[] color;
+    /**
+     * marked[v] = true if v has been visited in DFS
+     */
+    private boolean[] marked;
+    /**
+     * edgeTo[v] = last edge on the path to v.
+     */
+    private int[] edgeTo;
+    /**
+     * odd-length cycle
+     */
+    private Stack<Integer> cycle;
 
 
+    public Bipartite(Graph G) {
+        this.isBipartite = true;
+        this.color = new boolean[G.V()];
+        this.marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+
+        for (int v = 0; v < G.V(); v++) {
+            if (!marked[v]) {
+                dfs(G, v);
+            }
+        }
+        assert check(G);
+    }
+
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            /**
+             * short-circuit if odd-length cycle found
+             */
+            if (cycle != null) {
+                return;
+            }
+
+            /**
+             * found the uncolored vertex, so recur.
+             */
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                color[w] = !color[v];
+                dfs(G, w);
+            } else if (color[w] == color[v]) {
+                /**
+                 * if v-w create an odd-length cycle, find it.
+                 */
+            }
+
+        }
+    }
+
+    private boolean check(Graph G) {
+        return false;
+    }
 }
---
+++ src/main/java/org/ict/algorithm/graphs/BipartiteX.java
@@ -0,0 +1,8 @@
+package org.ict.algorithm.graphs;
+
+/**
+ * @author sniper
+ * @date 21 Dec, 2022
+ */
+public class BipartiteX {
+}
