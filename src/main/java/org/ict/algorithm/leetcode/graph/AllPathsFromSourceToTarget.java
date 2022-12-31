1a0227af664b7dfdb89498caa029abc0aa4a7233
xuelianhan <xueliansniper@gmail.com>
2022-12-31 18:44:59 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/graph/AllPathsFromSourceToTarget.java
+++ src/main/java/org/ict/algorithm/leetcode/graph/AllPathsFromSourceToTarget.java
@@ -1,7 +1,6 @@
 package org.ict.algorithm.leetcode.graph;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1,
@@ -39,15 +38,29 @@ import java.util.List;
  */
 public class AllPathsFromSourceToTarget {
 
+
+    /**
+     * Time Cost 1ms
+     * Understanding the following solution.
+     *
+     * @param graph
+     * @return
+     */
     public List<List<Integer>> allPathsSourceTargetV2(int[][] graph) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
+        /**
+         * Add the source node-0 to the path.
+         */
         path.add(0);
         backtrackV2(graph, result, path, 0);
         return result;
     }
 
     private void backtrackV2(int[][] graph, List<List<Integer>> result, List<Integer> path, int cur) {
+        /**
+         * If current node is the n-1, it means we have arrived at the destination.
+         */
         if (cur == graph.length - 1) {
             result.add(new ArrayList<>(path));
             return;
@@ -128,6 +141,35 @@ public class AllPathsFromSourceToTarget {
             path.remove(path.size() - 1);
         }
         visited[cur] = false;
+    }
+
+    /**
+     * Breadth-First-Search Solution
+     * Time Cost 7ms
+     * @param graph
+     * @return
+     */
+    public List<List<Integer>> allPathsSourceTargetV3(int[][] graph) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<List<Integer>> queue = new ArrayDeque<>();
+        /**
+         * Add the source node-0 to the queue.
+         */
+        queue.add(Arrays.asList(0));
+        while (!queue.isEmpty()) {
+            List<Integer> path = queue.poll();
+            int cur = path.get(path.size() - 1);
+            if (cur == graph.length - 1) {
+                result.add(new ArrayList<>(path));
+            }
+
+            for (int neighbor : graph[cur]) {
+                List<Integer> newPath = new ArrayList<>(path);
+                newPath.add(neighbor);
+                queue.offer(newPath);
+            }
+        }
+        return result;
     }
 
 }
