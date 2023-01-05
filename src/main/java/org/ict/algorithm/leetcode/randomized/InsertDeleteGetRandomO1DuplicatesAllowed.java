d7713e90ec3cdd5282535148b59fc8761aae0d14
xuelianhan <xueliansniper@gmail.com>
2023-01-05 14:28:19 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/randomized/InsertDeleteGetRandomO1DuplicatesAllowed.java
+++ src/main/java/org/ict/algorithm/leetcode/randomized/InsertDeleteGetRandomO1DuplicatesAllowed.java
@@ -1,5 +1,7 @@
 package org.ict.algorithm.leetcode.randomized;
 
+import java.util.*;
+
 /**
  * RandomizedCollection is a data structure that contains a collection of numbers,
  * possibly duplicates (i.e., a multiset). It should support inserting and removing specific elements and also removing a random element.
@@ -8,9 +10,9 @@ package org.ict.algorithm.leetcode.randomized;
  *
  * RandomizedCollection() Initializes the empty RandomizedCollection object.
  * bool insert(int val) Inserts an item val into the multiset, even if the item is already present.
- * Returns true if the item is not present, false otherwise.
+ *                      Returns true if the item is not present, false otherwise.
  * bool remove(int val) Removes an item val from the multiset if present.
- * Returns true if the item is present, false otherwise.
+ *                      Returns true if the item is present, false otherwise.
  * Note that if val has multiple occurrences in the multiset, we only remove one of them.
  * int getRandom() Returns a random element from the current multiset of elements.
  * The probability of each element being returned is linearly related to the number of same values the multiset contains.
@@ -52,6 +54,7 @@ package org.ict.algorithm.leetcode.randomized;
  * @author sniper
  * @date 05 Jan, 2023
  * LC381
+ * Phone Interview Question.
  */
 public class InsertDeleteGetRandomO1DuplicatesAllowed {
 
@@ -62,23 +65,95 @@ public class InsertDeleteGetRandomO1DuplicatesAllowed {
      * boolean param_1 = obj.insert(val);
      * boolean param_2 = obj.remove(val);
      * int param_3 = obj.getRandom();
+     * Time Cost 86ms
      */
     class RandomizedCollection {
 
+        private List<Integer> nums;
+
+        private Map<Integer, Set<Integer>> map;
+
+        private Random rand;
+
         public RandomizedCollection() {
-
+            nums = new ArrayList<>();
+            map = new HashMap<>();
+            rand = new Random();
         }
 
+        /**
+         * Inserts an item val into the multiset,
+         * even if the item is already present.
+         *
+         * e.g.nums:[3, 3, 3, 2, 2, 1]
+         * map:(1, (5)), (2, (3, 4)), (3, (0, 1, 2))
+         *
+         * @param val
+         * @return true if the item is not present, false otherwise.
+         */
         public boolean insert(int val) {
-            return false;
+            boolean exist = map.containsKey(val);
+            if (!exist) {
+                map.put(val, new HashSet<>());
+            }
+            map.get(val).add(nums.size());
+            nums.add(val);
+            return !exist;
         }
 
+        /**
+         * Removes an item val from the multiset if present.
+         *
+         * e.g.nums:[3, 3, 3, 2, 2, 1]
+         * map:(1, (5)), (2, (3, 4)), (3, (0, 1, 2))
+         *
+         * remove(3)
+         * idx:0, map.get(val).remove(idx) = map.get(3).remove(0)
+         * map:(1, (5)), (2, (3, 4)), (3, (1, 2))
+         *
+         * idx:0, idx < 5, last = 1
+         * nums.set(idx, last)
+         * nums:[1, 3, 3, 2, 2, 1]
+         *
+         * map.get(last).add(idx) = map.get(1).add(0)
+         * map:(1, (5, 0)), (2, (3, 4)), (3, (1, 2))
+         *
+         * map.get(last).remove(nums.size() - 1) = map.get(1).remove(5)
+         * map:(1, (0)), (2, (3, 4)), (3, (1, 2))
+         *
+         * nums.remove(nums.size() - 1) = nums.remove(5)
+         * nums:[1, 3, 3, 2, 2]
+         *
+         * @param val
+         * @return true if the item is present, false otherwise.
+         */
         public boolean remove(int val) {
-            return false;
+            if (!map.containsKey(val)) {
+                return false;
+            }
+            int idx = map.get(val).iterator().next();
+            map.get(val).remove(idx);
+            /**
+             * If val is not the last element, overwrite val at val's index of nums with the last element.
+             * Add new binding relationship of last element in the map.
+             * Remove old binding relationship of last element in the map.
+             */
+            if (idx < nums.size() - 1) {
+                int last = nums.get(nums.size() - 1);
+                nums.set(idx, last);
+                map.get(last).add(idx);
+
+                map.get(last).remove(nums.size() - 1);
+            }
+            if (map.get(val).isEmpty()) {
+                map.remove(val);
+            }
+            nums.remove(nums.size() - 1);
+            return true;
         }
 
         public int getRandom() {
-            return 0;
+            return nums.get(rand.nextInt(nums.size()));
         }
     }
 }
