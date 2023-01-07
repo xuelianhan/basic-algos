bd0bbf9cc91373bc150b32f66a272f67262d4fbd
xuelianhan <xueliansniper@gmail.com>
2023-01-07 17:10:26 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/greedy/CanPlaceFlowers.java
+++ src/main/java/org/ict/algorithm/leetcode/greedy/CanPlaceFlowers.java
@@ -44,14 +44,87 @@ public class CanPlaceFlowers {
     }
 
     /**
+     * We can find out the extra maximum number of flowers, count,
+     * that can be planted for the given flowerbed arrangement.
+     * To do so, we can traverse over all the elements of the flowerbed and find out those elements which are 0(implying an empty position).
+     * For every such element, we check if its both adjacent positions are also empty.
+     * If so, we can plant a flower at the current position without violating the no-adjacent-flowers-rule.
+     * For the first and last elements, we need not check the previous and the next adjacent positions respectively.
+     * If the count obtained is greater than or equal to n,
+     * the required number of flowers to be planted,
+     * we can plant nnn flowers in the empty spaces, otherwise not.
+     *
+     * @param flowerbed
+     * @param n
+     * @return
+     */
+    public boolean canPlaceFlowersV3(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            /**
+             * if current position is empty, check whether its left and right are both empty or not.
+             */
+            if (flowerbed[i] == 0) {
+                boolean emptyL = (i == 0 || flowerbed[i - 1] == 0);
+                boolean emptyR = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0);
+                if (emptyL && emptyR) {
+                    flowerbed[i] = 1;
+                    count++;
+                    if (count >= n) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return count >= n;
+    }
+
+    /**
+     * Understanding the following Solution.
      * Time Cost 1ms
-     * Similar with canPlaceFlowers but delete the block of checking case of flowerbed.length == 1.
+     * Time Complexity O(N)
+     * Space Complexity O(N)
      * @param flowerbed
      * @param n
      * @return
      */
     public boolean canPlaceFlowersV2(int[] flowerbed, int n) {
         int[] copy = new int[flowerbed.length + 2];
+        /**
+         * Add zero at the start, and the end place of copied array
+         */
+        copy[0] = 0;
+        copy[copy.length - 1] = 0;
+        for (int i = 1; i < copy.length - 1; i++) {
+            copy[i] = flowerbed[i - 1];
+        }
+        int res = 0;
+        for (int i = 1; i + 1 < copy.length;) {
+            if (copy[i - 1] == 0 && copy[i] == 0 && copy[i + 1] == 0) {
+                res++;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        return res >= n;
+    }
+
+    /**
+     * Understanding the following Solution.
+     * Time Cost 1ms
+     * Similar with canPlaceFlowers but delete the block of checking case of flowerbed.length == 1.
+     * Time Complexity O(N)
+     * Space Complexity O(N)
+     * @param flowerbed
+     * @param n
+     * @return
+     */
+    public boolean canPlaceFlowersV1(int[] flowerbed, int n) {
+        int[] copy = new int[flowerbed.length + 2];
+        /**
+         * Add zero at the start, and the end place of copied array
+         */
         copy[0] = 0;
         copy[copy.length - 1] = 0;
         for (int i = 1; i < copy.length - 1; i++) {
@@ -75,6 +148,8 @@ public class CanPlaceFlowers {
      * Time Cost 1ms
      * Copy the original array, and add zero at first and last places of the copy array.
      * e.g. copy = [0, original flowerbed elements here ,0], then check the copy array at three continuous places.
+     * Time Complexity O(N)
+     * Space Complexity O(N)
      *
      * e.g.flowerbed = [1,0,0,0,0,0,1], n = 2, expected:true
      * e.g.flowerbed = [1,0,0,0,0,0,0,0,1,0,0], n = 4, expected: false
