9bb4b4beb3b924a1f809cb241eb3d0821ccced6b
xuelianhan <xueliansniper@gmail.com>
2023-01-07 08:30:12 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/leetcode/greedy/MinimumOperationsToMakeTheArrayIncreasing.java
@@ -0,0 +1,72 @@
+package org.ict.algorithm.leetcode.greedy;
+
+/**
+ * You are given an integer array nums (0-indexed). In one operation, you can choose an element of the array and increment it by 1.
+ *
+ * For example, if nums = [1,2,3], you can choose to increment nums[1] to make nums = [1,3,3].
+ * Return the minimum number of operations needed to make nums strictly increasing.
+ *
+ * An array nums is strictly increasing if nums[i] < nums[i+1] for all 0 <= i < nums.length - 1.
+ * An array of length 1 is trivially strictly increasing.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,1]
+ * Output: 3
+ * Explanation: You can do the following operations:
+ * 1) Increment nums[2], so nums becomes [1,1,2].
+ * 2) Increment nums[1], so nums becomes [1,2,2].
+ * 3) Increment nums[2], so nums becomes [1,2,3].
+ * Example 2:
+ *
+ * Input: nums = [1,5,2,4,1]
+ * Output: 14
+ * Example 3:
+ *
+ * Input: nums = [8]
+ * Output: 0
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 5000
+ * 1 <= nums[i] <= 10^4
+ * @author sniper
+ * @date 07 Jan, 2023
+ * LC1827, Easy
+ */
+public class MinimumOperationsToMakeTheArrayIncreasing {
+
+    public int minOperationsV2(int[] nums) {
+        return 0;
+    }
+
+
+    public int minOperationsV1(int[] nums) {
+        return 0;
+    }
+
+
+    /**
+     * Time Cost 2ms
+     * @param nums
+     * @return
+     */
+    public int minOperations(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        int res = 0;
+        for (int i = 0, j = i + 1; j < nums.length; i++, j++) {
+            if (nums[i] >= nums[j]) {
+                int target = nums[i] + 1;
+                int diff = target - nums[j];
+                nums[j] = target;
+                res += diff;
+            }
+        }
+        return res;
+    }
+}
