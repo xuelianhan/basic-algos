53bfab1b20ee4252847c954b5174cebef0b93e49
xuelianhan <xueliansniper@gmail.com>
2023-01-17 21:39:11 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/binarysearch/FindPeakElement.java
+++ src/main/java/org/ict/algorithm/leetcode/binarysearch/FindPeakElement.java
@@ -45,9 +45,87 @@ public class FindPeakElement {
         System.out.println(result);
     }
 
+    /**
+     * Understanding the following solution.
+     * Given an array, we need to find the peak element.
+     * As, the sub-portions of the array are increasing/decreasing ( only then we would be able to find peak ),
+     * there are sub-portions of array which are sorted, so we could use binary search to get this problem done.
+     * But exactly how ?
+     *
+     * This is an interesting part.
+     *
+     * For a mid element, there could be three possible cases :
+     * Case 1 : mid lies on the right of our result peak ( Observation : Our peak element search space is leftside )
+     * Case 2 : mid is equal to the peak element ( Observation : mid element is greater than its neighbors )
+     * Case 3 : mid lies on the left. ( Observation : Our peak element search space is rightside )
+     *
+     * Some base cases :
+     *
+     * The array could be strictly increasing or strictly decreasing and as we have to return any of the possible peaks,
+     * so we could add a condition to check whether the 1st element/last element could be the peak.
+     * This point is being also supported by the fact that,
+     * we are looking for mid-1/ mid+1, and these indices compromised for 0th index / n-1 th index respectively.
+     * @author Naman
+     * @see <a href="https://leetcode.com/problems/find-peak-element/solutions/1290642/intuition-behind-conditions-complete-explanation-diagram-binary-search"></a>
+     * @param nums
+     * @return
+     */
+    public int findPeakElementV3(int[] nums) {
+        /**
+         * Single element
+         */
+        int n = nums.length;
+        if (n == 1) {
+            return 0;
+        }
+        /**
+         * Check whether the first/last element is the peek or not.
+         */
+        if (nums[0] > nums[1]) {
+            return 0;
+        }
+        if (nums[n - 1] > nums[ n - 2]) {
+            return n - 1;
+        }
+
+        /**
+         * Check the remained in the array
+         */
+        int start = 1;
+        int end = n - 2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] < nums[mid - 1]) {
+                end = mid - 1;
+            } else if (nums[mid] < nums[mid + 1]) {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
 
     /**
      * Understanding the following solution.
+     * Why low < high, instead of low <= high?
+     *
+     * e.g. nums = [1,3,2]
+     * low:0, high:2, low < high, mid = 1
+     * nums[mid] = nums[1] = 3, 3 > nums[2] = 2, high = mid = 1
+     * low:0, high:1, low < high, mid = 0
+     * nums[mid] = nums[0] = 1, 1 < nums[1] = 3, low = mid + 1 = 1
+     * low:1, high:1, low == high, while-loop-end, return high:1
+     *
+     * if using low <= high:
+     * low:1, high:1, low == high, mid = 1
+     * nums[mid] = nums[1] = 3, 3 > nums[2] = 2, high = mid = 1
+     * low:1, high:1, low == high, mid = 1
+     * nums[mid] = nums[1] = 3, 3 > nums[2] = 2, high = mid = 1
+     * ......
+     * You may find that the above process has been a dead-loop,
+     * so we cannot use low <= high here if we initialize high with nums.length - 1.
+     *
      * Time Complexity O(logN)
      * Space Complexity O(1)
      * @param nums
