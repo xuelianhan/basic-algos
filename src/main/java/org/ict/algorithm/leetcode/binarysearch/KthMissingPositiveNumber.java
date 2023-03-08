722df66eafbd68cbb2c60618ab1613d229fea48d
xuelianhan <xueliansniper@gmail.com>
2023-03-08 14:36:46 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/binarysearch/KthMissingPositiveNumber.java
+++ src/main/java/org/ict/algorithm/leetcode/binarysearch/KthMissingPositiveNumber.java
@@ -1,6 +1,10 @@
 package org.ict.algorithm.leetcode.binarysearch;
 
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
  *
@@ -37,20 +41,112 @@ package org.ict.algorithm.leetcode.binarysearch;
  */
 public class KthMissingPositiveNumber {
 
-    public int findKthPositiveV1(int[] arr, int k) {
-        int n = arr.length;
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-
-        }
-        return res;
+    public static void main(String[] args) {
+        KthMissingPositiveNumber instance = new KthMissingPositiveNumber();
+        int[] arr = {2,3,4,7,11};
+        int k = 5;
+        instance.findKthPositive(arr, k);
     }
 
     /**
      * e.g. arr = [2,3,4,7,11], k = 5, expect 9
+     * 1 2 3 4 5
+     * 2 3 4 7 11
+     * arr[i] - (i + 1):
+     * 1 1 1 3 6, this means the count of missing numbers before each index.
+     * for instance, there are 3 missing numbers before 7, they are 1,5,6.
+     * So we can start iterate the count missing number array, let's call it B.
+     * B:1 1 1 3 6
+     * What we need to find in the array-B?
+     * The answer is the first element greater than k or equals to k.
+     * At here, we get 6, 6 is the first element greater than k=5, and the index of 6 is 4.
+     * After that, we go back to the original array:[2,3,4,7,11], the value at index 4 is 11.
+     * So, we know that our answer is between 7 and 11.
+     *
+     * Explanation
+     * Let's assume the final result is x, A is the original array.
+     * And there are m number not missing in the range of [1, x].
+     * Binary search the m in range [0, A.size()].
+     *
+     * If there are m number not missing,
+     * that is A[0], A[1] ... A[m-1],
+     * the number of missing under A[m] is A[m] - 1 - m.
+     *
+     * If A[m] - 1 - m < k, m is too small, we update left = m.
+     * If A[m] - 1 - m >= k, m is big enough, we update right = m.
+     *
+     * Note that, we exit the while-loop, l = r,
+     * which equals to the number of missing number used.
+     * So the Kth positive number will be l + k.
+     *
+     *
+     * Complexity
+     * Time O(logN)
+     * Space O(1)
+     *
+     * @see <a href="https://leetcode.com/problems/kth-missing-positive-number/solutions/779999/java-c-python-o-logn"></a>
+     * @author lee215
+     * @param arr
+     * @param k
+     * @return
+     */
+    public int findKthPositiveV2(int[] arr, int k) {
+        int n = arr.length;
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] - mid - 1 < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left + k;
+    }
+
+    /**
+     *
+     * Note that the array is in strictly increasing order and hence there is no repetition.
+     *
+     * Think of this case,
+     * if every element in the array greater than k , the answer would be k.
+     * So, for every element <= k ,
+     * you need to increment k. (i.e. when you iterate from left to right).
+     * Since the array is in increasing order,
+     * you can break out of the loop on the first instance this condition fails.
+     *
+     * e.g. arr = [2,3,4,7,11], k = 5, expect 9
      * e.g. arr = [1, 3], k = 1, expect 2
      * e.g. arr = [3,10], k = 2, expect 2
      * e.g. arr = [5,6,7,8,9], k = 9, expect 14
+     *
+     * @see <a href="https://leetcode.com/problems/kth-missing-positive-number/solutions/876751/java-1-liner-o-n-simplest-easy-to-understand-beats-100"></a>
+     * @author niranjvin
+     * @param arr
+     * @param k
+     * @return
+     */
+    public int findKthPositiveV1(int[] arr, int k) {
+        for (int a : arr) {
+            if (a <= k) {
+                k++;
+            } else {
+                break;
+            }
+        }
+        return k;
+    }
+
+    /**
+     * Using HashSet to store all the elements in the array.
+     *
+     * e.g. arr = [2,3,4,7,11], k = 5, expect 9
+     * e.g. arr = [1,3], k = 1, expect 2
+     * e.g. arr = [3,10], k = 2, expect 2
+     * e.g. arr = [5,6,7,8,9], k = 9, expect 14
+     * @see <a href="https://leetcode.com/problems/kth-missing-positive-number/solutions/1004535/python-two-solutions-o-n-and-o-log-n-explained"></a>
+     * @author DBabichev
      * @param arr
      * @param k
      * @return
@@ -60,27 +156,20 @@ public class KthMissingPositiveNumber {
         if (arr[n - 1] == n) {
             return (n + k);
         }
-        int res = 0;
-        for (int i = 0; i < n;) {
-            if (arr[i] - (i + 1) < k) {
-                i++;
-            } else if (arr[i] - (i + 1) == k){
-                if (i == 0) {
-                    res = k;
-                } else {
-                    res = arr[i] - k;
-                }
-                break;
-            } else {
-                if (i == 0) {
-                    res = k;
-                } else {
-                    res = arr[i - 1] + k - (arr[i - 1] - i);
-                }
-                break;
+        /**
+         * Notice the boxed here, and use cnt instead of k, because we need to
+         * remain k not modified in for-loop, we decrease cnt instead.
+         */
+        int cnt = k;
+        Set<Integer> set = Arrays.stream(arr).boxed().collect(Collectors.toSet());
+        for (int i = 1; i <= (k + n); i++) {
+            if (!set.contains(i)) {
+                cnt--;
+            }
+            if (cnt == 0) {
+                return i;
             }
         }
-        //todo
-        return res;
+        return 0;
     }
 }
