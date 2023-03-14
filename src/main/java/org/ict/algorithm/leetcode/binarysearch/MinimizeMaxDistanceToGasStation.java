9374a1722dfe0d580fad34c2ced91f22eb670638
xuelianhan <xueliansniper@gmail.com>
2023-03-14 14:35:08 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/binarysearch/MinimizeMaxDistanceToGasStation.java
+++ src/main/java/org/ict/algorithm/leetcode/binarysearch/MinimizeMaxDistanceToGasStation.java
@@ -4,15 +4,11 @@ package org.ict.algorithm.leetcode.binarysearch;
  * On a horizontal number line,
  * we have gas stations at positions stations[0], stations[1], ..., stations[N-1],
  * where N = stations.length.
- *
  * Now, we add K more gas stations so that D, the maximum distance between adjacent gas stations, is minimized.
- *
  * Return the smallest possible value of D.
- *
  * Example:
  * Input: stations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], K = 9
  * Output: 0.500000
- *
  * Note:
  * stations.length will be an integer in range [10, 2000].
  * stations[i] will be an integer in range [0, 10^8].
