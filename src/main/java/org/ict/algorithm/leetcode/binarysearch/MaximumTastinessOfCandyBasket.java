84010ed22f2d2188b4fe326914593934f8d1511b
xueliansniper <xueliansniper@gmail.com>
2023-03-17 10:06:58 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/binarysearch/MaximumTastinessOfCandyBasket.java
+++ src/main/java/org/ict/algorithm/leetcode/binarysearch/MaximumTastinessOfCandyBasket.java
@@ -4,37 +4,30 @@ import java.util.Arrays;
 
 /**
  * You are given an array of positive integers price where price[i] denotes the price of the ith candy and a positive integer k.
- *
  * The store sells baskets of k distinct candies.
  * The tastiness of a candy basket is the smallest absolute difference of the prices of any two candies in the basket.
- *
  * Return the maximum tastiness of a candy basket.
  *
- *
- *
  * Example 1:
- *
  * Input: price = [13,5,1,8,21,2], k = 3
  * Output: 8
  * Explanation: Choose the candies with the prices [13,5,21].
  * The tastiness of the candy basket is: min(|13 - 5|, |13 - 21|, |5 - 21|) = min(8, 8, 16) = 8.
  * It can be proven that 8 is the maximum tastiness that can be achieved.
- * Example 2:
  *
+ * Example 2:
  * Input: price = [1,3,1], k = 2
  * Output: 2
  * Explanation: Choose the candies with the prices [1,3].
  * The tastiness of the candy basket is: min(|1 - 3|) = min(2) = 2.
  * It can be proven that 2 is the maximum tastiness that can be achieved.
- * Example 3:
  *
+ * Example 3:
  * Input: price = [7,7,7,7], k = 2
  * Output: 0
  * Explanation: Choosing any two distinct candies from the candies we have will result in a tastiness of 0.
  *
- *
  * Constraints:
- *
  * 2 <= k <= price.length <= 10^5
  * 1 <= price[i] <= 10^9
  *
@@ -74,7 +67,6 @@ public class MaximumTastinessOfCandyBasket {
          * 2.we need to calculate the smallest absolute difference of the prices of any two candies.
          */
         Arrays.sort(price);
-
         int n = price.length;
         int lo = 0;
         /**
@@ -84,7 +76,6 @@ public class MaximumTastinessOfCandyBasket {
          * can stay on price[n - 1] - price[0].
          */
         int hi = price[n - 1] - price[0] + 1;
-
         while (lo < hi) {
             /**
              * mid is one candidate.
