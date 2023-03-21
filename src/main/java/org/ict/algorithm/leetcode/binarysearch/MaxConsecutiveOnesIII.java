a244a6354472c40fcbe113dcfaa9cff0fec90349
xueliansniper <xueliansniper@gmail.com>
2023-03-21 11:50:15 +0800

    add comments

--- src/main/java/org/ict/algorithm/leetcode/binarysearch/MaxConsecutiveOnesIII.java
+++ src/main/java/org/ict/algorithm/leetcode/binarysearch/MaxConsecutiveOnesIII.java
@@ -34,8 +34,8 @@ import java.util.Queue;
 public class MaxConsecutiveOnesIII {
 
     public static void main(String[] args) {
-        int[] nums = {0,0,0,1};
-        int k = 4;
+        int[] nums = {1,1,1,0,0,0,1,1,1,1,0};
+        int k = 2;
         MaxConsecutiveOnesIII instance = new MaxConsecutiveOnesIII();
         int res = instance.longestOnes(nums, k);
         System.out.println(res);
@@ -43,6 +43,7 @@ public class MaxConsecutiveOnesIII {
 
 
     /**
+     * Two-Pointers Solution
      * e.g. nums = [0,0,0,1], k = 4, expected 4
      * right:0, nums[0]:0, queue:0, queue.size < 4, left:0, res=right-left+1=0-0+1=1
      * right:1, nums[1]:0, queue:0,1, queue.size < 4, left:0, res=right-left+1=1-0+1=2
@@ -50,6 +51,20 @@ public class MaxConsecutiveOnesIII {
      * right:3, nums[3]:1, queue:0,1,2, queue.size < 4, left:0, res=right-left+1=3-0+1=4
      * return res:4
      *
+     * e.g. nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2, expected:6
+     * right:0, nums[0]:1, queue:, queue.size=0, left:0, res=max(0,0-0+1)=1
+     * right:1, nums[1]:1, queue:, queue.size=0, left:0, res=max(0,1-0+1)=2
+     * right:2, nums[2]:1, queue:, queue.size=0, left:0, res=max(0,2-0+1)=3
+     * right:3, nums[3]:0, queue:3, queue.size=1, left:0, res=max(0,3-0+1)=4
+     * right:4, nums[4]:0, queue:3,4, queue.size=2, left:0, res=max(0,4-0+1)=5
+     * right:5, nums[5]:0, queue:3,4,5, queue.size=3, left=3+1=4, res=max(5,5-3+1)=5
+     * right:6, nums[6]:1, queue:4,5, queue.size=2, left:4, res=max(5,6-4+1)=5
+     * right:7, nums[7]:1, queue:4,5, queue.size=2, left:4, res=max(5,7-4+1)=4
+     * right:8, nums[8]:1, queue:4,5, queue.size=2, left:4, res=max(5,8-4+1)=5
+     * right:9, nums[9]:1, queue:4,5, queue.size=2, left:4, res=max(5,9-4+1)=6
+     * right:10, nums[10]:0, queue:4,5,10, queue.size=3, left:5, res=max(6, 10-5+1)=6
+     * return res:6
+     * 
      * @param nums
      * @param k
      * @return
@@ -57,6 +72,9 @@ public class MaxConsecutiveOnesIII {
     public int longestOnes(int[] nums, int k) {
         int res = 0;
         int left = 0;
+        /**
+         * Queue to store the index of value with zero.
+         */
         Queue<Integer> queue = new ArrayDeque<>();
         for (int right = 0; right < nums.length; right++) {
             if (nums[right] == 0) {
@@ -67,9 +85,10 @@ public class MaxConsecutiveOnesIII {
              * 1.left = queue.poll() + 1, not queue.poll()
              * 2.res = Math.max(res, right - left + 1), not Math.max(res, right - left);
              * e.g. nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2, expected:6
+             *
              */
             if (queue.size() > k) {
-                left = queue.poll();
+                left = queue.poll() + 1;
             }
             res = Math.max(res, right - left + 1);
         }
