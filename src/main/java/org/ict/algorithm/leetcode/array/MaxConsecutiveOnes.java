236242b192eb2494d6b2d495b9f813e8b7dfb1aa
xueliansniper <xueliansniper@gmail.com>
2023-03-21 08:50:26 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/leetcode/array/MaxConsecutiveOnes.java
@@ -0,0 +1,50 @@
+package org.ict.algorithm.leetcode.array;
+
+/**
+ * Given a binary array nums, return the maximum number of consecutive 1's in the array.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,0,1,1,1]
+ * Output: 3
+ * Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+ * Example 2:
+ *
+ * Input: nums = [1,0,1,1,0,1]
+ * Output: 2
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 10^5
+ * nums[i] is either 0 or 1.
+ * @author sniper
+ * @date 21 Mar, 2023
+ * LC485, Easy
+ */
+public class MaxConsecutiveOnes {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1};
+        MaxConsecutiveOnes instance = new MaxConsecutiveOnes();
+        int res = instance.findMaxConsecutiveOnes(nums);
+        System.out.println(res);
+    }
+
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int res = 0;
+        int cnt = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                cnt++;
+            } else {
+                res = Math.max(cnt, res);
+                cnt = 0;
+            }
+        }
+        return Math.max(res, cnt);
+    }
+}
