01f3c258e86540fbbba6658b83902d6f759ec9f3
xueliansniper <xueliansniper@gmail.com>
2023-03-22 15:35:31 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/greedy/FrogJumpII.java
+++ src/main/java/org/ict/algorithm/leetcode/greedy/FrogJumpII.java
@@ -2,30 +2,21 @@ package org.ict.algorithm.leetcode.greedy;
 
 /**
  * You are given a 0-indexed integer array stones sorted in strictly increasing order representing the positions of stones in a river.
- *
  * A frog, initially on the first stone, wants to travel to the last stone and then return to the first stone.
  * However, it can jump to any stone at most once.
- *
  * The length of a jump is the absolute difference between the position of the stone the frog is currently on and the position of the stone to which the frog jumps.
- *
  * More formally, if the frog is at stones[i] and is jumping to stones[j], the length of the jump is |stones[i] - stones[j]|.
  * The cost of a path is the maximum length of a jump among all jumps in the path.
- *
  * Return the minimum cost of a path for the frog.
  *
- *
- *
  * Example 1:
- *
- *
  * Input: stones = [0,2,5,6,7]
  * Output: 5
  * Explanation: The above figure represents one of the optimal paths the frog can take.
  * The cost of this path is 5, which is the maximum length of a jump.
  * Since it is not possible to achieve a cost of less than 5, we return it.
+ *
  * Example 2:
- *
- *
  * Input: stones = [0,3,9]
  * Output: 9
  * Explanation:
@@ -33,13 +24,12 @@ package org.ict.algorithm.leetcode.greedy;
  * In this case, the length of each jump will be 9. The cost for the path will be max(9, 9) = 9.
  * It can be shown that this is the minimum achievable cost.
  *
- *
  * Constraints:
- *
  * 2 <= stones.length <= 10^5
  * 0 <= stones[i] <= 10^9
  * stones[0] == 0
  * stones is sorted in a strictly increasing order.
+ *
  * @author sniper
  * @date 22 Mar, 2023
  * LC2498, Medium
@@ -47,6 +37,7 @@ package org.ict.algorithm.leetcode.greedy;
 public class FrogJumpII {
 
     public int maxJumpV1(int[] stones) {
+        //todo
         return 0;
     }
 
