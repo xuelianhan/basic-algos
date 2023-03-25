ac40152a0776392a75a32a2e733001b8b66062d7
xueliansniper <xueliansniper@gmail.com>
2023-03-25 19:18:54 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/binarysearch/SearchInRotatedSortedArray.java
+++ src/main/java/org/ict/algorithm/leetcode/binarysearch/SearchInRotatedSortedArray.java
@@ -40,13 +40,54 @@ public class SearchInRotatedSortedArray {
      * @param target
      * @return
      */
-    public int searchV5(int[] nums, int target) {
+    public int searchV7(int[] nums, int target) {
         int res = 0;
         //todo
         return res;
     }
 
+    public int searchV6(int[] nums, int target) {
+        int res = 0;
+        //todo
+        return res;
+    }
+
+    public int searchV5(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int split;
+            if ((nums[mid] < nums[0]) == (target < nums[0])) {
+                split = nums[mid];
+            } else {
+                split = (target < nums[0] ? Integer.MIN_VALUE : Integer.MAX_VALUE);
+            }
+            if (split < target) {
+                lo = mid + 1;
+            } else if (split > target) {
+                hi = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
     /**
+     * Explanation:
+     * Let's say nums looks like this: [12, 13, 14, 15, 16, 17, 18, 19, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+     * Because it's not fully sorted, we can't do normal binary search. But here comes the trick:
+     * If target is let's say 14, then we adjust nums to this, where "inf" means infinity:
+     * [12, 13, 14, 15, 16, 17, 18, 19, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf]
+     * If target is let's say 7, then we adjust nums to this:
+     * [-inf, -inf, -inf, -inf, -inf, -inf, -inf, -inf, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+     * And then we can simply do ordinary binary search.
+     * Of course, we don't actually adjust the whole array but instead adjust only on the fly only the elements we look at.
+     * And the adjustment is done by comparing both the target and the actual element against nums[0].
+     * If nums[mid] and target are "on the same side" of nums[0],
+     * we just take nums[mid].
+     * Otherwise, we use -infinity or +infinity as needed.
      * @see <a href="https://leetcode.com/problems/search-in-rotated-sorted-array/solutions/14435/clever-idea-making-it-simple"></a>
      * @author StefanPochmann
      * @param nums
@@ -54,9 +95,25 @@ public class SearchInRotatedSortedArray {
      * @return
      */
     public int searchV4(int[] nums, int target) {
-        int res = 0;
-        //todo
-        return res;
+        int lo = 0;
+        int hi = nums.length;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int num;
+            if ((nums[mid] < nums[0]) == (target < nums[0])) {
+                num = nums[mid];
+            } else {
+                num = (target < nums[0] ? Integer.MIN_VALUE : Integer.MAX_VALUE);
+            }
+            if (num < target) {
+                lo = mid + 1;
+            } else if (num > target) {
+                hi = mid;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 
     /**
