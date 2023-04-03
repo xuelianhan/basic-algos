8508e09d796d8ea310a74d383f4053d37adf6354
xueliansniper <xueliansniper@gmail.com>
2023-04-03 13:23:48 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/dynamicprogramming/DecodeWays.java
+++ src/main/java/org/ict/algorithm/leetcode/dynamicprogramming/DecodeWays.java
@@ -106,20 +106,55 @@ public class DecodeWays {
         return res;
     }
 
+    /**
+     * Dynamic programming Solution
+     * Time Complexity O(N)
+     * Space Complexity O(N)
+     * @param s
+     * @return
+     */
     public int numDecodingV3(String s) {
-        int res = 0;
-        //todo
-        return res;
-    }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
 
-    public int numDecodingV2(String s) {
-        int res = 0;
-        //todo
-        return res;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) != '0') {
+                dp[i] = dp[i + 1];
+                if ((i < (n - 1)) && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7')) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+        return dp[0];
     }
 
     /**
-     * Memoization Solution
+     * Dynamic programming Solution
+     * Time Complexity O(N)
+     * Space Complexity O(N)
+     * @param s
+     * @return
+     */
+    public int numDecodingV2(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                continue;
+            }
+            dp[i] = dp[i + 1];
+            if ((i < (n - 1)) && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7')) {
+                dp[i] += dp[i + 2];
+            }
+        }
+        return dp[0];
+    }
+
+    /**
+     * Recursion + Memoization Solution
      * A char may be decoded alone or by pairing with the next char.
      * Time Cost 0ms
      *
