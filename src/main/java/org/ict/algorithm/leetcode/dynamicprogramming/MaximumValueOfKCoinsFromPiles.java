21cbafd4c7f958f798b32a89a809af82036ce074
xueliansniper <xueliansniper@gmail.com>
2023-04-19 11:41:47 +0800

    add comments

--- src/main/java/org/ict/algorithm/leetcode/dynamicprogramming/MaximumValueOfKCoinsFromPiles.java
+++ src/main/java/org/ict/algorithm/leetcode/dynamicprogramming/MaximumValueOfKCoinsFromPiles.java
@@ -90,6 +90,24 @@ public class MaximumValueOfKCoinsFromPiles {
      *             return res
      *
      *         return dp(0, K)
+     * --------------------------------------------------------
+     * @see <a href="https://stackoverflow.com/questions/20353210/what-is-the-purpose-of-stdfunction-and-how-to-use-it"></a>
+     * int maxValueOfCoins(vector<vector<int>>& A, int K) {
+     *         int n = A.size();
+     *         vector<vector<int>> memo(n + 1, vector<int>(K + 1, 0));
+     *         function<int(int, int)> dp = [&](int i, int k) {
+     *             if (memo[i][k] > 0) return memo[i][k];
+     *             if (i == n || k == 0) return 0;
+     *             int res = dp(i + 1, k), cur = 0;
+     *             for (int j = 0; j < A[i].size() && j < k; ++j) {
+     *                 cur += A[i][j];
+     *                 res = max(res, dp(i + 1, k - j - 1) + cur);
+     *             }
+     *             memo[i][k] = res;
+     *             return res;
+     *         };
+     *         return dp(0, K);
+     *     }
      * -------------------------------------------------------------
      * Let use dp with states dp(n, k),
      * where it is (current index of pile, number of elements we still need to take).
@@ -143,6 +161,9 @@ public class MaximumValueOfKCoinsFromPiles {
         if (memo[i][k] != null) {
             return memo[i][k];
         }
+        /**
+         * Pick 0 coins from current pile
+         */
         int res = helper(piles, memo, k, i + 1);
         int sum = 0;
         /**
