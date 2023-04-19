ec735807a98a17d16ea6c74c08e6c6ec29938431
xueliansniper <xueliansniper@gmail.com>
2023-04-19 19:06:00 +0800

    add LC339, NestedListWeightSum.java

---
+++ src/main/java/org/ict/algorithm/leetcode/depthfirstsearch/NestedListWeightSum.java
@@ -0,0 +1,104 @@
+package org.ict.algorithm.leetcode.depthfirstsearch;
+
+import java.util.List;
+
+/**
+ * Description
+ * You are given a nested list of integers nestedList.
+ * Each element is either an integer or a list whose elements may also be integers or other lists.
+ * The depth of an integer is the number of lists that it is inside of.
+ * For example, the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth.
+ * Return the sum of each integer in nestedList multiplied by its depth.
+ *
+ * Example 1:
+ * Input: nestedList = [[1,1],2,[1,1]]
+ * Output: 10
+ * 2 inside of one: [2]
+ * 1 inside of two: [[1]]
+ * Explanation: Four 1's at depth 2, one 2 at depth 1. 1*2 + 1*2 + 2*1 + 1*2 + 1*2 = 10.
+ *
+ * Example 2:
+ * Input: nestedList = [1,[4,[6]]]
+ * Output: 27
+ * 1 inside of one:[1]
+ * 4 inside of two:[[4]]
+ * 6 inside of three:[[[6]]]
+ * Explanation: One 1 at depth 1, one 4 at depth 2, and one 6 at depth 3. 1*1 + 4*2 + 6*3 = 27.
+ *
+ *
+ * Example 3:
+ * Input: nestedList = [0]
+ * Output: 0
+ * 0 inside of one:[0]
+ * 0*1=0
+ *
+ *
+ * Constraints:
+ * 1 <= nestedList.length <= 50
+ * The values of the integers in the nested list is in the range [-100, 100].
+ * The maximum depth of any integer is less than or equal to 50.
+ * @author sniper
+ * @date 19 Apr, 2023
+ * LC339, Medium, frequency=134
+ */
+public class NestedListWeightSum {
+
+    public int depthSumV1(List<NestedInteger> nestedList) {
+        return 0;
+    }
+
+    public int depthSum(List<NestedInteger> nestedList) {
+        return 0;
+    }
+
+    /**
+     * // This is the interface that allows for creating nested lists.
+     * // You should not implement it, or speculate about its implementation
+     * public interface NestedInteger {
+     *     // Constructor initializes an empty nested list.
+     *     public NestedInteger();
+     *
+     *     // Constructor initializes a single integer.
+     *     public NestedInteger(int value);
+     *
+     *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+     *     public boolean isInteger();
+     *
+     *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+     *     // Return null if this NestedInteger holds a nested list
+     *     public Integer getInteger();
+     *
+     *     // Set this NestedInteger to hold a single integer.
+     *     public void setInteger(int value);
+     *
+     *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+     *     public void add(NestedInteger ni);
+     *
+     *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+     *     // Return empty list if this NestedInteger holds a single integer
+     *     public List<NestedInteger> getList();
+     * }
+     */
+     class NestedInteger {
+
+         public NestedInteger(){}
+         public NestedInteger(int value){
+             //todo
+         }
+         public boolean isInteger() {
+             return false;
+         }
+         public Integer getInteger() {
+             return 0;
+         }
+         public void setInteger(int value) {
+             //todo
+         }
+         public void add(NestedInteger ni) {
+             //todo
+         }
+         public List<NestedInteger> getList() {
+             return null;
+         }
+    }
+}
