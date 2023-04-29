90e8b7e74620396f9541e7ca3410d08556966e60
xueliansniper <xueliansniper@gmail.com>
2023-04-29 13:40:42 +0800

    add implementation of Stack

--- src/main/java/org/ict/algorithm/util/Stack.java
+++ src/main/java/org/ict/algorithm/util/Stack.java
@@ -1,6 +1,7 @@
 package org.ict.algorithm.util;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  *
@@ -31,46 +32,94 @@ public class Stack<Item> implements Iterable<Item> {
 	}
 
 	public void push(Item item) {
-		
+		Node<Item> oldfirst = first;
+		first = new Node<Item>();
+		first.item = item;
+		first.next = oldfirst;
+		N++;
 	}
 	
 	public Item pop() {
-		
-		return null;
+		if (isEmpty()) {
+			throw new NoSuchElementException("stack overflow!");
+		}
+		Item item = first.item;
+		first = first.next;
+		N--;
+		return item;
 	}
 	
 	public Item peek() {
-		
-		return null;
+		if (isEmpty()) {
+			throw new NoSuchElementException("stack overflow!");
+		}
+		return first.item;
 	}
 	
 	public String toString() {
-		
-		return null;
+		StringBuilder sb = new StringBuilder();
+		for(Item item : this) {
+			sb.append(item + " ");
+		}
+		return sb.toString();
 	}
 	
 	public Iterator<Item> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new ListIterator<Item>(first);
 	}
 	
 	private class ListIterator<Item> implements Iterator<Item> {
-
+		
+		private Node<Item> current;
+		
+		public ListIterator(Node<Item> first) {
+			current = first;
+		}
+		
 		public boolean hasNext() {
-			// TODO Auto-generated method stub
-			return false;
+			return current != null;
 		}
 
 		public Item next() {
-			// TODO Auto-generated method stub
-			return null;
+			if(!hasNext()) {
+				throw new NoSuchElementException("no more element!");
+			}
+			Item item = current.item;
+			current = current.next;
+			return item;
 		}
 
 		public void remove() {
-			// TODO Auto-generated method stub
-			
+			throw new UnsupportedOperationException();
 		}
-		
+	}
+	
+	/**
+	 * [hanxuelian@ict java]$ cat /home/hanxuelian/Desktop/test.log
+	 *	to be or not      to - be - - that - - - 
+	 * 
+	 * [hanxuelian@ict java]$ java org/ict/algorithm/util/Stack < /home/hanxuelian/Desktop/test.log
+     * to be not that or be (2 left on stack!)
+     * 
+     * to  <- 1
+     * be  <- 2     13 be   ->
+     * or  <- 3     12 or   ->
+     * not <- 4     9  not  ->
+     * to  <- 5     6  to   ->
+     * be  <- 7     8  be   ->
+     * that<- 10    11 that ->
+	 */
+	public static void main(String[] args) {
+		Stack<String> stack = new Stack<String>();
+		while(!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			if (!item.equals("-")) {
+				stack.push(item);
+			} else if (!stack.isEmpty()) {
+				StdOut.print(stack.pop() + " ");
+			}
+		}
+		StdOut.println("(" + stack.size() + " left on stack!)");
 	}
 
 }
