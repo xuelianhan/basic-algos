1bf043c3a0181201e2e51465a699128ef2ab2ff0
xueliansniper <xueliansniper@gmail.com>
2023-05-05 14:39:57 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/stack/DesignMaxStack.java
+++ src/main/java/org/ict/algorithm/leetcode/stack/DesignMaxStack.java
@@ -1,8 +1,6 @@
 package org.ict.algorithm.leetcode.stack;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Description
@@ -39,22 +37,130 @@ import java.util.Stack;
  *
  * Constraints:
  * -10^7 <= x <= 10^7
- * At most 105 calls will be made to push, pop, top, peekMax, and popMax.
+ * At most 10^5 calls will be made to push, pop, top, peekMax, and popMax.
  * There will be at least one element in the stack when pop, top, peekMax, or popMax is called.
+ *
  * @author sniper
  * @date 03 May 2023
  * LC716, Hard, frequency=41
  */
 public class DesignMaxStack {
 
+    /**
+     * LeetCode Official - Approach #2: Double Linked List + TreeMap
+     *
+     * Intuition
+     * Using structures like Array or Stack will never let us popMax quickly.
+     * We turn our attention to tree and linked-list structures that have a lower time complexity for removal,
+     * with the aim of making popMax faster than O(N) time complexity.
+     * Say we have a double linked list as our "stack".
+     * This reduces the problem to finding which node to remove, since we can remove nodes in O(1) time.
+     * We can use a TreeMap mapping values to a list of nodes to answer this question.
+     * TreeMap can find the largest value, insert values, and delete values, all in O(logN) time.
+     *
+     * Algorithm
+     * Let's store the stack as a double linked list dll, and store a map from value to a List of Node.
+     * When weMaxStack.push(x), we add a node to our dll, and add or update our entry map.get(x).add(node).
+     * When weMaxStack.pop(), we find the value val = dll.pop(), and remove the node from our map, deleting the entry if it was the last one.
+     * When weMaxStack.popMax(), we use the map to find the relevant node to unlink, and return its value.
+     * The above operations are more clear given that we have a working DoubleLinkedList class.
+     * The implementation provided uses head and tail_sentinels_to simplify the relevant Double LinkedList operations.
+     */
     class MaxStackV2 {
+        private DoubleLinkedList stack = new DoubleLinkedList();
+        private TreeMap<Integer, List<Node>> map = new TreeMap<>();
 
         public MaxStackV2() {}
 
+        public void push(int x) {
+            Node node = stack.add(x);
+            /**
+             * if (map.containsKey(x)) {
+             *     map.put(x, new ArrayList<>());
+             * }
+             * map.get(x).add(node);
+             */
+            map.computeIfAbsent(x, k -> new ArrayList<>()).add(node);
+        }
+
+        public int pop() {
+            Node node = stack.pop();
+            List<Node> nodeList = map.get(node.val);
+            int x = nodeList.remove(nodeList.size() - 1).val;
+            if (nodeList.isEmpty()) {
+                map.remove(node.val);
+            }
+            return x;
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int peekMax() {
+            return map.lastKey();
+        }
+
+        public int popMax() {
+            int x = peekMax();
+            List<Node> nodeList = map.get(x);
+            Node node = nodeList.remove(nodeList.size() - 1);
+            if (nodeList.isEmpty()) {
+                map.remove(x);
+            }
+            stack.remove(node);
+            return x;
+        }
+
+    }
+
+    class DoubleLinkedList {
+        private Node head = new Node();
+        private Node tail = new Node();
+
+        public DoubleLinkedList() {
+            head.next = tail;
+            tail.prev = head;
+        }
+
+        public Node add(int val) {
+            Node node = new Node(val);
+            node.next = tail;
+            node.prev = tail.prev;
+            tail.prev = node;
+            node.prev.next = node;
+            return node;
+        }
+
+        public Node remove(Node node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            return node;
+        }
+
+        public Node pop() {
+            return remove(tail.prev);
+        }
+
+        public int peek() {
+            return tail.prev.val;
+        }
+    }
+
+    class Node {
+        int val;
+        Node prev;
+        Node next;
+
+        public Node() {}
+        public Node(int val) {
+            this.val = val;
+        }
     }
 
     /**
      * LeetCode Official - Approach #1: Two Stacks
+     *
      * For peekMax, we remember the largest value we've seen on the side.
      * For example if we add [2, 1, 5, 3, 9] in stack , we'll store [2, 2, 5, 5, 9] in maxStack.
      * This works seamlessly with pop operations, and also it's easy to compute:
@@ -74,12 +180,22 @@ public class DesignMaxStack {
 
         public MaxStackV1() {}
 
+        /**
+         * A little different with method push of MaxStack
+         * e.g. push [2, 1, 5, 3, 9] into stack one by one.
+         * stack1: [2, 1, 5, 3, 9]
+         * stack2: [2, 2, 5, 5, 9]
+         * @param x
+         */
         public void push(int x) {
             int max = stack2.isEmpty() ? x : stack2.peek();
             stack2.push(max > x ? max : x);
             stack1.push(x);
         }
 
+        /**
+         * A little different with method pop of MaxStack
+         */
         public int pop() {
             stack2.pop();
             return stack1.pop();
@@ -93,6 +209,9 @@ public class DesignMaxStack {
             return stack2.peek();
         }
 
+        /**
+         * A little different with method popMax of MaxStack
+         */
         public int popMax() {
             int max = peekMax();
             Stack<Integer> aux = new Stack<>();
@@ -130,6 +249,12 @@ public class DesignMaxStack {
          */
         public MaxStack() {}
 
+        /**
+         * e.g. push [2, 1, 5, 3, 9] into stack one by one.
+         * stack1: [2, 1, 5, 3, 9]
+         * stack2: [2, 5, 9]
+         * @param x
+         */
         public void push(int x) {
             stack1.push(x);
             if (stack2.isEmpty() || stack2.peek() <= x) {
@@ -168,7 +293,7 @@ public class DesignMaxStack {
         public int popMax() {
             int max = stack2.peek();
             /**
-             * Store the elements not equals max temporarily into a temp stack.
+             * Store the elements of stack1 not equals max temporarily into a temp stack.
              */
             Stack<Integer> temp = new Stack<>();
             while (stack1.peek() != stack2.peek()) {
