ce595cedfa56040275d02cf7dd58a239ff650e0f
xueliansniper <xueliansniper@gmail.com>
2023-05-08 15:26:59 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/array/FirstMissingPositive.java
+++ src/main/java/org/ict/algorithm/leetcode/array/FirstMissingPositive.java
@@ -21,9 +21,11 @@ package org.ict.algorithm.leetcode.array;
 public class FirstMissingPositive {
 	
 	public static void main(String[] args) {
-		int[] A = {7, 8, 9, 11, 12};
-		System.out.println(firstMissingPositiveV2(A));
-		System.out.println(firstMissingPositiveV1(A));
+		int[] nums = {7, 8, 9, 11, 12};
+
+		FirstMissingPositive instance = new FirstMissingPositive();
+		int res = instance.firstMissingPositive(nums);
+		System.out.println(res);
 	}
 	
 	/**
@@ -32,59 +34,60 @@ public class FirstMissingPositive {
 	 * @param nums
 	 * @return
 	 */
-	public int firstMissingPositiveV3(int[] nums) {
+	public int firstMissingPositiveV2(int[] nums) {
 		int i = 0, n = nums.length;
 		while (i < n) {
-	        // If the current value is in the range of (0,length) and it's not at its correct position, 
-	        // swap it to its correct position.
-	        // Else just continue;
-			if (nums[i] >= 0 && nums[i] < n && nums[nums[i]] != nums[i])
+			// If the current value is in the range of (0,length) and it's not at its correct position,
+			// swap it to its correct position.
+			// Else just continue;
+			if (nums[i] >= 0 && nums[i] < n && nums[nums[i]] != nums[i]) {
 				swap(nums, i, nums[i]);
-			else
+			} else {
 				i++;
+			}
 		}
 		int k = 1;
-
-	    // Check from k=1 to see whether each index and value can be corresponding.
-		while (k < n && nums[k] == k)
+		// Check from k=1 to see whether each index and value can be corresponding.
+		while (k < n && nums[k] == k) {
 			k++;
-
-	    // If it breaks because of empty array or reaching the end. K must be the first missing number.
-		if (n == 0 || k < n)
+		}
+		// If it breaks because of empty array or reaching the end. K must be the first missing number.
+		if (n == 0 || k < n) {
 			return k;
-		else   // If k is hiding at position 0, K+1 is the number. 
+		} else {
+			// If k is hiding at position 0, K+1 is the number.
 			return nums[0] == k ? k + 1 : k;
-
+		}
 	}
 
-	
+
 	/**
-	 * The key here is to use swapping to keep constant space 
-	 * and also make use of the length of the array, 
-	 * which means there can be at most n positive integers. 
-	 * So each time we encounter an valid integer, 
-	 * find its correct position and swap. 
-	 * Otherwise we continue.
-	 * 
-	 * @param A
+	 * The key here is to use swapping to keep constant space
+	 * and also make use of the length of the array,
+	 * which means there can be at most n positive integers.
+	 * So each time we encounter an valid integer,
+	 * find its correct position and swap.
+	 * Otherwise, we continue.
+	 *
+	 * @param nums
 	 * @return
 	 */
-	public static int firstMissingPositiveV2(int[] A) {
-        int i = 0;
-        while(i < A.length) {
-            if (A[i] == i+1 || A[i] <= 0 || A[i] > A.length) {
-            	i++;
-            } else if (A[A[i]-1] != A[i]) {
-            	swap(A, i, A[i]-1);
-            } else {
-            	i++;
-            }
-        }
-        i = 0;
-        while(i < A.length && A[i] == i+1) {
-        	i++;
-        }
-        return i+1;
+	public int firstMissingPositiveV1(int[] nums) {
+		int i = 0;
+		while (i < nums.length) {
+			if (nums[i] == i+1 || nums[i] <= 0 || nums[i] > nums.length) {
+				i++;
+			} else if (nums[nums[i]-1] != nums[i]) {
+				swap(nums, i, nums[i]-1);
+			} else {
+				i++;
+			}
+		}
+		i = 0;
+		while(i < nums.length && nums[i] == i+1) {
+			i++;
+		}
+		return i+1;
     }
 
 	/**
@@ -92,32 +95,32 @@ public class FirstMissingPositive {
 	 * For example:
 	 * When we find 5, then swap it with A[4].
 	 * At last, the first place where its number is not right, return the place + 1.
-	 * @param A
+	 * @param nums
 	 * @return
 	 */
-	public static int firstMissingPositiveV1(int A[]) {
-		int len = A.length;
-        for (int i = 0; i < len; ++i) {
-        	// why use while in here? not if statement?
-        	while (A[i] > 0 && A[i] <= len && A[A[i] - 1] != A[i]) {
-        		swap(A, A[i], A[A[i] - 1]);
-        	}
-        }
-        
-        for (int i = 0; i < len; ++ i) {
-        	if (A[i] != i + 1) {
-        		return i + 1;
-        	}
-        }
-        
-        return len + 1;
+	public int firstMissingPositive(int[] nums) {
+		int len = nums.length;
+		for (int i = 0; i < len; ++i) {
+			// why use while in here? not if statement?
+			while (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]) {
+				swap(nums, nums[i], nums[nums[i] - 1]);
+			}
+		}
+		for (int i = 0; i < len; ++ i) {
+			if (nums[i] != i + 1) {
+				return i + 1;
+			}
+		}
+
+		return len + 1;
 	}
-	
+
+
 	public static void swap(int[] A, int i, int j){
-	    if (i != j) {
-	        A[i] ^= A[j];
-	        A[j] ^= A[i];
-	        A[i] ^= A[j];
-	    }
+		if (i != j) {
+			A[i] ^= A[j];
+			A[j] ^= A[i];
+			A[i] ^= A[j];
+		}
 	}
 }
