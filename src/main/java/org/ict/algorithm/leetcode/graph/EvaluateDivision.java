dad891c9b19abed8a90c60e7bf4eeed4df7ea72e
xueliansniper <xueliansniper@gmail.com>
2023-05-10 18:00:06 +0800

    add BFS for LC399

--- src/main/java/org/ict/algorithm/leetcode/graph/EvaluateDivision.java
+++ src/main/java/org/ict/algorithm/leetcode/graph/EvaluateDivision.java
@@ -1,6 +1,6 @@
 package org.ict.algorithm.leetcode.graph;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * You are given an array of variable pairs equations and an array of real numbers values,
@@ -47,17 +47,104 @@ import java.util.List;
 public class EvaluateDivision {
 
     /**
+     * Union-Find Solution
+     * @param equations
+     * @param values
+     * @param queries
+     * @return
+     */
+    public double[] calcEquationV2(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        //todo
+        return null;
+    }
+
+
+    /**
      * Breadth-First-Search Solution
+     * Time Cost 2ms
      * @param equations
      * @param values
      * @param queries
      * @return
      */
     public double[] calcEquationV1(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        return null;
+        int n = queries.size();
+        double[] res = new double[n];
+
+        Map<String, Map<String, Double>> graph = buildGraph(equations, values);
+        for (int i = 0; i < queries.size(); i++) {
+            List<String> pair = queries.get(i);
+            String start = pair.get(0);
+            String end = pair.get(1);
+            if (!graph.containsKey(start) || !graph.containsKey(end)) {
+                res[i]  = -1.0;
+                continue;
+            }
+
+            Queue<Pair> queue = new ArrayDeque<>();
+            Set<String> visited = new HashSet<>();
+            queue.offer(new Pair(start, 1.0));
+            visited.add(start);
+            boolean found = false;
+            while (!queue.isEmpty() && !found) {
+                for (int j = queue.size(); j > 0; j--) {
+                    Pair cur = queue.poll();
+                    if (cur.point.equals(end)) {
+                        found = true;
+                        res[i] = cur.value;
+                        break;
+                    }
+
+                    for (Map.Entry<String, Double> entry : graph.get(cur.point).entrySet()) {
+                        String middle = entry.getKey();
+                        if (visited.contains(middle)) {
+                            continue;
+                        }
+                        visited.add(middle);
+                        /**
+                         * cur.point / middle = entry.getValue()
+                         */
+                        double temp = entry.getValue() * cur.value;
+                        queue.offer(new Pair(entry.getKey(), temp));
+                    }
+                }
+            }
+            if (!found) {
+                res[i] = -1.0;
+            }
+        }
+        return res;
+    }
+
+    static class Pair {
+        String point;
+        double value;
+
+        public Pair(String point, double value) {
+            this.point = point;
+            this.value = value;
+        }
+    }
+
+    private Map<String, Map<String, Double>> buildGraph(List<List<String>> equations, double[] values) {
+        Map<String, Map<String, Double>> graph = new HashMap<>();
+        for (int i = 0; i < equations.size(); i++) {
+            List<String> list = equations.get(i);
+            String a = list.get(0);
+            String b = list.get(1);
+            graph.putIfAbsent(a, new HashMap<>());
+            graph.putIfAbsent(b, new HashMap<>());
+            graph.get(a).put(b, values[i]);
+            graph.get(b).put(a, 1.0 / values[i]);
+            graph.get(a).put(a, 1.0);
+            graph.get(b).put(b, 1.0);
+        }
+        return graph;
     }
 
     /**
+     * Understanding the following solution.
+     * Time Cost 1ms
      * Depth-First-Search Solution
      * @param equations
      * @param values
@@ -65,6 +152,64 @@ public class EvaluateDivision {
      * @return
      */
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        return null;
+        int n = queries.size();
+        double[] res = new double[n];
+
+        Map<String, Map<String, Double>> graph = new HashMap<>();
+        for (int i = 0; i < equations.size(); i++) {
+            List<String> list = equations.get(i);
+            String a = list.get(0);
+            String b = list.get(1);
+            graph.putIfAbsent(a, new HashMap<>());
+            graph.putIfAbsent(b, new HashMap<>());
+            graph.get(a).put(b, values[i]);
+            graph.get(b).put(a, 1.0 / values[i]);
+        }
+
+        for (int i = 0; i < queries.size(); i++) {
+            List<String> pair = queries.get(i);
+            String a = pair.get(0);
+            String c = pair.get(1);
+            if (!graph.containsKey(a) || !graph.containsKey(c)) {
+                res[i]  = -1.0;
+            } else {
+                res[i] = dfs(graph, a, c, new HashSet<>());
+            }
+        }
+        return res;
+    }
+
+
+
+    /**
+     * start / end = (start/ middle) * (middle/end)
+     * graph:
+     * {start, {{middle, 1.0 / 4.0}}}
+     * {middle, {{start, 4.0}, {end, 4.0 / 5.0}}}
+     * {end, {{middle, 5.0 / 4.0}}}
+     *
+     * start / end = (1.0/4.0) * (4.0/5.0) = 1.0/5.0
+     */
+    private double dfs(Map<String, Map<String, Double>> graph, String start, String end, Set<String> visited) {
+        if (start.equals(end)) {
+            return 1.0;
+        }
+        visited.add(start);
+        for (String middle: graph.get(start).keySet()) {
+            if (visited.contains(middle)) {
+                continue;
+            }
+            /**
+             * end / middle
+             */
+            double res = dfs(graph, middle, end, visited);
+            if (res > 0) {
+                /**
+                 * start / end = (start / middle) * (middle / end)
+                 */
+                return graph.get(start).get(middle) * res;
+            }
+        }
+        return -1.0;
     }
 }
