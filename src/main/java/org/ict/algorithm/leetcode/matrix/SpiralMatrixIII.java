d8d8f2deeda8ee5d4ff213b12f26b9ade7a22856
xueliansniper <xueliansniper@gmail.com>
2023-05-10 14:43:22 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/matrix/SpiralMatrixIII.java
+++ src/main/java/org/ict/algorithm/leetcode/matrix/SpiralMatrixIII.java
@@ -43,6 +43,25 @@ public class SpiralMatrixIII {
     /**
      * Understanding the following solution
      * Time Cost 4ms
+     * ------------------------------------
+     * class Solution {
+     * public:
+     *     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+     *         const vector<int> dx{1, 0, -1, 0};
+     *         const vector<int> dy{0, 1, 0, -1};
+     *         vector<vector<int>> res{{rStart, cStart}};
+     *
+     *         for (int i = 0; res.size() < rows * cols; ++i)
+     *         for (int step = 0; step < i / 2 + 1; ++step) {
+     *             rStart += dy[i % 4];
+     *             cStart += dx[i % 4];
+     *             if (0 <= rStart && rStart < rows && 0 <= cStart && cStart < cols)
+     *             res.push_back({rStart, cStart});
+     *         }
+     *
+     *         return res;
+     *     }
+     * };
      * @param rows
      * @param cols
      * @param rStart
