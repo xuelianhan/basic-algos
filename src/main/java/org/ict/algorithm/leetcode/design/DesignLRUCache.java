9460ed4f22b13a775e5eb3fa43b5efe53fd7b3d8
xueliansniper <xueliansniper@gmail.com>
2023-05-29 09:56:22 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/design/DesignLRUCache.java
+++ src/main/java/org/ict/algorithm/leetcode/design/DesignLRUCache.java
@@ -54,6 +54,69 @@ public class DesignLRUCache {
 
     /**
      * Understanding the following solution
+     * --------------------------------------
+     * class Node:
+     *     def __init__(self, key = 0, val = 0):
+     *         self.key = key
+     *         self.val = val
+     *         self.prev = None
+     *         self.next = None
+     *
+     * class LRUCache:
+     *
+     *     def __init__(self, capacity: int):
+     *         self.cache = {}
+     *         self.head = Node()
+     *         self.tail = Node()
+     *         self.capacity = capacity
+     *         self.size = 0
+     *         self.head.next = self.tail
+     *         self.tail.prev = self.head
+     *
+     *     def get(self, key: int) -> int:
+     *         if key not in self.cache:
+     *             return -1
+     *         node = self.cache[key]
+     *         #move to head
+     *         self.move_to_head(node)
+     *         return node.val
+     *
+     *     def put(self, key: int, value: int) -> None:
+     *         if key in self.cache:
+     *             node = self.cache[key]
+     *             node.val = value
+     *             # move to head
+     *             self.move_to_head(node)
+     *         else:
+     *             node = Node(key, value)
+     *             self.cache[key] = node
+     *             # add to head
+     *             self.add_to_head(node)
+     *             self.size += 1
+     *             if self.size > self.capacity:
+     *                 # remove tail
+     *                 node = self.remove_tail()
+     *                 self.cache.pop(node.key)
+     *                 self.size -= 1
+     *
+     *     def remove_node(self, node):
+     *         node.prev.next = node.next
+     *         node.next.prev = node.prev
+     *
+     *     def add_to_head(self, node):
+     *         node.next = self.head.next
+     *         self.head.next = node
+     *         node.prev = self.head
+     *         node.next.prev = node
+     *
+     *     def remove_tail(self):
+     *         node = self.tail.prev
+     *         self.remove_node(node)
+     *         return node
+     *
+     *     def move_to_head(self, node):
+     *         self.remove_node(node)
+     *         self.add_to_head(node)
      */
     static class LRUCacheV1 {
         private Map<Integer, Node> cache = new HashMap<>();
