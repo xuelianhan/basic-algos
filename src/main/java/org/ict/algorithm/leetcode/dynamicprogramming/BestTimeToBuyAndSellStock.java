21af4f2addf8feec3b381494aa90e10ba90821b4
xueliansniper <xueliansniper@gmail.com>
2023-06-06 12:44:44 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/dynamicprogramming/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/ict/algorithm/leetcode/dynamicprogramming/BestTimeToBuyAndSellStock.java
@@ -1,4 +1,4 @@
-package org.ict.algorithm.leetcode.array;
+package org.ict.algorithm.leetcode.dynamicprogramming;
 
 /**
  * You are given an array prices where prices[i] is the price of a given stock on the ith day.
