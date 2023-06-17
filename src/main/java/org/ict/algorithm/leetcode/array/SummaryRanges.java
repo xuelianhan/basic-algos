ed30c97ca87b1187d4157529df0c96bdf474321c
xueliansniper <xueliansniper@gmail.com>
2023-06-17 20:54:48 +0800

    add codes

---
+++ src/main/java/org/ict/algorithm/leetcode/array/SummaryRanges.java
@@ -0,0 +1,111 @@
+package org.ict.algorithm.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * You are given a sorted unique integer array nums.
+ * A range [a,b] is the set of all integers from a to b (inclusive).
+ * Return the smallest sorted list of ranges that cover all the numbers in the array exactly.
+ * That is, each element of nums is covered by exactly one of the ranges,
+ * and there is no integer x such that x is in one of the ranges but not in nums.
+ *
+ * Each range [a,b] in the list should be output as:
+ * "a->b" if a != b
+ * "a" if a == b
+ *
+ * Example 1:
+ * Input: nums = [0,1,2,4,5,7]
+ * Output: ["0->2","4->5","7"]
+ * Explanation: The ranges are:
+ * [0,2] --> "0->2"
+ * [4,5] --> "4->5"
+ * [7,7] --> "7"
+ *
+ *
+ * Example 2:
+ * Input: nums = [0,2,3,4,6,8,9]
+ * Output: ["0","2->4","6","8->9"]
+ * Explanation: The ranges are:
+ * [0,0] --> "0"
+ * [2,4] --> "2->4"
+ * [6,6] --> "6"
+ * [8,9] --> "8->9"
+ *
+ * Constraints:
+ * 0 <= nums.length <= 20
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * All the values of nums are unique.
+ * nums is sorted in ascending order.
+ * @author sniper
+ * @date 17 Jun 2023
+ * LC228, Easy
+ */
+public class SummaryRanges {
+
+    /**
+     * Understanding the following solution
+     * Time Complexity O(N)
+     * Space Complexity O(N)
+     * ------------------------------------
+     * class Solution:
+     *     def summaryRanges(self, nums: List[int]) -> List[str]:
+     *         res = []
+     *         n = len(nums)
+     *         i = 0
+     *         while i < n:
+     *             begin = nums[i]
+     *             while i < n - 1 and nums[i] + 1 == nums[i + 1]:
+     *                 i += 1
+     *             end = nums[i]
+     *             if begin == end:
+     *                 res.append(str(begin))
+     *             else:
+     *                 res.append(str(begin) + "->" + str(end))
+     *             i += 1
+     *         return res
+     * --------------------------------------
+     * class Solution {
+     * public:
+     *     vector<string> summaryRanges(vector<int>& nums) {
+     *         vector<string> res;
+     *         int n = nums.size();
+     *         for (int i = 0; i < n; ++i) {
+     *             const int begin = nums[i];
+     *             while (i < n - 1 && nums[i] + 1 == nums[i + 1]) {
+     *                 i++;
+     *             }
+     *             const int end = nums[i];
+     *             if (begin == end) {
+     *                 res.push_back(to_string(begin));
+     *             } else {
+     *                 res.push_back(to_string(begin) + "->" + to_string(end));
+     *             }
+     *         }
+     *         return res;
+     *     }
+     * };
+     * @param nums
+     * @return
+     */
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int begin = nums[i];
+            while (i < n - 1 && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+            int end = nums[i];
+            if (begin == end) {
+                res.add("" + begin);
+            } else {
+                res.add("" + begin + "->" + end);
+            }
+        }
+
+        return res;
+    }
+
+}
--- src/main/java/org/ict/algorithm/leetcode/slidewindow/SubstringWithConcatenationOfAllWords.java
+++ src/main/java/org/ict/algorithm/leetcode/slidewindow/SubstringWithConcatenationOfAllWords.java
@@ -52,10 +52,97 @@ import java.util.Map;
  */
 public class SubstringWithConcatenationOfAllWords {
 
+    public List<Integer> findSubstringV1(String s, String[] words) {
+        //todo
+        return null;
+    }
 
     /**
      * Understanding the following solution
      * Hash + Slide Window
+     * --------------------------------------------------------
+     * class Solution:
+     *     def findSubstring(self, s: str, words: List[str]) -> List[int]:
+     *         if len(s) == 0 or words == []:
+     *             return []
+     *
+     *         k = len(words)
+     *         n = len(words[0])
+     *         res = []
+     *         freq = collections.Counter(words)
+     *
+     *         for i in range(0, len(s) - k * n + 1):
+     *             seen = collections.defaultdict(int)
+     *             j = 0
+     *             while j < k:
+     *                 word = s[i + j * n: i + j * n + n]
+     *                 seen[word] += 1
+     *                 if seen[word] > freq[word]:
+     *                     break
+     *                 j += 1
+     *             if j == k:
+     *                 res.append(i)
+     *         return res
+     * --------------------------------------------------------
+     * class Solution {
+     * public:
+     *     vector<int> findSubstring(string s, vector<string>& words) {
+     *         if (s.empty() || words.empty()) {
+     *             return {};
+     *         }
+     *         const int k = words.size();
+     *         const int n = words[0].length();
+     *         vector<int> res;
+     *         unordered_map<string, int> freq;
+     *
+     *         for (const string& word : words) {
+     *             freq[word]++;
+     *         }
+     *
+     *         for (int i = 0; i < s.length() - k * n + 1; i++) {
+     *             unordered_map<string, int> seen;
+     *             int j = 0;
+     *             for (; j < k; j++) {
+     *                 const string& word = s.substr(i + j * n, n);
+     *                 seen[word]++;
+     *                 if (seen[word] > freq[word]) {
+     *                     break;
+     *                 }
+     *             }
+     *             if (j == k) {
+     *                 res.push_back(i);
+     *             }
+     *         }
+     *
+     *         return res;
+     *     }
+     * };
+     * --------------------------------------------------------
+     * e.g. s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+     * k:3, n:3, freq:[{"bar", 1}, {"foo", 1}, {"the", 1}]
+     * i:0, the length of s is 24, so the index-i ends up with 24 - 9 = 15
+     * i:0, j:0, j < k, i + j * n = 0, i + j * n + n = 3, word = s.substring(0, 3) = "bar", seen:[{"bar", 1}], j++
+     *      j:1, j < k, i + j * n = 3, i + j * n + n = 6, word = s.substring(3, 6) = "foo", seen:[{"bar", 1}, {"foo", 1}], j++
+     *      j:2, j < k, i + j * n = 6, i + j * n + n = 9, word = s.substring(6, 9) = "foo", seen:[{"bar", 1}, {"foo", 2}],
+     *          see.get("foo") > freq.get("foo"), inner-for-loop break
+     *          j:2, j != k
+     * i:1, j:0, ......, not satisfy
+     * i:2, j:0, ......, not satisfy
+     * i:3, j:0, ......, not satisfy
+     * i:4, j:0, ......, not satisfy
+     * i:5, j:0, ......, not satisfy
+     * i:6, j:0, j < k, i + j * n = 6,  i + j * n + n = 9,  word = s.substring(6, 9) = "foo", seen:[{"foo", 1}], j++
+     *      j:1, j < k, i + j * n = 9,  i + j * n + n = 12, word = s.substring(9, 12) = "bar", seen:[{"foo", 1}, {"bar", 1}], j++
+     *      j:2, j < k, i + j * n = 12, i + j * n + n = 15, word = s.substring(12, 15) = "the", seen:[{"foo", 1}, {"bar", 1}, {"the", 1}], j++
+     *      j:3, j == k, i:6 is added to the res list
+     * i:7, j:0, ......, not satisfy
+     * i:8, j:0, ......, not satisfy
+     * i:9, j:0, j < k, satisfy, 9 is added to the res list
+     * .....
+     * i:12, j:0, satisfy, 12 is added to the res list
+     *
+     * @author jianchao-li
+     * @see <a href="https://leetcode.com/problems/substring-with-concatenation-of-all-words/solutions/13658/easy-two-map-solution-c-java"></a>
      * @param s
      * @param words
      * @return
@@ -74,6 +161,9 @@ public class SubstringWithConcatenationOfAllWords {
             freq.put(word, freq.getOrDefault(word, 0) + 1);
         }
 
+        /**
+         * The slide window size is k*n, so while i == s.length() - k*n, the for-loop ends.
+         */
         for (int i = 0; i <= s.length() - k * n; i++) {
             Map<String, Integer> seen = new HashMap<>();
             int j = 0;
