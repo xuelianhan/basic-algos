d62362b7e340cad177ba4781573ab64beaac9888
xueliansniper <xueliansniper@gmail.com>
2023-06-25 09:40:48 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/binarysearch/FindMinimumInRotatedSortedArrayII.java
+++ src/main/java/org/ict/algorithm/leetcode/binarysearch/FindMinimumInRotatedSortedArrayII.java
@@ -45,7 +45,97 @@ package org.ict.algorithm.leetcode.binarysearch;
  */
 public class FindMinimumInRotatedSortedArrayII {
 
+    /**
+     * Divide-And-Conquer
+     * @param nums
+     * @return
+     */
+    public int findMinV1(int[] nums) {
+        return help(nums, 0, nums.length - 1);
+    }
+
+    private int help(int[] nums, int l, int r) {
+        if (l == r) {
+            return nums[r];
+        }
+        /**
+         * e.g. nums = [3, 1, 3]
+         */
+        if (nums[l] < nums[r]) {
+            return nums[l];
+        }
+        int mid = (l + r) / 2;
+        int leftV = help(nums, l, mid);
+        int rightV = help(nums, mid + 1, r);
+        return Math.min(leftV, rightV);
+    }
+
+
+    /**
+     * Understanding the following solution
+     * -------------------------------------------------------
+     * When there are a lot of duplicate numbers in the array,
+     * it breaks the dichotomous lookup mechanism and will not achieve O(lgn) time complexity,
+     * and will go back to the simple and brutal O(n),
+     * such as these two cases: [2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 2],
+     * and [2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2].
+     * When the first number and the last number and the middle number are all equal,
+     * the dichotomous lookup method breaks down because it cannot tell whether to go to the left or the right half.
+     * In this case, shifting the right pointer one place to the left (or shifting the left pointer one place to the right),
+     * and omitting an identical number has no effect on the result,
+     * because only one identical number is removed,
+     * and then the dichotomy continues for the remaining part,
+     * which in the worst case,
+     * such as when all elements of the array are identical,
+     * the time complexity rises to O(n).
+     * ------------------------------------------
+     * class Solution:
+     *     def findMin(self, nums: List[int]) -> int:
+     *         l, r = 0, len(nums) - 1
+     *         while l < r:
+     *             mid = (l + r) // 2
+     *             if nums[mid] == nums[r]:
+     *                 r -= 1
+     *             elif nums[mid] < nums[r]:
+     *                 r = mid
+     *             else:
+     *                 l = mid + 1
+     *         return nums[l]
+     * -------------------------------------------
+     * class Solution {
+     * public:
+     *     int findMin(vector<int>& nums) {
+     *         int l = 0;
+     *         int r = (int)nums.size() - 1;
+     *         while (l < r) {
+     *             int mid = l + (r - l) / 2;
+     *             if (nums[mid] == nums[r]) {
+     *                 r--;
+     *             } else if (nums[mid] < nums[r]) {
+     *                 r = mid;
+     *             } else {
+     *                 l = mid + 1;
+     *             }
+     *         }
+     *         return nums[l];
+     *     }
+     * };
+     * @param nums
+     * @return
+     */
     public int findMin(int[] nums) {
-        return 0;
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == nums[r]) {
+                r--;
+            } else if (nums[mid] < nums[r]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return nums[l];
     }
 }
---
+++ src/main/java/org/ict/algorithm/leetcode/heap/FindKPairsWithSmallestSums.java
@@ -0,0 +1,128 @@
+package org.ict.algorithm.leetcode.heap;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+ * Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+ * Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+ *
+ * Example 1:
+ * Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+ * Output: [[1,2],[1,4],[1,6]]
+ * Explanation:
+ * The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+ *
+ * Example 2:
+ * Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+ * Output: [[1,1],[1,1]]
+ * Explanation:
+ * The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+ *
+ * Example 3:
+ * Input: nums1 = [1,2], nums2 = [3], k = 3
+ * Output: [[1,3],[2,3]]
+ * Explanation:
+ * All possible pairs are returned from the sequence: [1,3],[2,3]
+ *
+ * Constraints:
+ * 1 <= nums1.length, nums2.length <= 10^5
+ * -10^9 <= nums1[i], nums2[i] <= 10^9
+ * nums1 and nums2 both are sorted in ascending order.
+ * 1 <= k <= 10^4
+ *
+ * @author sniper
+ * @date 24 Jun 2023
+ * LC373, Medium, High Frequency, Top-150
+ */
+public class FindKPairsWithSmallestSums {
+
+    /**
+     * Understanding the following solution
+     * @param nums1
+     * @param nums2
+     * @param k
+     * @return
+     */
+    public List<List<Integer>> kSmallestPairsV1(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<SumPair> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a.sum));
+        for (int i = 0; i < Math.min(k, nums1.length); i++) {
+            minHeap.offer(new SumPair(i, 0, nums1[i] + nums2[0]));
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        while (!minHeap.isEmpty() && res.size() < k) {
+            SumPair pair = minHeap.poll();
+            int i = pair.i;
+            int j = pair.j;
+            res.add(Arrays.asList(nums1[i], nums2[j]));
+            if (j + 1 < nums2.length) {
+                minHeap.offer(new SumPair(i, j + 1, nums1[i] + nums2[j + 1]));
+            }
+        }
+
+        return res;
+    }
+
+    /**
+     * Understanding the following solution
+     * @param nums1
+     * @param nums2
+     * @param k
+     * @return
+     */
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<SumPair> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a.sum));
+        for (int i = 0; i < k && i < nums1.length; i++) {
+            minHeap.offer(new SumPair(i, 0, nums1[i] + nums2[0]));
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        while (!minHeap.isEmpty() && res.size() < k) {
+            int i = minHeap.peek().i;
+            int j = minHeap.poll().j;
+            res.add(Arrays.asList(nums1[i], nums2[j]));
+            if (j + 1 < nums2.length) {
+                minHeap.offer(new SumPair(i, j + 1, nums1[i] + nums2[j + 1]));
+            }
+        }
+
+        return res;
+    }
+
+    private static class SumPair {
+        private int i;
+        private int j;
+        private int sum;
+        public SumPair(int i, int j, int sum) {
+            this.i = i;
+            this.j = j;
+            this.sum = sum;
+        }
+    }
+
+    /**
+     * Time Limit Exceeded
+     * @param nums1
+     * @param nums2
+     * @param k
+     * @return
+     */
+    public List<List<Integer>> kSmallestPairsOutOfTime(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0] + b[1] - a[0] - a[1]);
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                maxHeap.offer(new int[] {nums1[i], nums2[j]});
+                if (maxHeap.size() > k) {
+                    maxHeap.poll();
+                }
+            }
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        for (int[] item : maxHeap) {
+            res.add(Arrays.stream(item).boxed().collect(Collectors.toList()));
+        }
+        return res;
+    }
+}
