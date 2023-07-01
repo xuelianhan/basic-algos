c46f1eb00074ee46862f9598de6d5b1be74cc228
xueliansniper <xueliansniper@gmail.com>
2023-07-01 10:06:21 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/dynamicprogramming/EditDistance.java
+++ src/main/java/org/ict/algorithm/leetcode/dynamicprogramming/EditDistance.java
@@ -55,7 +55,7 @@ public class EditDistance {
     /**
      * Dynamic Programming Solution
      * Time Cost 6ms
-     * ----------------------------
+     * -----------------------------
      * dp[i][j] represents the steps needed to convert from the first i characters of word1 to the first j characters of word2.
      * First, assign a value to the first row and column of this two-dimensional array dp,
      * which is simple because the first row and column always correspond to a string that is empty,
@@ -74,7 +74,26 @@ public class EditDistance {
      *            ------- dp[i-1][j-1], if word1[i-1] == word2[j-1]
      * dp[i][j] = |
      *            |______ min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1
+     * -----------------------------------------------------------
+     * class Solution:
+     *     def minDistance(self, word1: str, word2: str) -> int:
+     *         m = len(word1)
+     *         n = len(word2)
+     *         dp = [[0] * (n + 1) for _ in range(m + 1)]
      *
+     *         for i in range(1, m + 1):
+     *             dp[i][0] = i
+     *
+     *         for j in range(1, n + 1):
+     *             dp[0][j] = j
+     *
+     *         for i in range(1, m + 1):
+     *             for j in range(1, n + 1):
+     *                 if word1[i - 1] == word2[j - 1]:
+     *                     dp[i][j] = dp[i - 1][j - 1]
+     *                 else:
+     *                     dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+     *         return dp[m][n]
      * @param word1
      * @param word2
      * @return
