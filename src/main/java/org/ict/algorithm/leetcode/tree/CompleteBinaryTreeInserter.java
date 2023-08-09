0d70882c24897ded6f4bd00746b5e634e392307f
xueliansniper <xueliansniper@gmail.com>
2023-08-09 10:21:51 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/tree/CompleteBinaryTreeInserter.java
+++ src/main/java/org/ict/algorithm/leetcode/tree/CompleteBinaryTreeInserter.java
@@ -1,5 +1,8 @@
 package org.ict.algorithm.leetcode.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A complete binary tree is a binary tree in which every level,
  * except possibly the last, is completely filled,
@@ -44,18 +47,65 @@ package org.ict.algorithm.leetcode.tree;
  */
 public class CompleteBinaryTreeInserter {
 
+    /**
+     * Time Cost 14ms
+     */
     class CBTInserter {
 
-        public CBTInserter(TreeNode root) {
+        private List<TreeNode> tree = new ArrayList<>();
 
+        /**
+         * new CBTInserter([1, 2]);
+         *     1
+         *    /
+         *  2
+         * @param root
+         */
+        public CBTInserter(TreeNode root) {
+            tree.add(root);
+            for (int i = 0; i < tree.size(); i++) {
+                TreeNode node = tree.get(i);
+                if (node.left != null) {
+                    tree.add(node.left);
+                }
+                if (node.right != null) {
+                    tree.add(node.right);
+                }
+            }
         }
 
+        /**
+         * insert(4):
+         *          1
+         *         / \
+         *        2   3
+         *       /
+         *      4
+         *
+         * @param val
+         * @return
+         */
         public int insert(int val) {
-            return 0;
+            final int n = tree.size();
+            TreeNode node = new TreeNode(val);
+            /**
+             * Notice (n - 1) / 2 here
+             */
+            TreeNode parent = tree.get((n - 1) / 2);
+            tree.add(node);
+            /**
+             * Notice n % 2 == 1
+             */
+            if (n % 2 == 1) {
+                parent.left = node;
+            } else {
+                parent.right = node;
+            }
+            return parent.val;
         }
 
         public TreeNode get_root() {
-            return null;
+            return tree.get(0);
         }
     }
     /**
