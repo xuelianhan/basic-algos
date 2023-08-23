94b3168c26a5d130fb2abc6cb20b19f01f8c597b
xueliansniper <xueliansniper@gmail.com>
2023-08-23 21:16:05 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/heap/ReorganizeString.java
+++ src/main/java/org/ict/algorithm/leetcode/heap/ReorganizeString.java
@@ -36,15 +36,65 @@ import java.util.Queue;
 public class ReorganizeString {
 
     /**
+     * No Sorting O(N):
+     *
+     * count letter appearance and store in hash[i]
+     * find the letter with the largest occurrence.
+     * put the letter into even index number (0, 2, 4 ...) char array
+     * put the rest into the array
+     *
      * e.g. s = "vvvlo"
+     * @author fangbiyi
+     * @see <a href="https://leetcode.com/problems/reorganize-string/solutions/232469/java-no-sort-o-n-0ms-beat-100"></a>
      * @param s
      * @return
      */
     public String reorganizeStringV1(String s) {
-        return null;
+        int[] bucket = new int[26];
+        int n = s.length();
+        int maxFreq = 0;
+        int letter = 0;
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+            bucket[c]++;
+            if (bucket[c] > maxFreq) {
+                maxFreq = bucket[c];
+                letter = c;
+            }
+        }
+
+        if (maxFreq > (n + 1) / 2) {
+            return "";
+        }
+
+        /**
+         * Put the most frequent letter into even index:
+         * 0, 2, 4, 6,...
+         */
+        char[] res = new char[n];
+        int idx = 0;
+        while (bucket[letter] > 0) {
+            res[idx] = (char)(letter + 'a');
+            idx += 2;
+            bucket[letter]--;
+        }
+
+        for (int i = 0; i < bucket.length; i++) {
+            while (bucket[i] > 0) {
+                if (idx >= n) {
+                    idx = 1;
+                }
+                res[idx] = (char)(i + 'a');
+                idx += 2;
+                bucket[i]--;
+            }
+        }
+
+        return String.valueOf(res);
     }
 
     /**
+     * Understanding the following solution
      * Time Cost 5ms
      * e.g. s = "vvvlo"
      * @param s
@@ -79,7 +129,7 @@ public class ReorganizeString {
         /**
          * e.g. s = "vvvlo"
          * maxHeap:{3, v},{1, l},{1, o}
-         * res:v, prevChar:v, prevFreq:2
+         * res:v, maxHeap:{1, l},{1, o}, prevChar:v, prevFreq:2
          * res:vl, prevFreq > 0, maxHeap:{2, v},{1, o}, prevChar:l, prevFreq:0
          * res:vlv, prevFreq==0, prevChar:v, prevFreq:1, maxHeap:{1, o}
          * res:vlvo, prevFreq > 0, maxHeap:{v, 1}, prevChar:o, prevFreq:0
