b42abb7a116dfd0bb35fb1b637900af04da4000d
xueliansniper <xueliansniper@gmail.com>
2023-08-24 23:13:58 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/randomized/RandomPickIndex.java
+++ src/main/java/org/ict/algorithm/leetcode/randomized/RandomPickIndex.java
@@ -1,11 +1,6 @@
 package org.ict.algorithm.leetcode.randomized;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Given an integer array nums with possible duplicates,
