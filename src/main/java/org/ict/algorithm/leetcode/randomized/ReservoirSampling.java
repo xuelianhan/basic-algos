5e4e766ab82cb32fa360b8d57a1fd51da5a9d3ac
xueliansniper <xueliansniper@gmail.com>
2023-08-25 08:05:13 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/randomized/ReservoirSampling.java
+++ src/main/java/org/ict/algorithm/leetcode/randomized/ReservoirSampling.java
@@ -45,14 +45,20 @@ import java.util.Random;
  *   we pick a random index from 0 to i and if the picked index is one of the first k indexes,
  *   we replace the element at picked index with stream[i]
  *   To simplify the proof, let us first consider the last item. 
- *   The probability that the last item is in final reservoir = The probability that one of the first k indexes is picked for last item = k/n (the probability of picking one of the k items from a list of size n)
+ *   The probability that the last item is in final reservoir = The probability
+ *   that one of the first k indexes is picked for last item = k/n
+ *   (the probability of picking one of the k items from a list of size n)
  *   Let us now consider the second last item. 
- *   The probability that the second last item is in final reservoir[] = [Probability that one of the first k indexes is picked in iteration for stream[n-2]] X [Probability that the index picked in iteration for stream[n-1] is not same as index picked for stream[n-2] ] = [k/(n-1)]*[(n-1)/n] = k/n.
+ *   The probability that the second last item is in final reservoir[] = [Probability
+ *   that one of the first k indexes is picked in iteration for stream[n-2]] X [Probability
+ *   that the index picked in iteration for stream[n-1] is not same as index picked for stream[n-2] ] = [k/(n-1)]*[(n-1)/n] = k/n.
  *   Similarly, we can consider other items for all stream items from stream[n-1] to stream[k] and generalize the proof.
  *
  *   Case2: For first k stream items, i.e., for stream[i] where 0 <= i < k
  *   The first k items are initially copied to reservoir[] and may be removed later in iterations for stream[k] to stream[n].
- *   The probability that an item from stream[0...k-1] is in final array = Probability that the item is not picked when items stream[k], stream[k+1], …. stream[n-1] are considered = [k/(k+1)] x [(k+1)/(k+2)] x [(k+2)/(k+3)] x … x [(n-1)/n] = k/n
+ *   The probability that an item from stream[0...k-1] is
+ *   in final array = Probability that the item is not picked when items stream[k], stream[k+1], …. stream[n-1]
+ *   are considered = [k/(k+1)] x [(k+1)/(k+2)] x [(k+2)/(k+3)] x … x [(n-1)/n] = k/n
  *   References:http://en.wikipedia.org/wiki/Reservoir_sampling
  */
 public class ReservoirSampling {
