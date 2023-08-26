acf3c2ace1046da592e8cc707f528eab9e7dbf82
xueliansniper <xueliansniper@gmail.com>
2023-08-27 00:11:27 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/twopointers/RotatingTheBox.java
+++ src/main/java/org/ict/algorithm/leetcode/twopointers/RotatingTheBox.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  * A stationary obstacle '*'
  * Empty '.'
  *
- * The box is rotated 90 degrees clockwise, causing some of the stones to fall due to gravity.
+ * The box is rotated 90 degrees clockwise, causing some stones to fall due to gravity.
  * Each stone falls down until it lands on an obstacle, another stone, or the bottom of the box.
  * Gravity does not affect the obstacles' positions,
  * and the inertia from the box's rotation does not affect the stones' horizontal positions.
@@ -57,6 +57,46 @@ import java.util.Arrays;
 public class RotatingTheBox {
 
     /**
+     * Time Cost 7ms
+     * @param box
+     * @return
+     */
+    public char[][] rotateTheBoxV1(char[][] box) {
+        int m = box.length;
+        int n = box[0].length;
+        char stone = '#';
+        char obstacle = '*';
+        char empty = '.';
+        /**
+         * Notice after rotated 90 degrees clockwise, the
+         * result matrix is n X m, not m X n
+         */
+        char[][] res = new char[n][m];
+        for (int i = 0; i < m; i++) {
+            int k = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (box[i][j] == obstacle) {
+                    k = j - 1;
+                }
+                if (box[i][j] == stone) {
+                    box[i][j] = empty;
+                    box[i][k] = stone;
+                    k--;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = n - 1; j >= 0; j--) {
+                res[j][m - i - 1] = box[i][j];
+            }
+        }
+
+        return res;
+    }
+
+    /**
+     * Time Cost 10ms
      * '#' represents stone
      * '*' represents obstacle
      * '.' represents empty
@@ -68,12 +108,27 @@ public class RotatingTheBox {
     public char[][] rotateTheBox(char[][] box) {
         int m = box.length;
         int n = box[0].length;
-        char[][] res = new char[m][n];
+        /**
+         * Notice after rotated 90 degrees clockwise, the
+         * result matrix is n X m, not m X n
+         */
+        char[][] res = new char[n][m];
+        char obstacle = '*';
+        char empty = '.';
+        /**
+         * Don't forget fill '.' here
+         */
         Arrays.stream(res).forEach(row -> Arrays.fill(row, '.'));
         for (int i = 0; i < m; i++) {
+            /**
+             * Notice the j starts from the tail to head here.
+             */
             for (int j = n - 1, k = n - 1; j >= 0; j--) {
-                if (box[i][j] != '.') {
-                    if (box[i][j] == '*') {
+                /**
+                 * box[i][j] is stone or obstacle.
+                 */
+                if (box[i][j] != empty) {
+                    if (box[i][j] ==  obstacle) {
                         k = j;
                     }
                     res[k--][m - i - 1] = box[i][j];
