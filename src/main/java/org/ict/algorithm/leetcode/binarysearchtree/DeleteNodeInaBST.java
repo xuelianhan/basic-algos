28b948789aa50e57925d5bbdb67d4bab53feed4b
xueliansniper <xueliansniper@gmail.com>
2023-08-28 12:00:27 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/binarysearchtree/DeleteNodeInaBST.java
+++ src/main/java/org/ict/algorithm/leetcode/binarysearchtree/DeleteNodeInaBST.java
@@ -47,7 +47,72 @@ package org.ict.algorithm.leetcode.binarysearchtree;
  */
 public class DeleteNodeInaBST {
 
-    public TreeNode deleteNode(TreeNode root, int key) {
+    public TreeNode deleteNodeV3(TreeNode root, int key) {
+        //todo
         return null;
+    }
+
+    public TreeNode deleteNodeV2(TreeNode root, int key) {
+        //todo
+        return null;
+    }
+
+    /**
+     * Generalized solution for all binary trees
+     * @param root
+     * @param key
+     * @return
+     */
+    public TreeNode deleteNodeV1(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == key) {
+            if (root.right == null) {
+                TreeNode left = root.left;
+                root = null;
+                return left;
+            } else {
+                TreeNode cur = root.right;
+                while (cur.left != null) {
+                    cur = cur.left;
+                }
+                swap(root, cur);
+            }
+        }
+        root.left = deleteNodeV1(root.left, key);
+        root.right = deleteNodeV1(root.right, key);
+        return root;
+    }
+
+    private void swap(TreeNode root, TreeNode cur) {
+        int temp = root.val;
+        root.val = cur.val;
+        cur.val = temp;
+    }
+
+
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null || root.right == null) {
+                root = (root.left == null ? root.right : root.left);
+            } else {
+                TreeNode cur = root.right;
+                while (cur.left != null) {
+                    cur = cur.left;
+                }
+                root.val = cur.val;
+                root.right = deleteNode(root.right, cur.val);
+            }
+        }
+        return root;
     }
 }
