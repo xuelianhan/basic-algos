f98bcb20c359808cee5cfbd64644b099f300a8c5
xueliansniper <xueliansniper@gmail.com>
2023-08-29 08:32:05 +0800

    add codes

--- src/main/java/org/ict/algorithm/leetcode/monotonicstack/RemoveKDigits.java
+++ src/main/java/org/ict/algorithm/leetcode/monotonicstack/RemoveKDigits.java
@@ -1,5 +1,8 @@
 package org.ict.algorithm.leetcode.monotonicstack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * Given string num representing a non-negative integer num, and an integer k,
  * return the smallest possible integer after removing k digits from num.
@@ -33,7 +36,59 @@ package org.ict.algorithm.leetcode.monotonicstack;
  */
 public class RemoveKDigits {
 
-    public String removeKdigits(String num, int k) {
+
+
+    public String removeKdigitsV1(String num, int k) {
+        //todo
         return null;
+    }
+
+    /**
+     * Make the number on the high digit as small as possible in order to make the whole remaining number as small as possible
+     * e.g. num = "1432219", k = 3
+     * i:0, k:3, stack:1
+     * i:1, k:3, stack:1,4
+     * i:2, k:2, stack:1,3
+     * i:3, k:2, stack:1,2
+     * i:4, k:2, stack:1,2,2
+     * i:5, k:1, stack:1,1,9
+     *
+     * @param num
+     * @param k
+     * @return
+     */
+    public String removeKdigits(String num, int k) {
+        /**
+         * e.g. num = "10", k = 2
+         */
+        if (num.length() == k) {
+            return "0";
+        }
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char ch : num.toCharArray()) {
+            while (k > 0 && !stack.isEmpty() && stack.peek() > ch) {
+                stack.pop();
+                k--;
+            }
+            stack.push(ch);
+        }
+
+        while (k > 0 && !stack.isEmpty()) {
+            stack.pop();
+            k--;
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (char ch : stack) {
+            /**
+             * Skip the leading '0'
+             */
+            if (ch == '0' && res.length() == 0) {
+                continue;
+            }
+            res.append(ch);
+        }
+        
+        return res.length() == 0 ? "0" : res.toString();
     }
 }
